d101e28dc87c780ddbfc97d81b3f073f930aaf27
sethyx <sethyx@gmail.com>
2013-03-27 21:15:23 +0100

    Add nightly build style
    Change-Id: I118a8c49c87ad11243f0d6ebe5cee5e1f20a83af
    Signed-off-by: sethyx <sethyx@gmail.com>

--- configs/common_versions.mk
+++ configs/common_versions.mk
@@ -4,17 +4,23 @@ PRODUCT_BUILD_PROP_OVERRIDES += BUILD_VERSION_TAGS=release-keys USER=android-bui
 DATE = $(shell vendor/aokp/tools/getdate)
 
 ifneq ($(AOKP_BUILD),)
-	# AOKP_BUILD=<goo version int>/<build string>
-	PRODUCT_PROPERTY_OVERRIDES += \
-	    ro.goo.developerid=aokp \
-	    ro.goo.rom=aokp \
-	    ro.goo.version=$(shell echo $(AOKP_BUILD) | cut -d/ -f1) \
-		ro.aokp.version=$(TARGET_PRODUCT)_jb-mr1_$(shell echo $(AOKP_BUILD) | cut -d/ -f2)
+    # AOKP_BUILD=<goo version int>/<build string>
+    PRODUCT_PROPERTY_OVERRIDES += \
+        ro.goo.developerid=aokp \
+        ro.goo.rom=aokp \
+        ro.goo.version=$(shell echo $(AOKP_BUILD) | cut -d/ -f1) \
+        ro.aokp.version=$(TARGET_PRODUCT)_jb-mr1_$(shell echo $(AOKP_BUILD) | cut -d/ -f2)
 else
-	PRODUCT_PROPERTY_OVERRIDES += \
-		ro.aokp.version=$(TARGET_PRODUCT)_unofficial_$(DATE)
+    ifneq ($(AOKP_NIGHTLY),)
+        # AOKP_NIGHTLY=true
+        PRODUCT_PROPERTY_OVERRIDES += \
+            ro.aokp.version=$(TARGET_PRODUCT)_nightly_$(DATE)
+    else
+    PRODUCT_PROPERTY_OVERRIDES += \
+        ro.aokp.version=$(TARGET_PRODUCT)_unofficial_$(DATE)
+    endif
 endif
 
 # Camera shutter sound property
 PRODUCT_PROPERTY_OVERRIDES += \
-	persist.sys.camera-sound=1
+    persist.sys.camera-sound=1
