8430747b017df67550b2012f12e94ffb9bf44df3
Dirk Rettschlag <dirk.rettschlag@gmail.com>
2013-04-17 14:26:00 +0200

    Add property to define phone type
    This value is used as fallback in TelephonyManager
    
    Change-Id: I1df5df66b8cd3de0235e1bbc7d537a17bd39593b
    Signed-off-by: Dirk Rettschlag <dirk.rettschlag@gmail.com>

--- configs/cdma.mk
+++ configs/cdma.mk
@@ -1,3 +1,6 @@
 # CDMA APN list
 PRODUCT_COPY_FILES += \
     vendor/aokp/prebuilt/common/etc/apns-conf-cdma.xml:system/etc/apns-conf.xml
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    gsm.current.phone-type=2
--- configs/gsm.mk
+++ configs/gsm.mk
@@ -5,3 +5,6 @@ PRODUCT_COPY_FILES += \
 # SIM Toolkit
 PRODUCT_PACKAGES += \
     Stk
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    gsm.current.phone-type=1
