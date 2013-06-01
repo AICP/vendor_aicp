63f7233ab773fe3f7ca7a29b2ab5d70745cfd9d5
Gergely Szell (sethyx) <szell@hu.ibm.com>
2013-06-01 21:30:57 +0200

    Add Google Analytics support
    PS2: add branch build prop
    PS3: add device prop since in many cases the ro.product.device property differs
         from the AOKP codename
    
    Change-Id: Ica9f4034bf8363fb89bc40d3beb134c5aa17803d
    Signed-off-by: Gergely Szell (sethyx) <szell@hu.ibm.com>

--- configs/common_versions.mk
+++ configs/common_versions.mk
@@ -2,6 +2,7 @@
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_VERSION_TAGS=release-keys USER=android-build BUILD_UTC_DATE=$(shell date +"%s")
 
 DATE = $(shell vendor/aokp/tools/getdate)
+AOKP_BRANCH=jb-mr1
 
 ifneq ($(AOKP_BUILD),)
     # AOKP_BUILD=<goo version int>/<build string>
@@ -9,7 +10,7 @@ ifneq ($(AOKP_BUILD),)
         ro.goo.developerid=aokp \
         ro.goo.rom=aokp \
         ro.goo.version=$(shell echo $(AOKP_BUILD) | cut -d/ -f1) \
-        ro.aokp.version=$(TARGET_PRODUCT)_jb-mr1_$(shell echo $(AOKP_BUILD) | cut -d/ -f2)
+        ro.aokp.version=$(TARGET_PRODUCT)_$(AOKP_BRANCH)_$(shell echo $(AOKP_BUILD) | cut -d/ -f2)
 else
     ifneq ($(AOKP_NIGHTLY),)
         # AOKP_NIGHTLY=true
@@ -20,6 +21,11 @@ else
         ro.aokp.version=$(TARGET_PRODUCT)_unofficial_$(DATE)
     endif
 endif
+
+# needed for statistics
+PRODUCT_PROPERTY_OVERRIDES += \
+        ro.aokp.branch=$(AOKP_BRANCH) \
+        ro.aokp.device=$(AOKP_PRODUCT)
 
 # Camera shutter sound property
 PRODUCT_PROPERTY_OVERRIDES += \
