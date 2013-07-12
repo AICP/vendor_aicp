b15305639c29f8e61e09b8fde7a0a7ba1aaceca4
Brandon McAnsh <brandon.mcansh@gmail.com>
2013-07-12 04:59:00 +0200

    jfltevzw: Update fingerprint to ME7 OTA
    * JDQ39.I545VRUAME7
    
    Change-Id: I2e7c941dc239412e2a742bbf432678fe6017e190
    Signed-off-by: Brandon McAnsh <brandon.mcansh@gmail.com>

--- products/jfltevzw.mk
+++ products/jfltevzw.mk
@@ -20,7 +20,7 @@ PRODUCT_DEVICE := jfltevzw
 PRODUCT_MODEL := SCH-I545
 PRODUCT_MANUFACTURER := Samsung
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltevzw TARGET_DEVICE=jfltevzw BUILD_FINGERPRINT="Verizon/jfltevzw/jfltevzw:4.2.2/JDQ39/I545VRUAMDK:user/release-keys" PRIVATE_BUILD_DESC="jfltevzw-user 4.2.2 JDQ39 I545VRUAMDK release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltevzw TARGET_DEVICE=jfltevzw BUILD_FINGERPRINT="Verizon/jfltevzw/jfltevzw:4.2.2/JDQ39/I545VRUAME7:user/release-keys" PRIVATE_BUILD_DESC="jfltevzw-user 4.2.2 JDQ39 I545VRUAME7 release-keys"
 
 # boot animation
 PRODUCT_COPY_FILES += \
