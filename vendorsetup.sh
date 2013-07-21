3cf9b4e8dee69b27ecf7c8f2eaf16a50915524e1
sethyx <sethyx@gmail.com>
2013-07-21 16:57:01 +0200

    pollux_windy: remove underscore
    Change-Id: Ide738406f148e5a120ccfa7b34c157c6456a2e21
    Signed-off-by: sethyx <sethyx@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -29,7 +29,7 @@ add_lunch_combo aokp_mint-userdebug
 add_lunch_combo aokp_odin-userdebug
 add_lunch_combo aokp_p930-userdebug
 add_lunch_combo aokp_pollux-userdebug
-add_lunch_combo aokp_pollux_windy-userdebug
+add_lunch_combo aokp_polluxwindy-userdebug
 add_lunch_combo aokp_solana-userdebug
 add_lunch_combo aokp_spyder-userdebug
 add_lunch_combo aokp_su640-userdebug
