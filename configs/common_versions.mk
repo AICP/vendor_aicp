f13e517408c0d6f9ccb572a5e199623cde1bea10
Brandon McAnsh <brandon.mcansh@gmail.com>
2013-08-13 03:19:55 +0200

    AOKP: bump to jb-mr2 and bump apns version
    Change-Id: If4013b878bc19e76b35b1a17b36825f6d56917cb
    Signed-off-by: Brandon McAnsh <brandon.mcansh@gmail.com>

--- configs/common_versions.mk
+++ configs/common_versions.mk
@@ -2,7 +2,7 @@
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_VERSION_TAGS=release-keys USER=android-build BUILD_UTC_DATE=$(shell date +"%s")
 
 DATE = $(shell vendor/aokp/tools/getdate)
-AOKP_BRANCH=jb-mr1
+AOKP_BRANCH=jb-mr2
 
 ifneq ($(AOKP_BUILD),)
     # AOKP_BUILD=<goo version int>/<build string>
