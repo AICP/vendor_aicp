0493cdeff3c5cd11415ad8edb24fa876c2d65c8f
DAGr8 <ren.o.custom@gmail.com>
2013-08-28 00:03:28 -0400

    Find 5 to aokp
    Change-Id: I261978910973658f3347af8e837427212f2c27d0
    
    World phone for find 5
    
    Change-Id: I261978910973658f3347af8e837427212f2c27d0

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -5,6 +5,7 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/d2usc.mk \
     $(LOCAL_DIR)/d2vzw.mk \
     $(LOCAL_DIR)/evita.mk \
+    $(LOCAL_DIR)/find5.mk \
     $(LOCAL_DIR)/grouper.mk \
     $(LOCAL_DIR)/hercules.mk \
     $(LOCAL_DIR)/i605.mk \
