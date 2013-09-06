44256638b047d06545cebc924aa1c9d1b061afd5
Dirk Rettschlag <dirk.rettschlag@gmail.com>
2013-09-06 09:49:47 +0200

    Update grouper fingerprint
    Change-Id: Ic2a05acf83d54756aa2f369d5f3dbc4d42e5e772
    Signed-off-by: Dirk Rettschlag <dirk.rettschlag@gmail.com>

--- products/grouper.mk
+++ products/grouper.mk
@@ -14,7 +14,7 @@ PRODUCT_DEVICE := grouper
 PRODUCT_MODEL := Nexus 7
 PRODUCT_MANUFACTURER := asus
 
-PRODUCT_BUILD_PROP_OVERRIDES := PRODUCT_NAME=nakasi BUILD_FINGERPRINT=google/nakasi/grouper:4.2.2/JDQ39/573038:user/release-keys PRIVATE_BUILD_DESC="nakasi-user 4.2.2 JDQ39 573038 release-keys" BUILD_NUMBER=573038
+PRODUCT_BUILD_PROP_OVERRIDES := PRODUCT_NAME=nakasi BUILD_FINGERPRINT=google/nakasi/grouper:4.3/JWR66V/573038:user/release-keys PRIVATE_BUILD_DESC="nakasi-user 4.3 JWR66V 573038 release-keys" BUILD_NUMBER=573038
 
 PRODUCT_COPY_FILES += \
     vendor/aokp/prebuilt/bootanimation/bootanimation_1280_800.zip:system/media/bootanimation-alt.zip
