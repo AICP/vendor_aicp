417acf9c9e161e8e6dbc48ae996c40312c6119ff
Whitehawkx <xxwhitehawk@gmail.com>
2013-09-06 21:27:52 -0700

    m7att/m7ul: update fingerprint from Google Edition
    Change-Id: Ib503cb9f13907f6d7cf456a7d07e15d3f24cbbe7
    Signed-off-by: Whitehawkx <xxwhitehawk@gmail.com>

--- products/m7ul.mk
+++ products/m7ul.mk
@@ -7,7 +7,7 @@ $(call inherit-product, vendor/aokp/configs/common.mk)
 $(call inherit-product, device/htc/m7ul/full_m7ul.mk)
 
 # Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=m7 BUILD_ID=JZO54K BUILD_FINGERPRINT="htc_europe/m7/m7:4.1.2/JZO54K/177101.12:user/release-keys" PRIVATE_BUILD_DESC="1.29.401.12 CL177101 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=m7 BUILD_ID=JWR66V.H10 BUILD_FINGERPRINT="htc/m7_google/m7:4.3/JWR66V.H10/230993:user/release-keys" PRIVATE_BUILD_DESC="3.06.1700.10 CL230993 release-keys"
 
 PRODUCT_COPY_FILES += \
     vendor/aokp/prebuilt/bootanimation/bootanimation_1080_1920.zip:system/media/bootanimation-alt.zip
