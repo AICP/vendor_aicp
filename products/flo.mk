4b8cba45633bdfc9ddb0c73ee9649593c6de12cd
Rohan Mathur <rohanmathur34@gmail.com>
2013-09-10 04:09:15 +0200

    aokp: Add flo
    Change-Id: I061114040ddc821d666e21477adc280dea4476fa
    Signed-off-by: Rohan Mathur <rohanmathur34@gmail.com>

---
+++ products/flo.mk
@@ -0,0 +1,25 @@
+# Inherit AOKP common_tablet bits
+$(call inherit-product, vendor/aokp/configs/common_tablet.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/asus/flo/full_flo.mk)
+
+# Flo Overlay
+PRODUCT_PACKAGE_OVERLAYS += vendor/aokp/overlay/flo
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := flo
+PRODUCT_NAME := aokp_flo
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Nexus 7
+PRODUCT_MANUFACTURER := asus
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=razor BUILD_FINGERPRINT=google/razor/flo:4.3/JSS15Q/779366:user/release-keys PRIVATE_BUILD_DESC="razor-user 4.3 JSS15Q 779366 release-keys"
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+
+PRODUCT_COPY_FILES += \
+    vendor/aokp/prebuilt/bootanimation/bootanimation_1080_1920.zip:system/media/bootanimation-alt.zip
+
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,6 +2,7 @@ add_lunch_combo aokp_mako-userdebug
 add_lunch_combo aokp_maguro-userdebug
 add_lunch_combo aokp_toro-userdebug
 add_lunch_combo aokp_toroplus-userdebug
+add_lunch_combo aokp_flo-userdebug
 add_lunch_combo aokp_grouper-userdebug
 add_lunch_combo aokp_tilapia-userdebug
 add_lunch_combo aokp_manta-userdebug
