a72149552527c5103d9481a332c3de64a55fbb6b
LorDClockaN <davor@losinj.com>
2013-10-28 20:27:56 +0100

    endeavoru: Update build.prop and build fingerprint
    Change-Id: I30a720fa0174c13dc972069fb16132dc878d4bf9

--- products/endeavoru.mk
+++ products/endeavoru.mk
@@ -16,11 +16,15 @@ PRODUCT_PACKAGE_OVERLAYS += vendor/aicp/overlay/endeavoru
 PRODUCT_DEVICE := endeavoru
 PRODUCT_NAME := aicp_endeavoru
 PRODUCT_BRAND := htc
-PRODUCT_MODEL := One X
+PRODUCT_MODEL := EndeavorU
 PRODUCT_MANUFACTURER := HTC
 
-# Set build fingerprint / ID / Product Name etc.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=endeavoru TARGET_DEVICE=endeavoru BUILD_FINGERPRINT="htc_europe/endeavoru/endeavoru:4.1.1/JRO03C/128187.31:user/release-keys" PRIVATE_BUILD_DESC="3.14.401.31 CL128187 release-keys"
+#Set build fingerprint / ID / Product Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=htc_europe \
+    TARGET_DEVICE=endeavoru \
+    BUILD_FINGERPRINT="htc/htc_europe/endeavoru:4.2.2/JDQ39/231174.2:user/release-keys" \
+    PRIVATE_BUILD_DESC="4.18.401.2 CL231174 release-keys"
 
 PRODUCT_COPY_FILES += \
     vendor/aicp/prebuilt/bootanimation/bootanimation_720_1280.zip:system/media/bootanimation-alt.zip
