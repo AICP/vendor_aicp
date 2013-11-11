3719d24515c7415152ff2fea2c8c32f250222823
Dirk Rettschlag <dirk.rettschlag@gmail.com>
2013-11-11 22:16:18 +0100

    add hammerhead to AOKP
    Signed-off-by: Dirk Rettschlag <dirk.rettschlag@gmail.com>

---
+++ products/hammerhead.mk
@@ -0,0 +1,24 @@
+# Inherit AOSP device configuration for hammerhead
+$(call inherit-product, device/lge/hammerhead/full_hammerhead.mk)
+
+# Inherit AOKP common bits
+$(call inherit-product, vendor/aokp/configs/common.mk)
+
+# Inherit GSM common stuff
+# TODO what to use for Hammerhead?
+#$(call inherit-product, vendor/aokp/configs/gsm.mk)
+
+# Overlay
+#PRODUCT_PACKAGE_OVERLAYS += vendor/aokp/overlay/hammerhead
+
+# Setup device specific product configuration
+PRODUCT_NAME := aokp_hammerhead
+PRODUCT_BRAND := google
+PRODUCT_DEVICE := hammerhead
+PRODUCT_MODEL := Nexus 5
+PRODUCT_MANUFACTURER := LGE
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=hammerhead BUILD_FINGERPRINT="google/hammerhead/hammerhead:4.4/KRT16M/893803:user/release-keys" PRIVATE_BUILD_DESC="hammerhead-user 4.4 KRT16M 893803 release-keys"
+
+PRODUCT_COPY_FILES += \
+    vendor/aokp/prebuilt/bootanimation/bootanimation_1080_1920.zip:system/media/bootanimation-alt.zip
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,4 @@
+add_lunch_combo aokp_hammerhead-userdebug
 add_lunch_combo aokp_mako-userdebug
 add_lunch_combo aokp_maguro-userdebug
 add_lunch_combo aokp_toro-userdebug
