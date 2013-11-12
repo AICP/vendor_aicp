89cfa4e3cafc0efc869fab98b28df7875438946d
audahadi <wan.audahadi@gmail.com>
2013-11-12 18:56:07 +0800

    fix product mk typo
    Change-Id: I84036bb115c149a28aa70a140890fbabc11574e8

--- products/n7000.mk
+++ products/n7000.mk
@@ -1,4 +1,4 @@
-# Inherit AOSP device configuration for galaxys2.
+# Inherit AOSP device configuration for n7000.
 $(call inherit-product, device/samsung/n7000/full_n7000.mk)
 
 # Inherit common product files.
@@ -7,23 +7,23 @@ $(call inherit-product, vendor/aicp/configs/common.mk)
 # Inherit GSM common stuff
 $(call inherit-product, vendor/aicp/configs/gsm.mk)
 
-# SGS2 overlay
+# N7000 overlay
 PRODUCT_PACKAGE_OVERLAYS += vendor/aicp/overlay/n7000
 
 # Setup device specific product configuration.
 PRODUCT_DEVICE := n7000
 PRODUCT_NAME := aicp_n7000
 PRODUCT_BRAND := Samsung
-PRODUCT_MODEL := GT-n7000
+PRODUCT_MODEL := GT-N7000
 
 # Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=GT-n7000 TARGET_DEVICE=GT-n7000 BUILD_FINGERPRINT=samsung/GT-n7000/GT-n7000:4.1.2/JZO54K/XXLPQ:user/release-keys PRIVATE_BUILD_DESC="GT-n7000-user 4.1.2 JZO54K XXLPQ release-keys"
-PRODUCT_RELEASE_NAME := GT-n7000
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=GT-N7000 TARGET_DEVICE=GT-N7000 BUILD_FINGERPRINT=samsung/GT-N7000/GT-N7000:4.0.3/IML74K/ZCLP6:user/release-keys PRIVATE_BUILD_DESC="GT-N7000-user 4.0.3 IML74K ZCLP6 release-keys"
+PRODUCT_RELEASE_NAME := GT-N7000
 
 # Copy n7000 specific prebuilt files
 PRODUCT_PACKAGES += \
     Thinkfree
 
 PRODUCT_COPY_FILES += \
-   vendor/aicp/prebuilt/bootanimation/bootanimation_480_800.zip:system/media/bootanimation-alt.zip
+   vendor/aicp/prebuilt/bootanimation/bootanimation_1280_800.zip:system/media/bootanimation-alt.zip
 
