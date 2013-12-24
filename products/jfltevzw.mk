b3ff383e623ff0af4428ca97918d41e7cd1f7aef
Brandon McAnsh <brandon.mcansh@gmail.com>
2013-12-24 21:20:12 +0100

    d2/jf:  set PRODUCT_GMS_CLIENTID_BASE Signed-off-by: Brandon McAnsh <brandon.mcansh@gmail.com>
    Change-Id: If2c653b5d8931d9180982c91cfdf1452033958b2

--- products/jfltevzw.mk
+++ products/jfltevzw.mk
@@ -22,6 +22,8 @@ PRODUCT_MANUFACTURER := Samsung
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltevzw TARGET_DEVICE=jfltevzw BUILD_FINGERPRINT="Verizon/jfltevzw/jfltevzw:4.2.2/JDQ39/I545VRUAME7:user/release-keys" PRIVATE_BUILD_DESC="jfltevzw-user 4.2.2 JDQ39 I545VRUAME7 release-keys"
 
+PRODUCT_GMS_CLIENTID_BASE := android-verizon
+
 # boot animation
 PRODUCT_COPY_FILES += \
        vendor/aokp/prebuilt/bootanimation/bootanimation_1080_1920.zip:system/media/bootanimation-alt.zip
