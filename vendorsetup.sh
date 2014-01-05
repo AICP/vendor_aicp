77ee62ca7a19cc6f38fe528c54b0a06243c459ac
KhasMek <Boushh@gmail.com>
2014-01-05 13:07:58 -0700

    Add grouper back to vendorsetup
     *le derp
    
    Change-Id: I7d6e8150bc043fe44b5159a1ef9e599ec9464c3f

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,6 +4,7 @@ add_lunch_combo aokp_flo-userdebug
 add_lunch_combo aokp_maguro-userdebug
 add_lunch_combo aokp_toro-userdebug
 add_lunch_combo aokp_manta-userdebug
+add_lunch_combo aokp_grouper-userdebug
 add_lunch_combo aokp_d2att-userdebug
 add_lunch_combo aokp_d2tmo-userdebug
 add_lunch_combo aokp_d2usc-userdebug
