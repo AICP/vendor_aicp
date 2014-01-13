AOKP/vendor_aokp

add Xperia Z1 and Z Ultra
Change-Id: I1a34eb77be75e4218ef27b3379ea55d95fb52f37
Signed-off-by: Arnav Gupta <championswimmer@aokp.co>

products/togari.mk
@@ -1,0 +1,16 @@
+# Inherit AOSP device configuration for honami
+$(call inherit-product-if-exists, device/sony/honami/aokp.mk)
+
+# honami overlay
+PRODUCT_PACKAGE_OVERLAYS += vendor/aokp/overlay/honami
+
+# Setup device specific product configuration.
+PRODUCT_NAME := aokp_honami
+PRODUCT_BRAND := Sony
+PRODUCT_DEVICE := honami
+PRODUCT_MODEL := Xperia Z Ultra
+PRODUCT_MANUFACTURER := Sony
+
+# boot animation
+PRODUCT_COPY_FILES += \
+    vendor/aokp/prebuilt/bootanimation/bootanimation_1080_1920.zip:system/media/bootanimation-alt.zip

AICP/vendor_aicp

products/togari.mk
@@ -1,0 +1,16 @@
+# Inherit AOSP device configuration for togari
+$(call inherit-product-if-exists, device/sony/togari/aokp.mk)
+
+# togari overlay
+PRODUCT_PACKAGE_OVERLAYS += vendor/aokp/overlay/togari
+
+# Setup device specific product configuration.
+PRODUCT_NAME := aokp_togari
+PRODUCT_BRAND := Sony
+PRODUCT_DEVICE := togari
+PRODUCT_MODEL := Xperia Z Ultra
+PRODUCT_MANUFACTURER := Sony
+
+# boot animation
+PRODUCT_COPY_FILES += \
+    vendor/aokp/prebuilt/bootanimation/bootanimation_1080_1920.zip:system/media/bootanimation-alt.zip