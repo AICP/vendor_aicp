bd7fae08dafe9bdd1461bb29aab2647c851957c9
LorDClockaN <davor@losinj.com>
2014-02-05 15:37:44 +0100

    FULL AICP versioning :P V2.0
    OTA ready!
    
    PS2: fix derp
    PS3: fix zip naming
    PS4: remove V from version
    PS5: Add V2.0 to commit name
    
    Change-Id: I0833cd495b0cf3c801e8e59aa59dd0b486e06678

--- configs/common_versions.mk
+++ configs/common_versions.mk
@@ -2,30 +2,48 @@
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_VERSION_TAGS=release-keys USER=android-build BUILD_UTC_DATE=$(shell date +"%s")
 
 DATE = $(shell vendor/aicp/tools/getdate)
-AOKP_BRANCH=kitkat
+AICP_BRANCH=kitkat
 
-ifneq ($(AOKP_BUILD),)
-    # AOKP_BUILD=<goo version int>/<build string>
-    PRODUCT_PROPERTY_OVERRIDES += \
-        ro.goo.developerid=aicp \
-        ro.goo.rom=aicp \
-        ro.goo.version=$(shell echo $(AOKP_BUILD) | cut -d/ -f1) \
-        ro.aicp.version=$(TARGET_PRODUCT)_$(AOKP_BRANCH)_$(shell echo $(AOKP_BUILD) | cut -d/ -f2)
+#ifneq ($(AICP_BUILD),)
+#    # AICP_BUILD=<goo version int>/<build string>
+#    PRODUCT_PROPERTY_OVERRIDES += \
+#        ro.goo.developerid=aicp \
+#        ro.goo.rom=aicp \
+#        ro.goo.version=$(shell echo $(AICP_BUILD) | cut -d/ -f1) \
+#        ro.aicp.version=$(TARGET_PRODUCT)_$(AICP_BRANCH)_$(shell echo $(AICP_BUILD) | cut -d/ -f2)
+#else
+#    ifneq ($(AICP_NIGHTLY),)
+#        # AICP_NIGHTLY=true
+#        PRODUCT_PROPERTY_OVERRIDES += \
+#            ro.aicp.version=$(TARGET_PRODUCT)_$(AICP_BRANCH)_nightly_$(DATE)
+#    else
+#        PRODUCT_PROPERTY_OVERRIDES += \
+#            ro.aicp.version=$(TARGET_PRODUCT)_$(AICP_BRANCH)_unofficial_$(DATE)
+#    endif
+#endif
+
+# AICP RELEASE VERSION
+AICP_VERSION_MAJOR = 2
+AICP_VERSION_MINOR = 0
+AICP_VERSION_MAINTENANCE =
+
+
+VERSION := $(AICP_VERSION_MAJOR).$(AICP_VERSION_MINOR)$(AICP_VERSION_MAINTENANCE)
+
+ifeq ($(DEVELOPER_VERSION),true)
+    AICP_VERSION := dev_$(BOARD)-$(VERSION)-$(shell date -u +%Y%m%d)
 else
-    ifneq ($(AOKP_NIGHTLY),)
-        # AOKP_NIGHTLY=true
-        PRODUCT_PROPERTY_OVERRIDES += \
-            ro.aicp.version=$(TARGET_PRODUCT)_$(AOKP_BRANCH)_nightly_$(DATE)
-    else
-        PRODUCT_PROPERTY_OVERRIDES += \
-            ro.aicp.version=$(TARGET_PRODUCT)_$(AOKP_BRANCH)_unofficial_$(DATE)
-    endif
+    AICP_VERSION := $(TARGET_PRODUCT)-$(AICP_BRANCH)-V$(VERSION)-$(shell date -u +%Y%m%d)
 endif
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.modversion=$(AICP_VERSION) \
+    ro.aicp.version=$(VERSION)
 
 # needed for statistics
 PRODUCT_PROPERTY_OVERRIDES += \
-        ro.aicp.branch=$(AOKP_BRANCH) \
-        ro.aicp.device=$(AOKP_PRODUCT)
+        ro.aicp.branch=$(AICP_BRANCH) \
+        ro.aicp.device=$(TARGET_PRODUCT)
 
 # Camera shutter sound property
 PRODUCT_PROPERTY_OVERRIDES += \
