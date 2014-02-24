4ba38a2f3eeafa4e2d1638b1417e0282954f7afe
Zips <hcolmenares@gmail.com>
2014-02-24 11:46:57 -0500

    Prepare for jfltexx
    Change-Id: I180f376af0713b37ebdaca748c2bf0e388615723

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -27,6 +27,7 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/jfltetmo.mk \
     $(LOCAL_DIR)/jflteusc.mk \
     $(LOCAL_DIR)/jfltevzw.mk \
+    $(LOCAL_DIR)/jfltexx.mk \
     $(LOCAL_DIR)/l900.mk \
     $(LOCAL_DIR)/ls980.mk \
     $(LOCAL_DIR)/m7.mk \
