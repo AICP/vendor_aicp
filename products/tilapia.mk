d88094ea69c0e1cdc232d95c8a0951746fc75dda
doc <doc.divxm@gmail.com>
2014-02-24 16:35:37 +0200

    tilapia: AICP'fied...
    Change-Id: I71d013fc1d98053d881431d64dc93c2a470940ce
    Signed-off-by: doc <doc.divxm@gmail.com>

--- products/tilapia.mk
+++ products/tilapia.mk
@@ -8,19 +8,24 @@ $(call inherit-product, vendor/aicp/configs/common_tablet.mk)
 $(call inherit-product, vendor/aicp/configs/gsm.mk)
 
 # Grouper Overlay
-PRODUCT_PACKAGE_OVERLAYS += vendor/aicp/overlay/grouper
+PRODUCT_PACKAGE_OVERLAYS += vendor/aicp/overlay/asus/grouper
 
-# Setup device specific product configuration.
-PRODUCT_NAME := aicp_tilapia
-PRODUCT_BRAND := google
-PRODUCT_DEVICE := tilapia
-PRODUCT_MODEL := Nexus 7
-PRODUCT_MANUFACTURER := asus
-
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=nakasig BUILD_FINGERPRINT="google/nakasig/tilapia:4.2.1/JOP40D/533553:user/release-keys" PRIVATE_BUILD_DESC="nakasig-user 4.2.1 JOP40D 533553 release-keys"
-
+# AICP common stuff
 PRODUCT_COPY_FILES += \
     vendor/aicp/prebuilt/bootanimation/bootanimation_1280_800.zip:system/media/bootanimation-alt.zip
 
+#MMS and Phone
 PRODUCT_PACKAGES += \
     Mms
+
+# Setup device specific product configuration.
+PRODUCT_NAME := aicp_tilapia
+PRODUCT_BRAND := Google
+PRODUCT_DEVICE := tilapia
+PRODUCT_MODEL := Nexus 7
+PRODUCT_MANUFACTURER := asus
+PRODUCT_RESTRICT_VENDOR_FILES := false
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=nakasig BUILD_FINGERPRINT="google/nakasig/tilapia:4.4.2/KOT49H/937116:user/release-keys" PRIVATE_BUILD_DESC="nakasig-user 4.4.2 KOT49H 937116 release-keys"
+
+
