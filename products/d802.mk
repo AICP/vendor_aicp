6be28722aadd56f5ec6aca5add57252301ede32e
Zips <hcolmenares@gmail.com>
2014-03-09 14:10:58 -0400

    d802: Welcome to AICP
    Change-Id: I330514f7b9978be660cfc0e483f85865715e43f6

--- products/d802.mk
+++ products/d802.mk
@@ -1,5 +1,5 @@
 # Inherit AICP device configuration for d802
-$(call inherit-product-if-exists, device/lge/d802/aokp.mk)
+$(call inherit-product-if-exists, device/lge/d802/aicp.mk)
 
 # d802 overlay
 # PRODUCT_PACKAGE_OVERLAYS += vendor/aicp/overlay/d802
