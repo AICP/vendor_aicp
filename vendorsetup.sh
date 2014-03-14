a16f2a9f651537740b3db44903890275f36989eb
Zips <hcolmenares@gmail.com>
2014-03-14 17:11:31 -0400

    vendorsetup: Grab devices from  build targets
    We only Build what we support
    
                PS1: Initial Setup
                PS2: Explain Why
    Change-Id: I6cbd6416505b021163e9959934c8d7d9026a29c0

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,45 +1,4 @@
-add_lunch_combo aicp_hammerhead-userdebug
-add_lunch_combo aicp_mako-userdebug
-add_lunch_combo aicp_flo-userdebug
-add_lunch_combo aicp_maguro-userdebug
-add_lunch_combo aicp_toro-userdebug
-add_lunch_combo aicp_manta-userdebug
-add_lunch_combo aicp_grouper-userdebug
-add_lunch_combo aicp_deb-userdebug
-add_lunch_combo aicp_d2lte-userdebug
-add_lunch_combo aicp_d800-userdebug
-add_lunch_combo aicp_d801-userdebug
-add_lunch_combo aicp_d802-userdebug
-add_lunch_combo aicp_dogo-userdebug
-add_lunch_combo aicp_endeavoru-userdebug
-add_lunch_combo aicp_find5-userdebug
-add_lunch_combo aicp_hlte-userdebug
-add_lunch_combo aicp_huashan-userdebug
-add_lunch_combo aicp_honami-userdebug
-add_lunch_combo aicp_i605-userdebug
-add_lunch_combo aicp_i9300-userdebug
-add_lunch_combo aicp_i9500-userdebug
-add_lunch_combo aicp_jfltecri-userdebug
-add_lunch_combo aicp_jfltecsp-userdebug
-add_lunch_combo aicp_jfltespr-userdebug
-add_lunch_combo aicp_jfltetmo-userdebug
-add_lunch_combo aicp_jflteusc-userdebug
-add_lunch_combo aicp_jfltevzw-userdebug
-add_lunch_combo aicp_jflte-userdebug
-add_lunch_combo aicp_l900-userdebug
-add_lunch_combo aicp_ls980-userdebug
-add_lunch_combo aicp_m7-userdebug
-add_lunch_combo aicp_m7spr-userdebug
-add_lunch_combo aicp_m7vzw-userdebug
-add_lunch_combo aicp_mint-userdebug
-add_lunch_combo aicp_n1-userdebug
-add_lunch_combo aicp_odin-userdebug
-add_lunch_combo aicp_pollux-userdebug
-add_lunch_combo aicp_polluxwindy-userdebug
-add_lunch_combo aicp_t0lte-userdebug
-add_lunch_combo aicp_t0lteatt-userdebug
-add_lunch_combo aicp_t0ltetmo-userdebug
-add_lunch_combo aicp_togari-userdebug
-add_lunch_combo aicp_tsubasa-userdebug
-add_lunch_combo aicp_vs980-userdebug
-add_lunch_combo aicp_yuga-userdebug
+for combo in $(curl -s  https://raw.github.com/AICP/vendor_jenkins/kitkat/aicp-build-targets | sed -e 's/#.*$//' | grep kitkat | awk {'print $2'})
+do
+    add_lunch_combo $combo
+done
