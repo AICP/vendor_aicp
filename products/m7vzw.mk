575411562bd3e4dd892e1d99ac5a2f4c4ab6cbf4
Matt Mower <mowerm@gmail.com>
2014-06-23 21:47:34 +0300

    m7vzw: update fingerprint from 4.10.605.3 OTA
    Change-Id: I435fcf3ce15537b5df6a1ada06b71e92ca1acf7a

--- products/m7vzw.mk
+++ products/m7vzw.mk
@@ -11,7 +11,8 @@ PRODUCT_COPY_FILES += \
     vendor/aicp/prebuilt/bootanimation/bootanimation_1080_1920.zip:system/media/bootanimation-alt.zip
 
 # Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=HTCOneVZW BUILD_FINGERPRINT="VERIZON/HTCOneVZW/m7wlv:4.2.2/JDQ39/221778.10:user/release-keys" PRIVATE_BUILD_DESC="1.10.605.10 CL221778 release-keys"
+    BUILD_FINGERPRINT="VERIZON/HTCOneVZW/m7wlv:4.4.2/KOT49H/334342.3:user/release-keys" \
+    PRIVATE_BUILD_DESC="4.10.605.3 CL334342 release-keys"
 
 # Device naming
 PRODUCT_DEVICE := m7vzw
