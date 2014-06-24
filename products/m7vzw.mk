0a4235e4488fd7982f3fcf595fafdf61394ae504
doc <doc.divxm@gmail.com>
2014-06-24 19:22:43 +0300

    vendor_aicp: Fix build for m7vzw
    Change-Id: I4b14377c856e5dcef97ce5dbf77b28ae523df67e
    Signed-off-by: doc <doc.divxm@gmail.com>

--- products/m7vzw.mk
+++ products/m7vzw.mk
@@ -11,8 +11,7 @@ PRODUCT_COPY_FILES += \
     vendor/aicp/prebuilt/bootanimation/bootanimation_1080_1920.zip:system/media/bootanimation-alt.zip
 
 # Set build fingerprint / ID / Product Name ect.
-    BUILD_FINGERPRINT="VERIZON/HTCOneVZW/m7wlv:4.4.2/KOT49H/334342.3:user/release-keys" \
-    PRIVATE_BUILD_DESC="4.10.605.3 CL334342 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=HTCOneVZW BUILD_FINGERPRINT="VERIZON/HTCOneVZW/m7wlv:4.4.2/KOT49H/334342.3:user/release-keys" PRIVATE_BUILD_DESC="4.10.605.3 CL334342 release-keys"
 
 # Device naming
 PRODUCT_DEVICE := m7vzw
