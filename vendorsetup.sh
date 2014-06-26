315d391b0d139fe8620b43f93e997093479347e2
Zips <hcolmenares@gmail.com>
2014-06-26 10:23:23 -0400

    Fix vendorsetup to pull from github
    Change-Id: I9931bfaa37de3ce602f24d836e8b6c857cbd49f1

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
-for combo in $(curl -s  https://raw.github.com/AICP/vendor_jenkins/kitkat/aicp-build-targets | sed -e 's/#.*$//' | grep kitkat | awk {'print $2'})
+for combo in $(curl -s  https://raw.githubusercontent.com/AICP/vendor_jenkins/kitkat/aicp-build-targets | sed -e 's/#.*$//' | grep kitkat | awk {'print $2'})
 do
     add_lunch_combo $combo
 done
