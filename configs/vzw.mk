b70a881e170f4e37f9649baeff4606019dda27e6
Zach <zachf714@gmail.com>
2014-06-29 00:08:16 +0300

    Verizon device permissions for unified devices
    these perms go into /system/vzw/ and get extracted as normal if verizon
    
    Change-Id: I6ff4cfd60dc6631fb89d8e6bce69492d76645e2f

--- configs/vzw.mk
+++ configs/vzw.mk
@@ -1,7 +1,16 @@
-#Permissions and Lib for Verizon Wireless Apps
+#Permissions and Lib for Verizon Wireless Apps based on Unified devices
+#GMS Client ID is only set on devices that are compiled specifically for vzw I.E. d2vzw not d2lte
+
+ifeq ($(VZW_UNIFIED),true)
+PRODUCT_COPY_FILES += \
+    vendor/aicp/prebuilt/vzw/app/VerizonSSO.apk:system/blobs/vzw/app/VerizonSSO.apk \
+    vendor/aicp/prebuilt/vzw/etc/permissions/com.vzw.hardware.ehrpd.xml:system/blobs/vzw/etc/permissions/com.vzw.hardware.ehrpd.xml \
+    vendor/aicp/prebuilt/vzw/etc/permissions/com.vzw.hardware.lte.xml:system/blobs/vzw/etc/permissions/com.vzw.hardware.lte.xml \
+    vendor/aicp/prebuilt/vzw/etc/permissions/com.vzw.vzwapnlib.xml:system/blobs/vzw/etc/permissions/com.vzw.vzwapnlib.xml \
+    vendor/aicp/prebuilt/vzw/lib/libmotricity.so:system/blobs/vzw/lib/libmotricity.so
+else
 PRODUCT_PACKAGES += \
-    VerizonSSO \
-    CellBroadcastReceiver
+    VerizonSSO
 
 PRODUCT_COPY_FILES += \
     vendor/aicp/prebuilt/vzw/etc/permissions/com.vzw.hardware.ehrpd.xml:system/etc/permissions/com.vzw.hardware.ehrpd.xml \
@@ -10,3 +19,4 @@ PRODUCT_COPY_FILES += \
     vendor/aicp/prebuilt/vzw/lib/libmotricity.so:system/lib/libmotricity.so
 
 PRODUCT_GMS_CLIENTID_BASE := android-verizon
+endif
--- products/hlte.mk
+++ products/hlte.mk
@@ -6,5 +6,11 @@ $(call inherit-product, vendor/aicp/configs/nfc_enhanced.mk)
 # Inherit some common AICP stuff.
 $(call inherit-product, vendor/aicp/configs/common.mk)
 
+# Unified device flag for Verizon permissions
+VZW_UNIFIED := true
+
+# Inherit common Verizon Wireless Perms and Lib
+$(call inherit-product, vendor/aicp/configs/vzw.mk)
+
 PRODUCT_DEVICE := hlte
 PRODUCT_NAME := aicp_hlte
