CyanogenMod/android_vendor_cm

CM11 Themes: Add feature for new themes.
Change-Id: I0da30606534c13a43de33bef08f79fbbb1b348d3

config/themes_common.mk
@@ -5,4 +5,5 @@ PRODUCT_PACKAGES += \
        com.tmobile.themes
 
 PRODUCT_COPY_FILES += \
-       vendor/cm/config/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml
+       vendor/cm/config/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml \
+       vendor/cm/config/permissions/org.cyanogenmod.theme.xml:system/etc/permissions/org.cyanogenmod.theme.xml

AICP/vendor_aicp

configs/themes_common.mk
@@ -5,4 +5,6 @@ PRODUCT_PACKAGES += \
        com.tmobile.themes 
 
 PRODUCT_COPY_FILES += \
-       vendor/aicp/prebuilt/common/etc/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml
+       vendor/aicp/prebuilt/common/etc/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml \
+       vendor/aicp/configs/permissions/org.cyanogenmod.theme.xml:system/etc/permissions/org.cyanogenmod.theme.xml
+