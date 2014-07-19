77f3af1458e8f8e6d12799f2e8b105d1d9ebaa9c
doc <doc.divxm@gmail.com>
2014-07-20 01:18:06 +0300

    vendor_aicp: Add Xperia Z1 Compact (codename: amami) to AICP
    Change-Id: I89ca421cfc2ac95eddfaff1122459f7d638cb9b1
    Signed-off-by: doc <doc.divxm@gmail.com>

---
+++ products/amami.mk
@@ -0,0 +1,16 @@
+# Inherit AOSP device configuration for amami
+$(call inherit-product-if-exists, device/sony/amami/aicp.mk)
+
+# amami overlay
+PRODUCT_PACKAGE_OVERLAYS += vendor/aicp/overlay/amami
+
+# Setup device specific product configuration.
+PRODUCT_NAME := aicp_amami
+PRODUCT_BRAND := Sony
+PRODUCT_DEVICE := amami
+PRODUCT_MODEL := Xperia Z1 compact
+PRODUCT_MANUFACTURER := Sony
+
+# boot animation
+PRODUCT_COPY_FILES += \
+    vendor/aokp/prebuilt/bootanimation/bootanimation_720.zip:system/media/bootanimation.zip
