4b2111a166f3baf895c4a19071feefca42e5ead7
doc <doc.divxm@gmail.com>
2014-07-20 03:30:24 +0300

    vendor_aicp: Add Amazon KFire (otterx) to AICP
    Change-Id: If5c295ee812dfd373113469b877605117adbd786

---
+++ products/otterx.mk
@@ -0,0 +1,20 @@
+# Copyright (C) 2013 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Inherit device configuration for Kindle Fire
+$(call inherit-product, device/amazon/otter-common/aicp.mk)
+$(call inherit-product, device/amazon/otterx/full_otterx.mk)
+
+PRODUCT_NAME := aicp_otterx
+PRODUCT_RELEASE_NAME := KFire
