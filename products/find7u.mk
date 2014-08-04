1b978830f780afb373bdbbd52c0c65d03c9233e0
n3ocort3x <peter.hofinger@gmail.com>
2014-08-04 14:34:01 +0200

    correct names find7 -> find7u
    Change-Id: I7d5145c901630bd66579e5a1a9cb0a6efee46a5a

--- products/find7u.mk
+++ products/find7u.mk
@@ -13,8 +13,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGE_OVERLAYS += vendor/aicp/overlay/find7
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := find7
-PRODUCT_NAME := aicp_find7
+PRODUCT_DEVICE := find7u
+PRODUCT_NAME := aicp_find7u
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=4.3/JLS36C/1390381364:user/release-keys PRIVATE_BUILD_DESC="msm8974-user 4.3 JLS36C eng.root.20140122.170107 release-keys"
 
