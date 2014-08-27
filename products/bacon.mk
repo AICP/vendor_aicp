792692c3c9eff9caf97d22a59089c69822dc29b5
MURA Stefano [STELIX PippoX3] <ssspinni@gmail.com>
2014-08-27 22:12:21 +0200

    AICP vendor - Bacon update
    Change-Id: I3826f998b503257e24b867cf15f995f75bc52028
    Signed-off-by: MURA Stefano [STELIX PippoX3] <ssspinni@gmail.com>

--- products/bacon.mk
+++ products/bacon.mk
@@ -10,11 +10,22 @@ $(call inherit-product, vendor/aicp/configs/common.mk)
 PRODUCT_COPY_FILES += \
     vendor/aokp/prebuilt/bootanimation/bootanimation_1080_1920.zip:system/media/bootanimation.zip
 
-PRODUCT_PACKAGE_OVERLAYS += vendor/aokp/overlay/bacon
+PRODUCT_PACKAGE_OVERLAYS += vendor/aicp/overlay/bacon
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := bacon
 PRODUCT_NAME := aicp_bacon
+PRODUCT_MANUFACTURER := OnePlus
+PRODUCT_MODEL := One
+
+PRODUCT_GMS_CLIENTID_BASE := android-oneplus
+
+PRODUCT_BRAND := oneplus
+TARGET_VENDOR_PRODUCT_NAME := bacon
+TARGET_VENDOR_DEVICE_NAME := A0001
+PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=A0001
+
+TARGET_CONTINUOUS_SPLASH_ENABLED := true
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
         BUILD_FINGERPRINT=oneplus/bacon/A0001:4.4.2/KVT49L/XNPH25R:user/release-keys \
