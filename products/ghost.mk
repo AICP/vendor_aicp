ceb5a64154fc5d21144dd32296e4bc080bae8bd9
John Brewer <lycanrom@hotmail.com>
2014-09-24 10:26:02 +0300

    vendor_aicp: Use proper ghost.mk
    Change-Id: I319a9fefc0e695c6bbb4bd1c84320d19fa101ade

--- products/ghost.mk
+++ products/ghost.mk
@@ -1,10 +1,17 @@
+# Inherit AOSP device configuration for xt1060
 $(call inherit-product, device/motorola/ghost/full_ghost.mk)
-
-# Inherit some common AICP stuff.
-$(call inherit-product, vendor/aicp/configs/gsm.mk)
 
 # Enhanced NFC
 $(call inherit-product, vendor/aicp/configs/nfc_enhanced.mk)
 
-PRODUCT_RELEASE_NAME := MOTO X
+# Inherit common product files.
+$(call inherit-product, vendor/aicp/configs/common.mk)
+
+# Unified device flag for Verizon permissions
+VZW_UNIFIED := true
+
+# Inherit common Verizon Wireless Perms and Lib
+$(call inherit-product, vendor/aicp/configs/vzw.mk)
+
+# Setup device specific product configuration.
 PRODUCT_NAME := aicp_ghost
