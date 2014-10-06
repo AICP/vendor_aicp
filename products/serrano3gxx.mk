53231c4cb1b66a8ab85f09cc86711a69d25b9d1d
doc <doc.divxm@gmail.com>
2014-10-07 01:15:27 +0300

    vendor_aicp: Update build props for serrano group
    Change-Id: I732c0e289717220589cedb12fe6ad3f525f7ebd7

--- products/serrano3gxx.mk
+++ products/serrano3gxx.mk
@@ -1,9 +1,17 @@
+# Inherit AOSP device configuration for serrano3gxx.
+$(call inherit-product, device/samsung/serrano3gxx/full_serrano3gxx.mk)
+
+# Inherit common product files.
+$(call inherit-product, vendor/aicp/configs/common.mk)
+
 # Inherit some common AICP stuff.
 $(call inherit-product, vendor/aicp/configs/gsm.mk)
 
-$(call inherit-product, device/samsung/serrano3gxx/full_serrano3gxx.mk)
-
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=serrano3gxx TARGET_DEVICE=serrano3g BUILD_FINGERPRINT="samsung/serrano3gxx/serrano3g:4.4.2/KOT49H/I9190XXUCNG1:user/release-keys" PRIVATE_BUILD_DESC="serrano3gxx-user 4.4.2 KOT49H I9190XXUCNG1 release-keys"
 
-PRODUCT_DEVICE := serrano3gxx
+# Setup device specific product configuration.
 PRODUCT_NAME := aicp_serrano3gxx
+PRODUCT_BRAND := samsung
+PRODUCT_DEVICE := serrano3gxx
+PRODUCT_MODEL := GT-I9190
+PRODUCT_MANUFACTURER := samsung
--- products/serranoltexx.mk
+++ products/serranoltexx.mk
@@ -1,10 +1,18 @@
+# Inherit AOSP device configuration for serranoltexx.
+$(call inherit-product, device/samsung/serranoltexx/full_serranoltexx.mk)
+
+# Inherit common product files.
+$(call inherit-product, vendor/aicp/configs/common.mk)
+
 # Inherit some common AICP stuff.
 $(call inherit-product, vendor/aicp/configs/gsm.mk)
-
-$(call inherit-product, device/samsung/serranoltexx/full_serranoltexx.mk)
 
 # Enhanced NFC
 $(call inherit-product, vendor/aicp/configs/nfc_enhanced.mk)
 
-PRODUCT_DEVICE := serranoltexx
+# Setup device specific product configuration.
 PRODUCT_NAME := aicp_serranoltexx
+PRODUCT_BRAND := samsung
+PRODUCT_DEVICE := serranoltexx
+PRODUCT_MODEL := GT-I9195
+PRODUCT_MANUFACTURER := samsung
