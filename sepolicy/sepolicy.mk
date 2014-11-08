3daf030af1a6922834ee43d3433687b6526957b9
LorDClockaN <davor@losinj.com>
2014-11-08 18:05:22 +0100

    Update sepolicy from CM12
    Change-Id: I3da1db179a036c65f75cdf7278e03961bba6152a

--- sepolicy/sepolicy.mk
+++ sepolicy/sepolicy.mk
@@ -9,7 +9,10 @@ BOARD_SEPOLICY_UNION += \
     file.te \
     file_contexts \
     genfs_contexts \
-    installd.te \
     seapp_contexts \
+    installd.te \
+    netd.te \
+    system.te \
+    ueventd.te \
     vold.te \
     mac_permissions.xml
