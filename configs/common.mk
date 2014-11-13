b9de74e6b52a066eec8ff1b1ac90b903590893a3
LorDClockaN <davor@losinj.com>
2014-11-13 20:19:20 +0100

    Build Apollo
    Change-Id: I24173a501f293ee924f744cc9b6ecb70dd5b8e97

--- configs/common.mk
+++ configs/common.mk
@@ -21,7 +21,8 @@ PRODUCT_PACKAGES += \
     PhotoPhase \
     SlimFileManager \
     SlimLauncher \
-    DashClock
+    DashClock \
+    Apollo
 
 ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
 PRODUCT_PROPERTY_OVERRIDES += \
