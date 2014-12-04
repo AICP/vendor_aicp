a53fa402c909879d459016060d4cb33ae1633a70
doc <doc.divxm@gmail.com>
2014-12-04 11:55:06 +0200

    ghost: Do not inherit from vzw.mk anymore
    ** Sorry guys.. Totally forgot to delete these lines..
    
    Change-Id: I552c843420ee2a7c8fff48eafa4ebd65692598af

--- products/ghost.mk
+++ products/ghost.mk
@@ -1,17 +1,12 @@
 # Inherit AOSP device configuration for xt1060
 $(call inherit-product, device/motorola/ghost/full_ghost.mk)
 
-# Enhanced NFC
-$(call inherit-product, vendor/aicp/configs/nfc_enhanced.mk)
-
 # Inherit common product files.
 $(call inherit-product, vendor/aicp/configs/common.mk)
 
-# Unified device flag for Verizon permissions
-VZW_UNIFIED := true
-
-# Inherit common Verizon Wireless Perms and Lib
-$(call inherit-product, vendor/aicp/configs/vzw.mk)
+# Enhanced NFC
+$(call inherit-product, vendor/aicp/configs/nfc_enhanced.mk)
 
 # Setup device specific product configuration.
+PRODUCT_RELEASE_NAME := MOTO X
 PRODUCT_NAME := aicp_ghost
