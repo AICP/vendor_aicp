fd239b2d0668bebc1fc4d5492b8b9797d4d0de77
dcd <dcd1182@gmail.com>
2014-12-11 01:01:45 +0200

    vendor_aicp: Update fingerprint for jfltetmo
    Change-Id: I060aeb3c8d479a9c503ab95cd05ec061f78e1484

--- products/jfltetmo.mk
+++ products/jfltetmo.mk
@@ -1,19 +1,14 @@
-$(call inherit-product, device/samsung/jfltetmo/full_jfltetmo.mk)
+$(call inherit-product, vendor/aicp/configs/common.mk)
 
-# Enhanced NFC
 $(call inherit-product, vendor/aicp/configs/nfc_enhanced.mk)
 
-# Inherit some common stuff.
-$(call inherit-product, vendor/aicp/configs/gsm.mk)
-
-# Inherit some common stuff.
-$(call inherit-product, vendor/aicp/configs/common.mk)
+$(call inherit-product, device/samsung/jfltetmo/full_jfltetmo.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
   PRODUCT_NAME=jfltetmo \
   TARGET_DEVICE=jfltetmo \
-  BUILD_FINGERPRINT="samsung/jfltetmo/jfltetmo:4.4.4/KTU84P/M919UVUFNH7:user/release-keys" \
-  PRIVATE_BUILD_DESC="jfltetmo-user 4.4.4 KTU84P M919UVUFNH7 release-keys"
+  BUILD_FINGERPRINT="samsung/jfltetmo/jfltetmo:4.4.4/KTU84P/M919UVUFNK2:user/release-keys" \
+  PRIVATE_BUILD_DESC="jfltetmo-user 4.4.4 KTU84P M919UVUFNK2 release-keys"
 
 PRODUCT_NAME := aicp_jfltetmo
 PRODUCT_DEVICE := jfltetmo
