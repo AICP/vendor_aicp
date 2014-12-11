cb24f9f5ab4ba30253ff451780ba42cf270821a2
OmarEinea <eineao@gmail.com>
2014-12-11 16:10:25 +0100

    vendorsetup: Grab lollipop supported devices
    Signed-off-by: OmarEinea <eineao@gmail.com>
    
    Change-Id: I8b7b2e17dfb9756ca7b8f5579384dc02cb7bf857

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
-for combo in $(curl -s  https://raw.githubusercontent.com/AICP/vendor_jenkins/kitkat/aicp-build-targets | sed -e 's/#.*$//' | grep kitkat | awk {'print $2'})
+for combo in $(curl -s  https://raw.githubusercontent.com/AICP/vendor_jenkins/lp5.0/aicp-build-targets | sed -e 's/#.*$//' | grep lp5.0 | awk {'print $2'})
 do
     add_lunch_combo $combo
 done
