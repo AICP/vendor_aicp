95197b82a8c357ea0b02ab157aaa8364b27dec08
doc <doc.divxm@gmail.com>
2014-12-22 11:21:45 +0200

    vendor_aicp: Bring jfltespr to AICP
    Change-Id: Ib604aa46d4af4a452bb41d4dd41a79dec59b28c6

--- products/jfltespr.mk
+++ products/jfltespr.mk
@@ -1,8 +1,10 @@
 $(call inherit-product, vendor/aicp/configs/common.mk)
 
+# Enhanced NFC
 $(call inherit-product, vendor/aicp/configs/nfc_enhanced.mk)
 
 $(call inherit-product, device/samsung/jfltespr/full_jfltespr.mk)
+
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=jfltespr \
@@ -15,6 +17,6 @@ PRODUCT_GMS_CLIENTID_BASE := android-samsung
 PRODUCT_NAME := aicp_jfltespr
 PRODUCT_DEVICE := jfltespr
 
-# boot animation
+#bootanimation
 PRODUCT_COPY_FILES += \
-       vendor/aicp/prebuilt/bootanimation/bootanimation_1080_1920.zip:system/media/bootanimation-alt.zip
+    vendor/aicp/prebuilt/bootanimation/bootanimation_1080_1920.zip:system/media/bootanimation-alt.zip
