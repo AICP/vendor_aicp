947e4f4bb587617537e3cedd8cbc29de37df6c5d
doc <doc.divxm@gmail.com>
2014-12-24 18:52:48 +0200

    find7: Update inheritance order
    Change-Id: I07f139f513eb2447c11689bb1be7a528c8a458f7

--- products/find7ext.mk
+++ products/find7ext.mk
@@ -1,20 +1,11 @@
-$(call inherit-product, device/oppo/find7ext/full_find7ext.mk)
-
-# Inherit GSM common stuff
-$(call inherit-product, vendor/aicp/configs/gsm.mk)
-
-# Inherit some common AOKP stuff
 $(call inherit-product, vendor/aicp/configs/common.mk)
 
-# boot animation
-PRODUCT_COPY_FILES += \
-    vendor/aokp/prebuilt/bootanimation/bootanimation_1080.zip:system/media/bootanimation.zip
+# Enhanced NFC
+$(call inherit-product, vendor/aicp/configs/nfc_enhanced.mk)
 
-PRODUCT_PACKAGE_OVERLAYS += vendor/aicp/overlay/find7
 
-# Inline kernel
-TARGET_KERNEL_SOURCE := kernel/oppo/find7
-TARGET_KERNEL_CONFIG := custom_find7_defconfig
+$(call inherit-product, device/oppo/find7ext/full_find7ext.mk)
+
 
 PRODUCT_NAME := aicp_find7ext
 PRODUCT_DEVICE := find7
@@ -25,3 +16,7 @@ PRODUCT_MANUFACTURER := Oppo
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT=4.4.2/KVT49L/1390465867:user/release-keys \
     PRIVATE_BUILD_DESC="msm8974-user 4.4.2 KVT49L eng.root.20141017.144947 release-keys"
+
+# boot animation
+PRODUCT_COPY_FILES += \
+    vendor/aicp/prebuilt/bootanimation/bootanimation_1080_1920.zip:system/media/bootanimation.zip
