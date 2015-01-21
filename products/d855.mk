b1fa5dfb8cb0efa3ff15c44b2454b6056c17f7bc
AngelBob <cambercreak@gmail.com>
2015-01-21 22:25:57 +0100

    D855: Update fingerprint to LGD855AT-00-V20f-GLOBAL-COM-DEC-22-2014+0
    Change-Id: I018bd0d8d77b1e45cc274dbf021e1f972a5f614f

--- products/d855.mk
+++ products/d855.mk
@@ -18,8 +18,8 @@ PRODUCT_MANUFACTURER := lge
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g3" \
     PRODUCT_NAME="g3_global_com" \
-    BUILD_FINGERPRINT="lge/g3_global_com/g3:4.4.2/KVT49L.A1404795316/1404795316:user/release-keys" \
-    PRIVATE_BUILD_DESC="g3_global_com-user 4.4.2 KVT49L.A1404795316 1404795316 release-keys"
+    BUILD_FINGERPRINT="lge/g3_global_com/g3:5.0/LRX21R.A1419207951/1419207951:user/release-keys" \
+    PRIVATE_BUILD_DESC="g3_global_com-user 5.0 LRX21R.A1419207951 1419207951 release-keys"
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2560
