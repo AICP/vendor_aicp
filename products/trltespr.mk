9a002f85a6fd0e79f68ba6d00734c31e1cf09614
spleef <spleef79@gmail.com>
2015-02-01 00:50:03 +0200

    trltespr: Add AICP support
    Change-Id: Ib1b3e4037b37ba64f670994c07285cbcc1b3a56b

---
+++ products/trltespr.mk
@@ -0,0 +1,36 @@
+#
+# Copyright (C) 2014 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+$(call inherit-product, vendor/aicp/configs/common.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+
+$(call inherit-product, device/samsung/trltespr/full_trltespr.mk)
+
+
+PRODUCT_NAME := trltespr
+PRODUCT_DEVICE := trltespr
+PRODUCT_NAME := aicp_trltespr
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=trltespr BUILD_FINGERPRINT=samsung/trltespr/trltespr:4.4.4/KTU84P/N910PVPU1ANK2:user/release-keys PRIVATE_BUILD_DESC="trltespr-user 5.0.2 LRX22G N910PVPU1ANK2 release-keys"
+
+#Bootanimation
+TARGET_SCREEN_HEIGHT := 2560
+TARGET_SCREEN_WIDTH := 1440
+-include vendor/aicp/configs/bootanimation.mk
