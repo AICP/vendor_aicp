9d6c648324751a99ea3c4bb17142e2ef672b9e18
OmarEinea <eineao@gmail.com>
2015-02-02 14:05:24 +0100

    togari: Prepare for official support
    Change-Id: I5d2a08c598cddd203f8f857af54525c1f789d112

--- products/togari.mk
+++ products/togari.mk
@@ -1,15 +1,16 @@
-# Inherit AOSP device configuration for togari
-$(call inherit-product-if-exists, device/sony/togari/aicp.mk)
+# Inherit CM common Phone stuff.
+$(call inherit-product, vendor/aicp/configs/common.mk)
 
-# togari overlay
-PRODUCT_PACKAGE_OVERLAYS += vendor/aicp/overlay/togari
+# Enhanced NFC
+$(call inherit-product, vendor/aicp/configs/nfc_enhanced.mk)
+
+$(call inherit-product, device/sony/togari/full_togari.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6833 BUILD_FINGERPRINT=Sony/C6833/C6833:4.4.4/14.4.A.0.157/ev__jQ:user/release-keys PRIVATE_BUILD_DESC="C6833-user 4.4.4 14.4.A.0.157 ev__jQ release-keys"
 
 # Setup device specific product configuration.
 PRODUCT_NAME := aicp_togari
-PRODUCT_BRAND := Sony
 PRODUCT_DEVICE := togari
-PRODUCT_MODEL := Xperia Z Ultra
-PRODUCT_MANUFACTURER := Sony
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1920
