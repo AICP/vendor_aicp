7d7d4f03aa72e408830f9ec7c40214f54b05f80c
David Hays <dhays90@gmail.com>
2015-02-17 20:40:21 +0200

    m7spr: Update fingerprint for 5.0.2(LRX22F)
    Change-Id: I9a0551bbdc6c07121ddda08543740201806c8dda

--- products/m7spr.mk
+++ products/m7spr.mk
@@ -7,7 +7,11 @@ $(call inherit-product, vendor/aicp/configs/nfc_enhanced.mk)
 $(call inherit-product, device/htc/m7spr/full_m7spr.mk)
 
 # Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=m7wls BUILD_FINGERPRINT="htc/sprint_wwe/m7wls:4.4.2/KOT49H/337390.3:user/release-keys" PRIVATE_BUILD_DESC="5.03.651.3 CL337390 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=sprint_wwe \
+    BUILD_ID=LRX22F \
+    BUILD_FINGERPRINT="htc/sprint_wwe/m7wls:5.0.2/LRX22F/461956.2:user/release-keys" \
+    PRIVATE_BUILD_DESC="6.16.651.2 CL461956 release-keys"
 
 # Device naming
 PRODUCT_DEVICE := m7spr
