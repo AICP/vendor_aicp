1dd8359925e39c5099f3a9688bdc339e911b78db
Jonathon Fitch <lightman1@virginmedia.com>
2015-02-23 22:22:25 +0200

    m7: Update build.prop entries to Lollipop GPE strings
    Change-Id: Ieb3a07aa10abd5280d68c647995f29ea0d61a22a

--- products/m7.mk
+++ products/m7.mk
@@ -6,7 +6,7 @@ $(call inherit-product, vendor/aicp/configs/nfc_enhanced.mk)
 $(call inherit-product, device/htc/m7/full_m7.mk)
 
 
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_ID=KTU84L.H4 BUILD_FINGERPRINT="htc/m7_google/m7:4.4.3/KTU84L.H4/347097:user/release-keys" PRIVATE_BUILD_DESC="4.07.1700.4 CL347097 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_ID=LRX22C.H3 BUILD_FINGERPRINT="htc/m7_google/m7:5.0.1/LRX22C.H3/434277:user/release-keys" PRIVATE_BUILD_DESC="5.11.1700.3 CL434277 release-keys"
 
 PRODUCT_NAME := aicp_m7
 PRODUCT_DEVICE := m7
