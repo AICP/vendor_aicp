ef9d59d56517439a1132b4c696e90f367ce9e68f
doc <doc.divxm@gmail.com>
2015-03-11 13:13:23 +0200

    Revert "Add custom AAPT config" for serrano group
    Change-Id: I9fc179bf93253903a4162083f656fe37697aa790

--- products/serrano3gxx.mk
+++ products/serrano3gxx.mk
@@ -4,10 +4,6 @@ $(call inherit-product, vendor/aicp/configs/common.mk)
 # Inherit AOSP device configuration for serrano3gxx.
 $(call inherit-product, device/samsung/serrano3gxx/full_serrano3gxx.mk)
 
-# Device uses high-density artwork where available
-PRODUCT_AAPT_CONFIG := normal hdpi
-PRODUCT_AAPT_PREF_CONFIG := hdpi
-
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=serrano3gxx TARGET_DEVICE=serrano3g BUILD_FINGERPRINT="samsung/serrano3gxx/serrano3g:4.4.2/KOT49H/I9190XXUCNG1:user/release-keys" PRIVATE_BUILD_DESC="serrano3gxx-user 4.4.2 KOT49H I9190XXUCNG1 release-keys"
 
 PRODUCT_DEVICE := serrano3gxx
