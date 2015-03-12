35c34cec1e57cb5dab328925f6481a6acf5ade55
doc <doc.divxm@gmail.com>
2015-03-12 15:39:51 +0200

    vendor_aicp: Update i9300.mk
    Change-Id: If00f6dcb3840451c45e9e6871b3f2435104c945a

--- products/i9300.mk
+++ products/i9300.mk
@@ -1,17 +1,23 @@
-# Inherit common product files.
+# Inherit from our custom product configuration
 $(call inherit-product, vendor/aicp/configs/common.mk)
 
-# Inherit AOSP device configuration for i9300.
+# Enhanced NFC
+$(call inherit-product, vendor/aicp/configs/nfc_enhanced.mk)
+
+# Inherit device configuration
 $(call inherit-product, device/samsung/i9300/full_i9300.mk)
 
 
-# Setup device specific product configuration.
+# Discard inherited values and use our own instead.
 PRODUCT_NAME := aicp_i9300
-PRODUCT_BRAND := samsung
 PRODUCT_DEVICE := i9300
-PRODUCT_MODEL := GT-I9300
+PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
+PRODUCT_MODEL := GT-I9300
 
+
+
+# Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=m0xx TARGET_DEVICE=m0 BUILD_FINGERPRINT="samsung/m0xx/m0:4.3/JSS15J/I9300XXUGMJ9:user/release-keys" PRIVATE_BUILD_DESC="m0xx-user 4.3 JSS15J I9300XXUGMJ9 release-keys"
 PRODUCT_RELEASE_NAME := GT-I9300
 
