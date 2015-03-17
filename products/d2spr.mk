16f2c9ab51e93f7a67d17a0f585c72b0821258ad
dcd <dcd1182@gmail.com>
2015-03-17 13:24:05 +0200

    d2spr: Update fingerprint to L710VPUDNJ2
    Change-Id: I303938e33b64ec485c13912f940874ec3f9d15f4

--- products/d2spr.mk
+++ products/d2spr.mk
@@ -8,8 +8,8 @@ $(call inherit-product, device/samsung/d2spr/full_d2spr.mk)
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=d2spr \
     TARGET_DEVICE=d2spr \
-    BUILD_FINGERPRINT="samsung/d2spr/d2spr:4.4.2/KOT49H/L710VPUDND8:user/release-keys" \
-    PRIVATE_BUILD_DESC="d2spr-user 4.4.2 KOT49H L710VPUDND8 release-keys"
+    BUILD_FINGERPRINT="samsung/d2spr/d2spr:4.4.2/KOT49H/L710VPUDNJ2:user/release-keys" \
+    PRIVATE_BUILD_DESC="d2spr-user 4.4.2 KOT49H L710VPUDNJ2 release-keys"
 
 PRODUCT_NAME := aicp_d2spr
 PRODUCT_DEVICE := d2spr
