c5b87d270c81d20510ed98925f3039a87b0245ae
LorDClockaN <davor@losinj.com>
2015-03-22 16:15:32 +0100

    lp5.0 -> lp5.1
    Change-Id: Iabded0f45a4bf10198c3ea3c6c114b5f329db124

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
-for combo in $(curl -s  https://raw.githubusercontent.com/AICP/vendor_jenkins/lp5.0/aicp-build-targets | sed -e 's/#.*$//' | grep lp5.0 | awk {'print $2'})
+for combo in $(curl -s  https://raw.githubusercontent.com/AICP/vendor_jenkins/lp5.1/aicp-build-targets | sed -e 's/#.*$//' | grep lp5.1 | awk {'print $2'})
 do
     add_lunch_combo $combo
 done
