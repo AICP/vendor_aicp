875670a1d2ed2df4e56d154812a0e7359cde61fd
doc <doc.divxm@gmail.com>
2015-03-25 00:37:26 +0200

    grouper/tilapia: Update fingerprints to LMY47D
    Change-Id: If4f9558263e46b3ae97e973e1aa2fff083a79ace

--- products/grouper.mk
+++ products/grouper.mk
@@ -20,7 +20,7 @@ PRODUCT_MANUFACTURER := asus
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
 #Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=nakasi BUILD_FINGERPRINT="google/nakasi/grouper:5.0/LRX21P/1570855:user/release-keys" PRIVATE_BUILD_DESC="nakasi-user 5.0 LRX21P 1570855 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=nakasi BUILD_FINGERPRINT="google/nakasi/grouper:5.1/LMY47D/1743759:user/release-keys" PRIVATE_BUILD_DESC="nakasi-user 5.1 LMY47D 1743759 release-keys"
 
 
 # Boot animation
