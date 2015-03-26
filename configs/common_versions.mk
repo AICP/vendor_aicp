e5a3545deec3a64c38dd1b4777698f915d9cce26
LorDClockaN <davor@losinj.com>
2015-03-26 15:43:59 +0100

    Add 5.1 to zip name
    Change-Id: I2f86049f82a26b4e1d9cdaa2164e3cf16a628bc5

--- configs/common_versions.mk
+++ configs/common_versions.mk
@@ -2,7 +2,7 @@
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_VERSION_TAGS=release-keys USER=android-build BUILD_UTC_DATE=$(shell date +"%s")
 
 DATE = $(shell vendor/aicp/tools/getdate)
-AICP_BRANCH=lp
+AICP_BRANCH=lp5.1
 
 # AICP RELEASE VERSION
 AICP_VERSION_MAJOR = 8
