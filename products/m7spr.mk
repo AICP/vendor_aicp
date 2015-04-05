3e36035026df801eb3c2a54d5e00781383a56325
Kyle Laker <10kylelaker@gmail.com>
2015-04-05 21:16:16 +0300

    m7spr: Update Fingerprint for LRX22G
    Update the finger print and description to match the values provided
    in the latest OTA for the device
    
    Change-Id: Ibe9215594049684b6a9d25f37b5c05a03fc4c2bf

--- products/m7spr.mk
+++ products/m7spr.mk
@@ -9,9 +9,9 @@ $(call inherit-product, device/htc/m7spr/full_m7spr.mk)
 # Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=sprint_wwe \
-    BUILD_ID=LRX22F \
-    BUILD_FINGERPRINT="htc/sprint_wwe/m7wls:5.0.2/LRX22F/461956.2:user/release-keys" \
-    PRIVATE_BUILD_DESC="6.16.651.2 CL461956 release-keys"
+    BUILD_ID=LRX22G \
+    BUILD_FINGERPRINT="htc/sprint_wwe/m7wls:5.0.2/LRX22G/499067.6:user/release-keys" \
+    PRIVATE_BUILD_DESC="6.23.651.2 CL499067 release-keys"
 
 # Device naming
 PRODUCT_DEVICE := m7spr
