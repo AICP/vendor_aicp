fae347be1d30d1165185e04e4c3ca47f85e3186b
doc <doc.divxm@gmail.com>
2015-04-15 11:32:54 +0300

    hammerheadcaf: Update fingerprint to LMY47I
    Change-Id: I18d56a91bd59beecf0014039d5900a7eeb8af46b

--- products/hammerheadcaf.mk
+++ products/hammerheadcaf.mk
@@ -9,13 +9,16 @@ $(call inherit-product, device/lge/hammerheadcaf/full_hammerhead.mk)
 
 
 # Setup device specific product configuration
+PRODUCT_DEVICE := hammerheadcaf
 PRODUCT_NAME := aicp_hammerheadcaf
 PRODUCT_BRAND := google
-PRODUCT_DEVICE := hammerheadcaf
-PRODUCT_MODEL := Nexus 5
+PRODUCT_MODEL := Nexus 5 CAF
 PRODUCT_MANUFACTURER := LGE
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=hammerhead BUILD_FINGERPRINT=google/hammerhead/hammerhead:5.0.1/LRX22C/1602158:user/release-keys PRIVATE_BUILD_DESC="hammerhead-user 5.0.1 LRX22C 1602158 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=hammerhead \
+    BUILD_FINGERPRINT=google/hammerhead/hammerhead:5.1/LMY47I/1767468:user/release-keys \
+    PRIVATE_BUILD_DESC="hammerhead-user 5.1 LMY47I 1767468 release-keys"
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1920
