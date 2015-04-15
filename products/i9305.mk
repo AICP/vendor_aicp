93d0133088eb9432eea226f09996347ae315ddb5
doc <doc.divxm@gmail.com>
2015-04-15 12:37:33 +0300

    i9305: Updates for AICP 5.1
    Change-Id: I6cd6e41cd548c8ed2c647fcc1c086abaac84990c

--- products/i9305.mk
+++ products/i9305.mk
@@ -1,3 +1,6 @@
+# Release name
+PRODUCT_RELEASE_NAME := i9305
+
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/aicp/configs/common.mk)
 
@@ -7,13 +10,8 @@ $(call inherit-product, vendor/aicp/configs/nfc_enhanced.mk)
 # Inherit device configuration
 $(call inherit-product, device/samsung/i9305/full_i9305.mk)
 
-# Boot animation
-TARGET_SCREEN_HEIGHT := 1280
-TARGET_SCREEN_WIDTH := 720
--include vendor/aicp/configs/bootanimation.mk
-
-# Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=m3xx TARGET_DEVICE=m3 BUILD_FINGERPRINT="samsung/m3xx/m3:4.1.2/JZO54K/I9305XXBMA6:user/release-keys" PRIVATE_BUILD_DESC="m3xx-user 4.1.2 JZO54K I9305XXBMA6 release-keys"
+# Configure dalvik heap
+$(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)vvvvvvvvv
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := i9305
@@ -21,3 +19,15 @@ PRODUCT_NAME := aicp_i9305
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := GT-I9305
 PRODUCT_MANUFACTURER := samsung
+
+# Set build fingerprint / ID / Product Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += \
+	PRODUCT_NAME=m3xx \
+	TARGET_DEVICE=m3 \
+	BUILD_FINGERPRINT="samsung/m3xx/m3:5.1/LMY47I/1767468:user/release-keys" \
+	PRIVATE_BUILD_DESC="m3xx-user 5.1 LMY47I 1767468 release-keys"
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+-include vendor/aicp/configs/bootanimation.mk
