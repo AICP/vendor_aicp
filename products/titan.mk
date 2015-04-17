4acff583f471e0b76871bbd4a48ab58f3d776160
strangerweather <strangerweather@gmail.com>
2015-04-17 11:22:56 +0100

    Titan : add Device identifier
    Change-Id: I35cdffe6a9a45f316cee1cee78dbe49ef2d67601

--- products/titan.mk
+++ products/titan.mk
@@ -6,6 +6,13 @@ $(call inherit-product, device/motorola/titan/full_titan.mk)
 PRODUCT_RELEASE_NAME := titan
 PRODUCT_NAME := aicp_titan
 
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := titan
+PRODUCT_NAME := aicp_titan
+PRODUCT_BRAND := Motorola
+PRODUCT_MODEL := Moto G (2014)
+PRODUCT_MANUFACTURER := Motorola
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
