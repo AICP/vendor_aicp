9107ebfbdb6dba13b6a3a89348faac630d44b6d7
strangerweather <strangerweather@gmail.com>
2015-04-17 09:24:50 +0100

    Falcon: add Device identifier
    Change-Id: Ifa2ceedab4c0565c5178ee907778a7dbc7248bae

--- products/falcon.mk
+++ products/falcon.mk
@@ -6,6 +6,13 @@ $(call inherit-product, device/motorola/falcon/full_falcon.mk)
 PRODUCT_RELEASE_NAME := MOTO G
 PRODUCT_NAME := aicp_falcon
 
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := falcon
+PRODUCT_NAME := aicp_falcon
+PRODUCT_BRAND := Motorola
+PRODUCT_MODEL := Moto G
+PRODUCT_MANUFACTURER := Motorola
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
