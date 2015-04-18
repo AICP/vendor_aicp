c819eef37cee820bf5d618f5da7c4dfaef43155a
doc <doc.divxm@gmail.com>
2015-04-18 22:09:03 +0300

    yureka: Add to AICP
    Change-Id: I5801deaa9fb3327e49a7be39cbcd96a15166ae1f

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -61,6 +61,7 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/su640.mk \
     $(LOCAL_DIR)/targa.mk \
     $(LOCAL_DIR)/t0lte.mk \
+    $(LOCAL_DIR)/tomato.mk \
     $(LOCAL_DIR)/trltespr.mk \
     $(LOCAL_DIR)/trltetmo.mk \
     $(LOCAL_DIR)/trltexx.mk \
---
+++ products/tomato.mk
@@ -0,0 +1,33 @@
+# Copyright (C) 2014 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Inherit some common AICP stuff.
+$(call inherit-product, vendor/aicp/configs/common.mk)
+
+$(call inherit-product, device/yu/tomato/full_tomato.mk)
+
+PRODUCT_NAME := aicp_tomato
+BOARD_VENDOR := yu
+PRODUCT_DEVICE := tomato
+
+PRODUCT_GMS_CLIENTID_BASE := android-micromax
+
+TARGET_VENDOR_PRODUCT_NAME := YUREKA
+TARGET_VENDOR_DEVICE_NAME := YUREKA
+PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=YUREKA PRODUCT_NAME=YUREKA
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+-include vendor/aicp/configs/bootanimation.mk
