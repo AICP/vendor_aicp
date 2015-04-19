ccd4b56f4facd6973627e166e220a44987267f4e
Mattia D'Alleva <antaresone@antaresone.eu>
2015-04-19 22:41:11 +0300

    jfltexx: Update fingerprint to LRX22C (5.0.1)
    Change-Id: I25e59f6039a51349927820ff47e34106ae14f483

--- products/jfltexx.mk
+++ products/jfltexx.mk
@@ -11,8 +11,8 @@ $(call inherit-product, device/samsung/jfltexx/full_jfltexx.mk)
 PRODUCT_BUILD_PROP_OVERRIDES += \
   PRODUCT_NAME=jfltexx \
   TARGET_DEVICE=jfltexx \
-  BUILD_FINGERPRINT="samsung/jfltexx/jflte:4.2.2/JDQ39/I9505XXUAMDE:user/release-keys" \
-  PRIVATE_BUILD_DESC="jfltexx-user 4.2.2 JDQ39 I9505XXUAMDE release-keys"
+  BUILD_FINGERPRINT="samsung/jfltexx/jflte:5.0.1/LRX22C/I9505XXUHOB7:user/release-keys" \
+  PRIVATE_BUILD_DESC="jfltexx-user 5.0.1 LRX22C I9505XXUHOB7 release-keys"
 
 PRODUCT_NAME := aicp_jfltexx
 PRODUCT_DEVICE := jfltexx
