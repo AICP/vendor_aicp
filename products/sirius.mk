0af9dd9557c7ea38cadbf2ada14750726e848a7f
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2015-04-25 01:23:09 +0300

    sirius: update (again :D ) fingerprint to 5.0.2 (23.1.A.0.690)
    Change-Id: Iad69ceca442f6dc80d2e9dee44f89d1e5aae6a81

--- products/sirius.mk
+++ products/sirius.mk
@@ -7,9 +7,9 @@ $(call inherit-product, vendor/aicp/configs/nfc_enhanced.mk)
 # Inherit device configuration
 $(call inherit-product, device/sony/sirius/full_sirius.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=D6503 \
-                                BUILD_FINGERPRINT=Sony/D6503/D6503:5.0.2/23.1.A.0.690/814068233:user/release-keys \
-                                PRIVATE_BUILD_DESC="D6503-user 5.0.2 23.1.A.0.690 814068233 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=D6503
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=Sony/D6503/D6503:5.0.2/23.1.A.0.690/814068233:user/release-keys
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="D6503-user 5.0.2 23.1.A.0.690 814068233 release-keys
 
 # Device identifier. This must come after all inclusions
 PRODUCT_NAME := aicp_sirius
