5eeb7780a78985e3aa49c01b5c0158537ebc8dc3
doc <doc.divxm@gmail.com>
2015-05-02 13:11:41 +0300

    vendor_aicp: Add support for Xperia M (nicki)
    Change-Id: Ie339ae43fa90eb92501a2f912e0211479bed58dd

---
+++ products/nicki.mk
@@ -0,0 +1,38 @@
+# Copyright (C) 2013 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Inherit some common AICP stuff.
+$(call inherit-product, vendor/aicp/configs/common.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/aicp/configs/nfc_enhanced.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/sony/nicki/nicki.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := nicki
+PRODUCT_NAME := aicp_nicki
+PRODUCT_BRAND := Sony
+PRODUCT_MODEL := nicki
+PRODUCT_MANUFACTURER := Sony
+PRODUCT_CHARACTERISTICS := phone
+
+# Release name
+PRODUCT_RELEASE_NAME := Xperiam
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 854
+TARGET_SCREEN_WIDTH := 480
+-include vendor/aicp/configs/bootanimation.mk
