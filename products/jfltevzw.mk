0ab8cb5dc5fec039d4201097a8dfdeb5791393d2
fitsnugly <rickfitsnugly@gmail.com>
2015-05-05 16:27:56 +0300

    jfltevzw: Update fingerprint to 5.0.1 (LRX22C)
    Change-Id: Id5812f6b9eb11c4ac99c6c504a2aa21d9e1c8df1

--- products/jfltevzw.mk
+++ products/jfltevzw.mk
@@ -7,8 +7,8 @@ $(call inherit-product, device/samsung/jfltevzw/full_jfltevzw.mk)
 PRODUCT_BUILD_PROP_OVERRIDES += \
   PRODUCT_NAME=jfltevzw \
   TARGET_DEVICE=jfltevzw \
-  BUILD_FINGERPRINT="Verizon/jfltevzw/jfltevzw:4.4.2/KOT49H/I545VRUFNK1:user/release-keys" \
-  PRIVATE_BUILD_DESC="jfltevzw-user 4.4.2 KOT49H I545VRUFNK1 release-keys"
+  BUILD_FINGERPRINT="Verizon/jfltevzw/jfltevzw:5.0.1/LRX22C/I545VRUGOC1:user/release-keys" \
+  PRIVATE_BUILD_DESC="jfltevzw-user 5.0.1 LRX22C I545VRUGOC1 release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-verizon
 
