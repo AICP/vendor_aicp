77996d24dc1548aa349deb5f137b3122b0a0ec31
eyosen <abittin@gmail.com>
2015-05-23 14:05:58 +0300

    deb: update fingerprint to LMY47V
    Change-Id: Ia4fb924226b4cd3a22cb08c734252e5a745186d9

--- products/deb.mk
+++ products/deb.mk
@@ -20,7 +20,7 @@ PRODUCT_MANUFACTURER := Asus
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
 #Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=razorg BUILD_FINGERPRINT=google/razorg/deb:5.0.2/LRX22G/1649326:user/release-keys PRIVATE_BUILD_DESC="razorg-user 5.0.2 LRX22G 1649326 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=razorg BUILD_FINGERPRINT=google/razorg/deb:5.1.1/LMY47V/1148727:user/release-keys PRIVATE_BUILD_DESC="razorg-user 5.1.1 LMY47V 1148727 release-keys"
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1920
