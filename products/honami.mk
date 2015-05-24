fb4ebb9480743ab3df5ab9e2aa9b1c5be19a6fdf
David Viteri <davidteri91@gmail.com>
2015-05-24 17:42:01 +0300

    sirius/honami: update fingerprint to latest lollipop release
    Change-Id: I45b5bd67c81d4f8ed373aa58fd283c9ec4df7d48

--- products/honami.mk
+++ products/honami.mk
@@ -6,7 +6,9 @@ $(call inherit-product, vendor/aicp/configs/nfc_enhanced.mk)
 
 $(call inherit-product, device/sony/honami/full_honami.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6903 BUILD_FINGERPRINT=Sony/C6903/C6903:4.4.4/14.4.A.0.157/ev__jQ:user/release-keys PRIVATE_BUILD_DESC="C6903-user 4.4.4 14.4.A.0.157 ev__jQ release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6903
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=Sony/C6903/C6903:5.0.2/14.5.A.0.270/3750474323:user/release-keys
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="C6903-user 5.0.2 14.5.A.0.270 3750474323 release-keys"
 
 # Setup device specific product configuration.
 PRODUCT_NAME := aicp_honami
