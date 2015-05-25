f5d7d65aef5e484878a9d11aa1a5606f1508eb10
doc HD <doc.divxm@gmail.com>
2015-05-25 17:14:45 +0200

    Revert "Move inherit Stagefright plugins to its own makefile rules"
    This reverts commit 9ee6e3dcd8169ed310560453e591a57dcba211cb.
    
    Change-Id: Ib14ec54b5915654b4fda47f5aa5b332d190665f2

--- configs/stagefrightplugins.mk
+++
@@ -1,8 +0,0 @@
-# Stagefright FFMPEG plugin
-ifneq ($(TARGET_PRODUCT),aicp_m8)
-PRODUCT_PACKAGES += \
-    libstagefright_soft_ffmpegadec \
-    libstagefright_soft_ffmpegvdec \
-    libFFmpegExtractor \
-    libnamparser
-endif
