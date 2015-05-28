3bd57a45a025e1b11d60c859e3db7b6ae44c2082
dhacker29 <dhackerdvm@gmail.com>
2015-05-28 11:49:27 +0300

    shamu: Update fingerprint to 5.1.1
    Change-Id: Ic3ebc3c798765ff821aacc994c0e1e4f15b8ad50

--- products/shamu.mk
+++ products/shamu.mk
@@ -16,8 +16,8 @@ PRODUCT_MODEL := Nexus 6
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=shamu \
-    BUILD_FINGERPRINT=google/shamu/shamu:5.1/LMY47I/1767468:user/release-keys \
-    PRIVATE_BUILD_DESC="shamu-user 5.1 LMY47I 1767468 release-keys"
+    BUILD_FINGERPRINT=google/shamu/shamu:5.1.1/LYZ28E/1914015:user/release-keys \
+    PRIVATE_BUILD_DESC="shamu-user 5.1.1 LYZ28E 1914015 release-keys"
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2560
