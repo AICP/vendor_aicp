9527ea97885bbef0224a56b3792ccf92cc482a91
doc <doc.divxm@gmail.com>
2015-06-15 19:37:02 +0300

    chagallwifi: Fix bootanimation resolution
    Change-Id: If4836e21184e00304a5ff2489d3678fa99249778

--- products/chagallwifi.mk
+++ products/chagallwifi.mk
@@ -14,5 +14,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2560
-TARGET_SCREEN_WIDTH := 1600
+TARGET_SCREEN_WIDTH := 1440
 -include vendor/aicp/configs/bootanimation.mk
