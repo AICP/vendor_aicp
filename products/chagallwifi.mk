8c10c39e6621849572e760d7d332987bd8eb42d0
doc <doc.divxm@gmail.com>
2015-06-15 19:16:04 +0300

    chagallwifi: Add AICP support for Galaxy Tab S Pro WiFi (SM-T800)
    Change-Id: I7a3040e08a44482b74a96c28d118ad9f49a833e7

---
+++ products/chagallwifi.mk
@@ -0,0 +1,18 @@
+# Inherit some common AICP stuff.
+$(call inherit-product, vendor/aicp/configs/common_tablet.mk)
+
+$(call inherit-product, device/samsung/chagallwifi/full_chagallwifi.mk)
+
+PRODUCT_NAME := aicp_chagallwifi
+PRODUCT_DEVICE := chagallwifi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_MODEL=SM-T800 \
+    PRODUCT_NAME=chagallwifi \
+    PRODUCT_DEVICE=chagallwifi \
+    TARGET_DEVICE=chagallwifi \
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2560
+TARGET_SCREEN_WIDTH := 1600
+-include vendor/aicp/configs/bootanimation.mk
