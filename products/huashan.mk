94a265c1c1a52e52a0b309c02bff91f42168df2d
doc <doc.divxm@gmail.com>
2015-06-23 16:01:32 +0300

    huashan: Update product.mk for lp5.1
    Change-Id: I8fa2a6094ee170b0075ff32fdd2d6240230c154c

--- products/huashan.mk
+++ products/huashan.mk
@@ -1,22 +1,14 @@
-# Inherit AICP device configuration for huashan
-$(call inherit-product-if-exists, device/sony/huashan/aicp.mk)
-
-# Inherit AICP common bits
+# Inherit AICP common Phone stuff.
 $(call inherit-product, vendor/aicp/configs/common.mk)
 
 # Enhanced NFC
 $(call inherit-product, vendor/aicp/configs/nfc_enhanced.mk)
 
-# Inherit AOSP device configuration for huashan
 $(call inherit-product, device/sony/huashan/full_huashan.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C5303 BUILD_FINGERPRINT=Sony/C5303/C5303:5.12.1.A.0.266/9f5_nw:user/release-keys PRIVATE_BUILD_DESC="C5303-user 5.1 JB-MR2-VISKAN-CAF-140119-1358 30 test-keys"
+# Build fingerprints
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C5303 BUILD_FINGERPRINT=Sony/C5303/C5303:4.3/12.1.A.1.207/Nvt_nw:user/release-keys PRIVATE_BUILD_DESC="C5303-user 4.3 JB-MR2-VISKAN-140318-1014 227 test-keys"
 
-
-# huashan overlay
-PRODUCT_PACKAGE_OVERLAYS += vendor/aicp/overlay/huashan
-
-# Setup device specific product configuration.
 PRODUCT_NAME := aicp_huashan
 PRODUCT_BRAND := Sony
 PRODUCT_DEVICE := huashan
