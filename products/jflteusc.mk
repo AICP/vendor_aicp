72005dcdff60d44f9eaa7c4d7c2c43917ab9edc8
doc HD <doc.divxm@gmail.com>
2015-07-05 14:35:33 +0300

    jflteusc: Update product.mk
    Change-Id: I584d0141d408557467dbdb8d405c401adb0b0e28

--- products/jflteusc.mk
+++ products/jflteusc.mk
@@ -1,21 +1,23 @@
-# Inherit common product files.
+# Inherit some common CM stuff.
 $(call inherit-product, vendor/aicp/configs/common.mk)
 
 # Enhanced NFC
 $(call inherit-product, vendor/aicp/configs/nfc_enhanced.mk)
 
-# Inherit AOSP device configuration for jflteusc
 $(call inherit-product, device/samsung/jflteusc/full_jflteusc.mk)
 
 
-# Setup device specific product configuration.
+PRODUCT_BUILD_PROP_OVERRIDES += \
+  PRODUCT_NAME=jflteusc \
+  TARGET_DEVICE=jflteusc \
+  BUILD_FINGERPRINT="samsung/jflteusc/jflteusc:4.4.2/KOT49H/R970TYUFNK1:user/release-keys" \
+  PRIVATE_BUILD_DESC="jflteusc-user 4.4.2 KOT49H R970TYUFNK1 release-keys"
+
 PRODUCT_NAME := aicp_jflteusc
 PRODUCT_BRAND := Samsung
 PRODUCT_DEVICE := jflteusc
 PRODUCT_MODEL := SCH-R970
 PRODUCT_MANUFACTURER := Samsung
-
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jflteusc TARGET_DEVICE=jflteusc BUILD_FINGERPRINT="samsung/jflteusc/jflteusc:4.2.2/JDQ39/R970VXUAMDB:user/release-keys" PRIVATE_BUILD_DESC="jflteusc-user 4.2.2 JDQ39 R970VXUAMDB release-keys"
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1920
