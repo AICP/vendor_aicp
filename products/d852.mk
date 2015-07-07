3f558f204460775ece842edf27b2347ba39d71f7
doc HD <doc.divxm@gmail.com>
2015-07-07 09:45:38 +0300

    d852: Update fingerprint to 5.0 LRX21R
    Change-Id: I7838f08d46a27f565d1c5ed5e1a3d42164a1c6e2

--- products/d852.mk
+++ products/d852.mk
@@ -18,8 +18,8 @@ PRODUCT_MANUFACTURER := lge
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g3" \
     PRODUCT_NAME="g3_bell_ca" \
-    BUILD_FINGERPRINT="lge/g3_bell_ca/g3:4.4.2/KVT49L.D85210b/D85210b.1405397224:user/release-keys" \
-    PRIVATE_BUILD_DESC="g3_bell_ca-user 4.4.2 KVT49L.D85210b D85210b.1405397224 release-keys"
+    BUILD_FINGERPRINT="lge/g3_global_com/g3:5.0/LRX21R.A1421650137/1421650137:user/release-keys" \
+    PRIVATE_BUILD_DESC="g3_global_com-user 5.0 LRX21R.A1421650137 1421650137 release-keys"
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2560
