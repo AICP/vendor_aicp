6b67f1ece0793658f1e8f83bff116b13e994ec64
Adnan Begovic <adnan@cyngn.com>
2015-07-14 15:46:50 +0300

    vendor_aicp: Create CM_PLATFORM_SDK_VERSION.
    Change-Id: I2f41d638ed2c1b6f7f107859dfec8a3f094fb40a

--- configs/common.mk
+++ configs/common.mk
@@ -187,6 +187,20 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/aicp/prebuilt/common/app/KernelAdiutor.apk:system/priv-app/KernelAdiutor/KernelAdiutor.apk
 
+ifndef CM_PLATFORM_SDK_VERSION
+  # This is the canonical definition of the SDK version, which defines
+  # the set of APIs and functionality available in the platform.  It
+  # is a single integer that increases monotonically as updates to
+  # the SDK are released.  It should only be incremented when the APIs for
+  # the new release are frozen (so that developers don't write apps against
+  # intermediate builds).
+  CM_PLATFORM_SDK_VERSION := 1
+endif
+
+# CyanogenMod Platform SDK Version
+PRODUCT_PROPERTY_OVERRIDES += \
+  ro.cm.build.version.plat.sdk=$(CM_PLATFORM_SDK_VERSION)
+
 # -include vendor/cyngn/product.mk
 
 # OTA
