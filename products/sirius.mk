f0c3fd52a883f1063c24d192b2cfa1a1162e251b
David Viteri <davidteri91@gmail.com>
2015-08-04 23:39:55 +0300

    sirius: update fingerprint to latest lollipop release
    Change-Id: I7de5ca4ccba885d1dcc06486bdc23074434103ab

--- products/sirius.mk
+++ products/sirius.mk
@@ -8,8 +8,8 @@ $(call inherit-product, vendor/aicp/configs/nfc_enhanced.mk)
 $(call inherit-product, device/sony/sirius/full_sirius.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=D6503
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=Sony/D6503/D6503:5.0.2/23.1.A.1.28/151083418:user/release-keys
-PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="D6503-user 5.0.2 23.1.A.1.28 151083418 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=Sony/D6503/D6503:5.1.1/23.4.A.0.546/864948651:user/release-keys
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="D6503-user 5.1.1 23.4.A.0.546 864948651 release-keys"
 
 # Device identifier. This must come after all inclusions
 PRODUCT_NAME := aicp_sirius
