9178eb007f68edf9ec499d3813059c48808ad867
doc HD <doc.divxm@gmail.com>
2015-08-05 17:35:35 +0300

    z3c: Add AICP support for Sony Xperia Z3 Compact

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -91,4 +91,5 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/wx_na_wf.mk \
     $(LOCAL_DIR)/xt1053.mk \
     $(LOCAL_DIR)/xt1060.mk \
-    $(LOCAL_DIR)/yuga.mk
+    $(LOCAL_DIR)/yuga.mk \
+    $(LOCAL_DIR)/z3c.mk
---
+++ products/z3c.mk
@@ -0,0 +1,19 @@
+# Inherit AICP common Phone stuff.
+$(call inherit-product, vendor/aicp/configs/common.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/aicp/configs/nfc_enhanced.mk)
+
+$(call inherit-product, device/sony/z3c/full_z3c.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=D5803
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=Sony/D5803/D5803:5.1.1/23.4.A.0.546/864948651:user/release-keys
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="D5803-user 5.1.1 23.4.A.0.546 864948651 release-keys"
+
+PRODUCT_NAME := aicp_z3c
+PRODUCT_DEVICE := z3c
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+-include vendor/aicp/configs/bootanimation.mk
