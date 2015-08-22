feb7b28bfa42b3c5a82625c909619c8657cc304d
doc HD <doc.divxm@gmail.com>
2015-08-22 11:18:33 +0300

    vendor_aicp: Remove Unsupported Devices
    Change-Id: Iac1dc5e227c2ad26512e17ad566a3e4bbaacaee4

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -1,8 +1,5 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/a510.mk \
     $(LOCAL_DIR)/amami.mk \
-    $(LOCAL_DIR)/ancora.mk \
-    $(LOCAL_DIR)/ariesve.mk \
     $(LOCAL_DIR)/armani.mk \
     $(LOCAL_DIR)/bacon.mk \
     $(LOCAL_DIR)/cancro.mk \
@@ -16,24 +13,17 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/d800.mk \
     $(LOCAL_DIR)/d801.mk \
     $(LOCAL_DIR)/d802.mk \
-    $(LOCAL_DIR)/dogo.mk \
     $(LOCAL_DIR)/e980.mk \
     $(LOCAL_DIR)/endeavoru.mk \
-    $(LOCAL_DIR)/evita.mk \
     $(LOCAL_DIR)/falcon.mk \
-    $(LOCAL_DIR)/find5.mk \
     $(LOCAL_DIR)/flounder.mk \
     $(LOCAL_DIR)/grouper.mk \
     $(LOCAL_DIR)/hammerhead.mk \
     $(LOCAL_DIR)/hammerheadcaf.mk \
-    $(LOCAL_DIR)/hercules.mk \
     $(LOCAL_DIR)/himaul.mk \
     $(LOCAL_DIR)/honami.mk \
     $(LOCAL_DIR)/huashan.mk \
     $(LOCAL_DIR)/hltetmo.mk \
-    $(LOCAL_DIR)/i605.mk \
-    $(LOCAL_DIR)/i9100.mk \
-    $(LOCAL_DIR)/i9100g.mk \
     $(LOCAL_DIR)/i9300.mk \
     $(LOCAL_DIR)/i9305.mk \
     $(LOCAL_DIR)/jewel.mk \
@@ -46,32 +36,16 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/jfltexx.mk \
     $(LOCAL_DIR)/klimtlte.mk \
     $(LOCAL_DIR)/klimtwifi.mk \
-    $(LOCAL_DIR)/l900.mk \
     $(LOCAL_DIR)/ls980.mk \
     $(LOCAL_DIR)/lettuce.mk \
     $(LOCAL_DIR)/m7.mk \
     $(LOCAL_DIR)/m7spr.mk \
     $(LOCAL_DIR)/m7vzw.mk \
-    $(LOCAL_DIR)/maguro.mk \
     $(LOCAL_DIR)/mako.mk \
-    $(LOCAL_DIR)/manta.mk \
-    $(LOCAL_DIR)/maserati.mk \
-    $(LOCAL_DIR)/mint.mk \
-    $(LOCAL_DIR)/moto_msm8960dt.mk \
-    $(LOCAL_DIR)/n7000.mk \
-    $(LOCAL_DIR)/n7100.mk \
     $(LOCAL_DIR)/nicki.mk \
-    $(LOCAL_DIR)/odin.mk \
-    $(LOCAL_DIR)/p930.mk \
     $(LOCAL_DIR)/peregrine.mk \
-    $(LOCAL_DIR)/pollux.mk \
-    $(LOCAL_DIR)/pollux_windy.mk \
     $(LOCAL_DIR)/shamu.mk \
-    $(LOCAL_DIR)/solana.mk \
     $(LOCAL_DIR)/sprout4.mk \
-    $(LOCAL_DIR)/spyder.mk \
-    $(LOCAL_DIR)/su640.mk \
-    $(LOCAL_DIR)/targa.mk \
     $(LOCAL_DIR)/t0lte.mk \
     $(LOCAL_DIR)/tomato.mk \
     $(LOCAL_DIR)/trltespr.mk \
@@ -81,15 +55,10 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/tilapia.mk \
     $(LOCAL_DIR)/titan.mk \
     $(LOCAL_DIR)/togari.mk \
-    $(LOCAL_DIR)/toro.mk \
-    $(LOCAL_DIR)/toroplus.mk \
-    $(LOCAL_DIR)/tsubasa.mk \
-    $(LOCAL_DIR)/umtsspyder.mk \
     $(LOCAL_DIR)/v500.mk \
     $(LOCAL_DIR)/vs920.mk \
     $(LOCAL_DIR)/vs980.mk \
     $(LOCAL_DIR)/wx_na_wf.mk \
     $(LOCAL_DIR)/xt1053.mk \
     $(LOCAL_DIR)/xt1060.mk \
-    $(LOCAL_DIR)/yuga.mk \
     $(LOCAL_DIR)/z3c.mk
