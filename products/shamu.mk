aa48290a11796e250426643df45031c6c1967a1a
doc HD <doc.divxm@gmail.com>
2015-09-12 00:45:34 +0300

    shamu: Update fingerprint to LMY48M
    Change-Id: I2f9547ccbf57cb1059086a5baf10bdf4f9e27fa6

--- products/shamu.mk
+++ products/shamu.mk
@@ -18,8 +18,8 @@ TARGET_VENDOR := moto
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=shamu \
-    BUILD_FINGERPRINT=google/shamu/shamu:5.1.1/LYZ28E/1914015:user/release-keys \
-    PRIVATE_BUILD_DESC="shamu-user 5.1.1 LYZ28E 1914015 release-keys"
+    BUILD_FINGERPRINT=google/shamu/shamu:5.1.1/LMY48M/2167285:user/release-keys \
+    PRIVATE_BUILD_DESC="shamu-user 5.1.1 LMY48M 2167285 release-keys"
 
 # AICP Device Maintainers
 PRODUCT_BUILD_PROP_OVERRIDES += \
