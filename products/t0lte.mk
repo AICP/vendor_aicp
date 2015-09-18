503a5b82d8dc6e359937c43ae99245fe11244df0
Zipsnet <hcolmenares@gmail.com>
2015-09-18 12:04:41 +0300

    t0lte: Added Device Maintainers
    Change-Id: If0ada110a5c1611bf2597b640fa5908503dbe355

--- products/t0lte.mk
+++ products/t0lte.mk
@@ -18,6 +18,11 @@ PRODUCT_MANUFACTURER := samsung
 # Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=t0lte TARGET_DEVICE=t0lte BUILD_FINGERPRINT="samsung/t0ltexx/t0lte:4.1.2/JZO54K/N7105XXDLL4:user/release-keys" PRIVATE_BUILD_DESC="t0ltexx-user 4.1.2 JZO54K N7105XXDLL4 release-keys"
 
+# AICP Device Maintainers
+PRODUCT_BUILD_PROP_OVERRIDES += \
+        DEVICE_MAINTAINERS="Hector (Zips)"
+
+
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1280
