3f12d209dc5c974447938bbfacca24e8ecac1f49
HashBang <hashbang173@gmail.com>
2015-09-27 16:57:20 -0400

    Add support for clark (Moto X Pure Edition 2015)
    Change-Id: Iee5c86a2aff3b2041d0bc3aef7e2a492b8c9cad0

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -5,6 +5,7 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/cancro.mk \
     $(LOCAL_DIR)/chagalllte.mk \
     $(LOCAL_DIR)/chagallwifi.mk \
+    $(LOCAL_DIR)/clark.mk \
     $(LOCAL_DIR)/condor.mk \
     $(LOCAL_DIR)/d2att.mk \
     $(LOCAL_DIR)/d2tmo.mk \
---
+++ products/clark.mk
@@ -0,0 +1,27 @@
+# Inherit 64-bit configs
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
+# Inherit some common AICP stuff.
+$(call inherit-product, vendor/aicp/configs/common.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/aicp/configs/nfc_enhanced.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/motorola/clark/aosp_clark.mk)
+
+DEVICE_PACKAGE_OVERLAYS += device/motorola/clark/overlay
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := aicp_clark
+PRODUCT_BRAND := motorola
+PRODUCT_MODEL := XT1575
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT=motorola/clark_retus/clark:5.1.1/LPH23.116-18/18:user/release-keys \
+    PRIVATE_BUILD_DESC="clark_retus-user 5.1.1 LPH23.116-18 18 release-keys"
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2560
+TARGET_SCREEN_WIDTH := 1440
+-include vendor/aicp/configs/bootanimation.mk
