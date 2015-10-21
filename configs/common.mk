b01363e886d6561988840887c4d130379661af6d
Steve Kondik <steve@cyngn.com>
2015-10-21 19:59:01 +0300

    vendor_aicp: Turn off carrier provisioning by default to allow tethering
    Change-Id: I682e4746414f5fc51ed48e264e6bfe7ccc25856d

--- configs/common.mk
+++ configs/common.mk
@@ -76,6 +76,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.selinux=1
 
+# Thank you, please drive thru!
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.dun.override=0
+
 # Backup Tool
 PRODUCT_COPY_FILES += \
     vendor/aicp/prebuilt/bin/backuptool.sh:install/bin/backuptool.sh \
