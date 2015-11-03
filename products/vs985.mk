611915598742fcf7991a0c5b10e857a9c34a35f0
doc HD <doc.divxm@gmail.com>
2015-11-03 21:37:47 +0300

    vs985: Update fingerprint to LMY47V (5.1.1)
    Change-Id: I8082e31acdb5625b24e80c19d2cc842b5d1bc2b1

--- products/vs985.mk
+++ products/vs985.mk
@@ -17,8 +17,8 @@ PRODUCT_MANUFACTURER := lge
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g3" \
     PRODUCT_NAME="g3_vzw_us" \
-    BUILD_FINGERPRINT="lge/g3_vzw/g3:5.0.1/LRX21Y/1516810098cf1:user/release-keys" \
-    PRIVATE_BUILD_DESC="g3_vzw-user 5.0.1 LRX21Y 1516810098cf1 release-keys"
+    BUILD_FINGERPRINT="lge/g3_vzw/g3:5.1.1/LMY47V/1525410464a1b:user/release-keys" \
+    PRIVATE_BUILD_DESC="g3_vzw-user 5.1.1 LMY47V 1525410464a1b release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-verizon
 
