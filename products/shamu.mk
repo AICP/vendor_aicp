4d7548551987fb1604868387b1842891fb3ffa3d
doc HD <doc.divxm@gmail.com>
2015-11-05 15:29:21 +0300

    shamu: Update fingerprpint to MRA58N
    Change-Id: I50859fefe068990618fa92e93737068c127738ed

--- products/shamu.mk
+++ products/shamu.mk
@@ -18,8 +18,8 @@ TARGET_VENDOR := moto
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=shamu \
-    BUILD_FINGERPRINT=google/shamu/shamu:6.0/MRA58K/2256973:user/release-keys \
-    PRIVATE_BUILD_DESC="shamu-user 6.0 MRA58K 2256973 release-keys"
+    BUILD_FINGERPRINT=google/shamu/shamu:6.0/MRA58N/2289998:user/release-keys \
+    PRIVATE_BUILD_DESC="shamu-user 6.0 MRA58N 2289998 release-keys"
 
 # AICP Device Maintainers
 PRODUCT_BUILD_PROP_OVERRIDES += \
