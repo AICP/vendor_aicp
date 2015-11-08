492e2a0592c5c78a7c5e2d61fcf84bff86d3b93b
Ethan Chen <intervigil@gmail.com>
2015-11-08 06:26:00 +0200

    himaul: Define extra vendor flags
    AICP'fy: Original CM commit is in himaul device repo
    http://review.cyanogenmod.org/#/c/118578/
    
    Change-Id: I4bfd190c1bd5e3760eabfe65f57978eb642f2dca

--- products/himaul.mk
+++ products/himaul.mk
@@ -7,6 +7,9 @@ $(call inherit-product, vendor/aicp/configs/nfc_enhanced.mk)
 $(call inherit-product, device/htc/himaul/full_himaul.mk)
 
 PRODUCT_NAME := aicp_himaul
+BOARD_VENDOR := htc
+TARGET_VENDOR := htc
+PRODUCT_DEVICE := himaul
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="htc_himaulatt" \
