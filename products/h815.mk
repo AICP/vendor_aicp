ec8c93ed186073f7cd558c0a89753c89264d0429
doc HD <doc.divxm@gmail.com>
2015-12-07 15:35:06 +0200

    h815: Update product.mk for mm6.0
    Change-Id: Ia28a42d369e613460f119e9eead104235536aa7d

--- products/h815.mk
+++ products/h815.mk
@@ -3,33 +3,28 @@ $(call inherit-product, vendor/aicp/configs/common.mk)
 
 # Inherit telephony stuff
 $(call inherit-product, vendor/aicp/configs/telephony.mk)
- 
+
 # Enhanced NFC
 $(call inherit-product, vendor/aicp/configs/nfc_enhanced.mk)
- 
+
 # Inherit AOSP device configuration for H815
 $(call inherit-product, device/lge/h815/full_h815.mk)
- 
- 
+
 # Setup device specific product configuration
-PRODUCT_DEVICE := h815
-PRODUCT_NAME := aicp_h815
-PRODUCT_BRAND := lge
-PRODUCT_MODEL := LG-H815
-PRODUCT_MANUFACTURER := LGE
- 
+PRODUCT_NAME := cm_h815
+
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g4" \
     PRODUCT_NAME="g4_global_com" \
-    BUILD_FINGERPRINT="lge/p1_global_com/p1:5.1/LMY47D/151161552fef7:user/release-keys" \
-    PRIVATE_BUILD_DESC="p1_global_com-user 5.1 LMY47D 151161552fef7 release-keys"
- 
+    BUILD_FINGERPRINT="lge/p1_global_com/p1:6.0/MRA58K/152940055675e:user/release-keys" \
+    PRIVATE_BUILD_DESC="p1_global_com-user 6.0 MRA58K 152940055675e release-keys"
+
 # AICP Device Maintainers
 PRODUCT_BUILD_PROP_OVERRIDES += \
         DEVICE_MAINTAINERS="Sinisa (thermskin)"
- 
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2560
 TARGET_SCREEN_WIDTH := 1440
- 
+
 -include vendor/aicp/configs/bootanimation.mk
