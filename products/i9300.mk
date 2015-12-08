411ddeac0203bd8bea50c14d1fb260751feb62f9
doc HD <doc.divxm@gmail.com>
2015-12-08 16:51:07 +0200

    i9300: Update productmk for mm6.0
    Change-Id: Ifdf3bdbb85f05fcb610a7f4a3ecb2fb0500ba91e

--- products/i9300.mk
+++ products/i9300.mk
@@ -1,3 +1,6 @@
+# Release name
+PRODUCT_RELEASE_NAME := i9300
+
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/aicp/configs/common.mk)
 
@@ -10,19 +13,15 @@ $(call inherit-product, vendor/aicp/configs/nfc_enhanced.mk)
 # Inherit device configuration
 $(call inherit-product, device/samsung/i9300/full_i9300.mk)
 
-
-# Discard inherited values and use our own instead.
-PRODUCT_NAME := aicp_i9300
+# Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := i9300
+PRODUCT_NAME := aicp_i9300
 PRODUCT_BRAND := samsung
-PRODUCT_MANUFACTURER := samsung
 PRODUCT_MODEL := GT-I9300
-
-
+PRODUCT_MANUFACTURER := samsung
 
 # Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=m0xx TARGET_DEVICE=m0 BUILD_FINGERPRINT="samsung/m0xx/m0:4.3/JSS15J/I9300XXUGMJ9:user/release-keys" PRIVATE_BUILD_DESC="m0xx-user 4.3 JSS15J I9300XXUGMJ9 release-keys"
-PRODUCT_RELEASE_NAME := GT-I9300
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1280
