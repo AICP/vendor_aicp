b1ce23e9e5eed31077155024f56c7e3da4c512c2
Roman Birg <roman@cyngn.com>
2015-12-09 13:55:46 +0100

    angler: update fingerprint
    Change-Id: Ibda014cf4bae5cbcf2049d8877d14467a7144c51
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- products/angler.mk
+++ products/angler.mk
@@ -21,8 +21,8 @@ TARGET_VENDOR := huawei
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=angler \
-    BUILD_FINGERPRINT=google/angler/angler:6.0/MDB08M/2353240:user/release-keys \
-    PRIVATE_BUILD_DESC="angler-user 6.0 MDB08M 2353240 release-keys"
+    BUILD_FINGERPRINT=google/angler/angler:6.0.1/MMB29M/2431559:user/release-keys \
+    PRIVATE_BUILD_DESC="angler-user 6.0.1 MMB29M 2431559 release-keys"
 
 # AICP Device Maintainers
 PRODUCT_BUILD_PROP_OVERRIDES += \
