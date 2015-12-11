cb16a75c5dd4e58e38f8c92597052e7f4e25e425
HashBang <hashbang173@gmail.com>
2015-12-11 16:19:40 -0500

    flounder: marshmallow
    Change-Id: Idf801407ea2d16ed4cb6f5fd03ad08fc891b8a66

--- products/flounder.mk
+++ products/flounder.mk
@@ -9,6 +9,24 @@ $(call inherit-product, device/htc/flounder/aosp_flounder.mk)
 
 $(call inherit-product-if-exists, vendor/htc/flounder/device-vendor.mk)
 
+BOARD_NEEDS_VENDORIMAGE_SYMLINK := true
+
+# Inline kernel building
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-4.9/bin
+KERNEL_TOOLCHAIN_PREFIX := aarch64-linux-android-
+TARGET_KERNEL_SOURCE := kernel/htc/flounder
+TARGET_KERNEL_CONFIG := flounder_defconfig
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+BOARD_KERNEL_CMDLINE := androidboot.selinux=enforcing
+TARGET_PREBUILT_KERNEL := false
+
+# Extra Packages
+PRODUCT_PACKAGES += \
+    com.android.nfc_extras
+
+# CM Overlays
+DEVICE_PACKAGE_OVERLAYS += device/htc/flounder/overlay-cm
+
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := flounder
 PRODUCT_NAME := aicp_flounder
@@ -19,7 +37,14 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 
 # AICP Device Maintainers
 PRODUCT_BUILD_PROP_OVERRIDES += \
-	DEVICE_MAINTAINERS="Joshua G (Hashbang173)"
+    DEVICE_MAINTAINERS="Hashbang173"
+
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=flounder \
+    BUILD_FINGERPRINT=google/volantis/flounder:6.0.1/MMB29K/2419427:user/release-keys \
+    PRIVATE_BUILD_DESC="volantis-user 6.0.1 MMB29K 2419427 release-keys" \
+    BUILD_ID=MMB29K
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2048
