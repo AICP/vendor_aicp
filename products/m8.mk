def067aa5beeee7f9960ac9ff31923d177dc376a
doc HD <doc.divxm@gmail.com>
2015-12-19 23:42:27 +0200

    m8: Update product.mk for mm6.0
    Change-Id: Idc30e1f83bdce4a2613bd3377a233193aee06555

--- products/m8.mk
+++ products/m8.mk
@@ -12,14 +12,6 @@ $(call inherit-product, device/htc/m8/full_m8.mk)
 $(call inherit-product-if-exists, vendor/htc/m8/m8-vendor.mk)
 
 PRODUCT_NAME := aicp_m8
-PRODUCT_BRAND := google
-PRODUCT_DEVICE := m8
-PRODUCT_MODEL := HTC One m8
-PRODUCT_MANUFACTURER := HTC
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-       BUILD_ID=KOT49H BUILD_FINGERPRINT="htc/htc_europe/htc_m8:4.4.2/KOT49H/325784.5:user/release-keys" \
-       PRIVATE_BUILD_DESC="1.54.401.5 CL325784 release-keys"
 
 # AICP Device Maintainers
 PRODUCT_BUILD_PROP_OVERRIDES += \
