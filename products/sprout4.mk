8fa5ed0aaa3f1773624f6110451359b425f95c02
TheStrix <parthbhatia98@gmail.com>
2015-12-25 17:34:01 +0530

    sprout4: Update fingerprint
    Change-Id: Icc6edd8876837f6a2d995ffb2aa2343286e16c9a

--- products/sprout4.mk
+++ products/sprout4.mk
@@ -6,7 +6,7 @@ $(call inherit-product, vendor/aicp/configs/telephony.mk)
 
 $(call inherit-product, device/google/sprout4/sprout4.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=5.1/LMY47O/1783956:user/release-keys PRIVATE_BUILD_DESC="sprout-user 5.1 LMY47O 1783956 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=6.0.1/MMB29K/2419427:user/release-keys PRIVATE_BUILD_DESC="sprout-user 6.0.1 MMB29K 2419427 release-keys"
 
 PRODUCT_NAME := aicp_sprout4
 PRODUCT_DEVICE :=sprout4
