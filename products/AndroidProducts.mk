751e1b9a27a197f8db4b86447f71f2394a9cd3ca
doc HD <doc.divxm@gmail.com>
2016-01-05 11:17:49 +0200

    oneplus2: Add to AICP devices
    Change-Id: Ie9705309acd55660b020ca265b1e5e25ffb647cb

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -48,6 +48,7 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/mako.mk \
     $(LOCAL_DIR)/manta.mk \
     $(LOCAL_DIR)/nicki.mk \
+    $(LOCAL_DIR)/oneplus2.mk \
     $(LOCAL_DIR)/otus.mk \
     $(LOCAL_DIR)/peregrine.mk \
     $(LOCAL_DIR)/shamu.mk \
