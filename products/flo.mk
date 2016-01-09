bd610f3afe2ccb6e0e617f2cbc83a0c7073c92f6
doc HD <doc.divxm@gmail.com>
2016-01-09 23:59:43 +0200

    deb | flo: Update fingerprint to MMB29O
    Change-Id: Ia904d6b5295ae54665e8de77c1aa6f7d8d9b87b7

--- products/flo.mk
+++ products/flo.mk
@@ -22,8 +22,8 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 #Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=razor \
-    BUILD_FINGERPRINT=google/razor/flo:6.0.1/MMB29K/2419427:user/release-keys \
-    PRIVATE_BUILD_DESC="razor-user 6.0.1 MMB29K 2419427 release-keys"
+    BUILD_FINGERPRINT=google/razor/flo:6.0.1/MMB29O/2459718:user/release-keys \
+    PRIVATE_BUILD_DESC="razor-user 6.0.1 MMB29O 2459718 release-keys"
 
 # AICP Device Maintainers
 PRODUCT_BUILD_PROP_OVERRIDES += \
