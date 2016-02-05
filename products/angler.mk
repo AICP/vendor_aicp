e6d0c4d31cd66b4cf77daad18bff7bb73e9e06c0
nuclearmistake <nuclearmistake@gmail.com>
2016-02-05 21:50:39 +0100

    angler: update fingerprint to MMB29Q
    Change-Id: Id5d01a319bbf2221451fd29492fc42f5df8d58df

--- products/angler.mk
+++ products/angler.mk
@@ -21,8 +21,8 @@ TARGET_VENDOR := huawei
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=angler \
-    BUILD_FINGERPRINT=google/angler/angler:6.0.1/MMB29P/2473553:user/release-keys \
-    PRIVATE_BUILD_DESC="angler-user 6.0.1 MMB29P 2473553 release-keys"
+    BUILD_FINGERPRINT=google/angler/angler:6.0.1/MMB29Q/2480792:user/release-keys \
+    PRIVATE_BUILD_DESC="angler-user 6.0.1 MMB29Q 2480792 release-keys"
 
 # AICP Device Maintainers
 PRODUCT_BUILD_PROP_OVERRIDES += \
