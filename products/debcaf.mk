6cfff843c10bb3882db9b4cacb09fbc1514a9165
doc HD <doc.divxm@gmail.com>
2016-02-23 22:42:44 +0200

    debcaf: Inherit aosp_deb.mk
    Change-Id: I051509b2c452774ecbb256b0f5fdd5836b9c5b8f

--- products/debcaf.mk
+++ products/debcaf.mk
@@ -11,7 +11,7 @@ $(call inherit-product, vendor/aicp/configs/telephony.mk)
 $(call inherit-product, vendor/aicp/configs/nfc_enhanced.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/asus/debcaf/full_deb.mk)
+$(call inherit-product, device/asus/debcaf/aosp_deb.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := debcaf
