06eec502bdaca397a1aae172a68ea0261e6d5517
doc HD <doc.divxm@gmail.com>
2016-02-23 21:39:32 +0200

    debcaf | flocaf: add support for deb & flo CAF versions
    Change-Id: I2600fb30a9db1d9d2e51b214e0b4471d0c628d20

---
+++ products/debcaf.mk
@@ -0,0 +1,38 @@
+# Release name
+PRODUCT_RELEASE_NAME := debcaf
+
+# Inherit some common stuff.
+$(call inherit-product, vendor/aicp/configs/common_tablet.mk)
+
+# Inherit telephony stuff
+$(call inherit-product, vendor/aicp/configs/telephony.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/aicp/configs/nfc_enhanced.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/asus/debcaf/full_deb.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := debcaf
+PRODUCT_NAME := aicp_debcaf
+PRODUCT_BRAND := Google
+PRODUCT_MODEL := Nexus 7 2013 CAF
+PRODUCT_MANUFACTURER := Asus
+
+PRODUCT_RESTRICT_VENDOR_FILES := false
+
+#Set build fingerprint / ID / Product Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=razorg \
+    BUILD_FINGERPRINT=google/razorg/deb:6.0.1/MMB29Q/2480792:user/release-keys \
+    PRIVATE_BUILD_DESC="razorg-user 6.0.1 MMB29Q 2480792 release-keys"
+
+# AICP Device Maintainers
+PRODUCT_BUILD_PROP_OVERRIDES += \
+	DEVICE_MAINTAINERS="doc HD (semdoc), Ali B (eyosen)"
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+-include vendor/aicp/configs/bootanimation.mk
---
+++ products/flocaf.mk
@@ -0,0 +1,35 @@
+# Release name
+PRODUCT_RELEASE_NAME := flocaf
+
+# Inherit some common stuff.
+$(call inherit-product, vendor/aicp/configs/common_tablet.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/aicp/configs/nfc_enhanced.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/asus/flocaf/full_flo.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := flocaf
+PRODUCT_NAME := aicp_flocaf
+PRODUCT_BRAND := Google
+PRODUCT_MODEL := Nexus 7 2013 CAF
+PRODUCT_MANUFACTURER := Asus
+
+PRODUCT_RESTRICT_VENDOR_FILES := false
+
+#Set build fingerprint / ID / Product Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=razor \
+    BUILD_FINGERPRINT=google/razor/flo:6.0.1/MMB29Q/2480792:user/release-keys \
+    PRIVATE_BUILD_DESC="razor-user 6.0.1 MMB29Q 2480792 release-keys"
+
+# AICP Device Maintainers
+PRODUCT_BUILD_PROP_OVERRIDES += \
+	DEVICE_MAINTAINERS="Ali B (eyosen), doc HD (semdoc)"
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+-include vendor/aicp/configs/bootanimation.mk
