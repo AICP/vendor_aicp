cd7adb62402f40f93671331169523971d689d4c8
Peter Repukat <orly.alia5@gmail.com>
2016-02-28 19:37:47 +0100

    togari: update makefile
    Change-Id: Icf2fa170e7863b6ced7677590bae6aaf9b6bbe4f

--- products/togari.mk
+++ products/togari.mk
@@ -7,7 +7,7 @@ $(call inherit-product, vendor/aicp/configs/telephony.mk)
 # Enhanced NFC
 $(call inherit-product, vendor/aicp/configs/nfc_enhanced.mk)
 
-$(call inherit-product, device/sony/togari/full_togari.mk)
+$(call inherit-product, device/sony/togari/aicp.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6833
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=Sony/C6833/C6833:5.0.2/14.5.A.0.270/3305956307:user/release-keys
