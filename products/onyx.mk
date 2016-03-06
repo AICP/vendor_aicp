ea6a7a0bfdef4988b21b89f3303fe86fb546ec51
doc HD <doc.divxm@gmail.com>
2016-03-06 13:06:02 +0200

    onyx: Add to supported devices
    Change-Id: I90b707ffe7dfb67895d4314a8c32d2b30dec4275

---
+++ products/onyx.mk
@@ -0,0 +1,40 @@
+# Copyright (C) 2016 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Inherit some common AICP stuff.
+$(call inherit-product, vendor/aicp/configs/common.mk)
+
+# Inherit telephony stuff
+$(call inherit-product, vendor/aicp/configs/telephony.mk)
+
+# Inherit from onyx device
+$(call inherit-product, device/oneplus/onyx/device.mk)
+
+# Call the proprietary setup
+$(call inherit-product-if-exists, vendor/oneplus/onyx/onyx-vendor.mk)
+
+PRODUCT_NAME := aicp_onyx
+PRODUCT_DEVICE := onyx
+PRODUCT_MANUFACTURER := OnePlus
+
+PRODUCT_GMS_CLIENTID_BASE := android-oneplus
+
+PRODUCT_BRAND := OnePlus
+TARGET_VENDOR := oneplus
+TARGET_VENDOR_PRODUCT_NAME := onyx
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+-include vendor/aicp/configs/bootanimation.mk
