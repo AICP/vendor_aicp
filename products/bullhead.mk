50f1b9a84552b187dfb7c1ad7efc3a95afd9e698
Akhil Narang <akhilnarang.1999@gmail.com>
2016-03-10 09:24:23 +0200

    bullhead: update fingerprint from MMB29V
    Change-Id: I0081751fe21710dcd31ec65e6109ad326553d1e2

--- products/bullhead.mk
+++ products/bullhead.mk
@@ -19,9 +19,9 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=bullhead \
-    BUILD_FINGERPRINT=google/bullhead/bullhead:6.0.1/MMB29Q/2480792:user/release-keys \
-    PRIVATE_BUILD_DESC="bullhead-user 6.0.1 MMB29Q 2480792 release-keys" \
-    BUILD_ID=MMB29Q
+    BUILD_FINGERPRINT=google/bullhead/bullhead:6.0.1/MMB29V/2554798:user/release-keys \
+    PRIVATE_BUILD_DESC="bullhead-user 6.0.1 MMB29V 2554798 release-keys" \
+    BUILD_ID=MMB29V
 
 # AICP Device Maintainers
 PRODUCT_BUILD_PROP_OVERRIDES += \
