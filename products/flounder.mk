02191dcf49522ed39a106cbd9b6855fb630bbd06
HashBang <hashbang173@gmail.com>
2016-03-11 14:33:08 -0500

    flounder: update fingerprint for MMB29V
    Change-Id: Ic088c0530f605ad6e40fd30e2e7d25155dd986e4

--- products/flounder.mk
+++ products/flounder.mk
@@ -42,9 +42,9 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=flounder \
-    BUILD_FINGERPRINT=google/volantis/flounder:6.0.1/MMB29R/2482564:user/release-keys \
-    PRIVATE_BUILD_DESC="volantis-user 6.0.1 MMB29R 2482564 release-keys" \
-    BUILD_ID=MMB29R
+    BUILD_FINGERPRINT=google/volantis/flounder:6.0.1/MMB29V/2554798:user/release-keys \
+    PRIVATE_BUILD_DESC="volantis-user 6.0.1 MMB29V 2554798 release-keys" \
+    BUILD_ID=MMB29V
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2048
