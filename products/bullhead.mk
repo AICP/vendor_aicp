cf9b80cd4cc2079faf74e136cfbe2b6aa7fad176
Akhil Narang <akhilnarang.1999@gmail.com>
2016-03-16 19:04:27 +0200

    bullhead: update fingerprint to MHC19J
    Change-Id: I1399890fde06ab82fb9741ce6b9f2f8ca4ac76cc
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- products/bullhead.mk
+++ products/bullhead.mk
@@ -19,9 +19,9 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=bullhead \
-    BUILD_FINGERPRINT=google/bullhead/bullhead:6.0.1/MMB29V/2554798:user/release-keys \
-    PRIVATE_BUILD_DESC="bullhead-user 6.0.1 MMB29V 2554798 release-keys" \
-    BUILD_ID=MMB29V
+    BUILD_FINGERPRINT=google/bullhead/bullhead:6.0.1/MHC19J/2595691:user/release-keys \
+    PRIVATE_BUILD_DESC="bullhead-user 6.0.1 MHC19J 2595691 release-keys" \
+    BUILD_ID=MHC19J
 
 # AICP Device Maintainers
 PRODUCT_BUILD_PROP_OVERRIDES += \
