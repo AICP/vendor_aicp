02c568873cde21015b6d85ff84588b23eb6c3de4
Vatsal <koradiavatsal@gmail.com>
2016-03-19 00:49:33 +0530

    Surnia : Switch to AICP MM
    Change-Id: I88844759f1df7e4f485ef87815f2325e4eb6f6e6

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -56,6 +56,7 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/peregrine.mk \
     $(LOCAL_DIR)/shamu.mk \
     $(LOCAL_DIR)/sprout4.mk \
+    $(LOCAL_DIR)/surnia.mk \
     $(LOCAL_DIR)/thea.mk \
     $(LOCAL_DIR)/t0lte.mk \
     $(LOCAL_DIR)/tomato.mk \
---
+++ products/surnia.mk
@@ -0,0 +1,40 @@
+# Copyright (C) 2014 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+# Inherit AICP common stuff
+$(call inherit-product, vendor/aicp/configs/common.mk)
+
+# Inherit telephony stuff
+$(call inherit-product, vendor/aicp/configs/telephony.mk)
+
+$(call inherit-product, device/motorola/surnia/full_surnia.mk)
+
+# AICP Device Maintainers
+PRODUCT_BUILD_PROP_OVERRIDES += \
+        DEVICE_MAINTAINERS="Vatsal K (Vatsal)"
+
+# Boot animation
+TARGET_SCREEN_WIDTH := 540
+TARGET_SCREEN_HEIGHT := 960
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := surnia
+PRODUCT_NAME := aicp_surnia
+PRODUCT_BRAND := Motorola
+PRODUCT_MANUFACTURER := Motorola
+PRODUCT_RELEASE_NAME := surnia
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME="Moto E LTE (2nd gen)"
