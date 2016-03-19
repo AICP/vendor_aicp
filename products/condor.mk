71c8fc17265cf7d4ec1a426b847bb8cfc2525641
C-Aniruddh <aniruddh.chandratre@gmail.com>
2016-03-19 08:59:21 -0400

    condor: change maintainer
    Change-Id: I0256e8e4f0fb68fe1d8abe15a95cf865dd4fc8ad

--- products/condor.mk
+++ products/condor.mk
@@ -14,7 +14,7 @@ PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
 # AICP Device Maintainers
 PRODUCT_BUILD_PROP_OVERRIDES += \
-	DEVICE_MAINTAINERS="Parth B (TheStrix)"
+	DEVICE_MAINTAINERS="Aniruddh Chandratre (ace312)"
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 960
