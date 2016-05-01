be4de7bce4b0f353bf45272eeb654209f51b7ce9
Trafalgar-Square <schneller.demian@gmail.com>
2016-05-01 15:58:21 +0200

    vendor: create n7100.mk
    I will maintain this. I am the new CyanogenMod maintainer for this.
    
    Change-Id: I11e1030b3d238da1cb8f8442227bdc25d7feea53

--- products/n7100.mk
+++ products/n7100.mk
@@ -1,5 +1,5 @@
 # Release name
-PRODUCT_RELEASE_NAME := i9300
+PRODUCT_RELEASE_NAME := n7100
 
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/aicp/configs/common.mk)
@@ -11,17 +11,17 @@ $(call inherit-product, vendor/aicp/configs/telephony.mk)
 $(call inherit-product, vendor/aicp/configs/nfc_enhanced.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/samsung/i9300/full_i9300.mk)
+$(call inherit-product, device/samsung/n7100/full_n7100.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := i9300
-PRODUCT_NAME := aicp_i9300
+PRODUCT_DEVICE := n7100
+PRODUCT_NAME := aicp_n7100
 PRODUCT_BRAND := samsung
-PRODUCT_MODEL := GT-I9300
+PRODUCT_MODEL := GT-N7100
 PRODUCT_MANUFACTURER := samsung
 
 # Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=m0xx TARGET_DEVICE=m0 BUILD_FINGERPRINT="samsung/m0xx/m0:4.3/JSS15J/I9300XXUGMJ9:user/release-keys" PRIVATE_BUILD_DESC="m0xx-user 4.3 JSS15J I9300XXUGMJ9 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=m0xx TARGET_DEVICE=m0 BUILD_FINGERPRINT="samsung/t03gxx/t03g:4.3/JSS15J/N7100XXUEMK4:user/release-keys" PRIVATE_BUILD_DESC="t03gxx-user 4.3 JSS15J N7100XXUEMK4 release-keys"
 
 # AICP Device Maintainers
 PRODUCT_BUILD_PROP_OVERRIDES += \
