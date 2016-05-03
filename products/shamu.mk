3b61edf1b79c5a2ad7b9b969ddd002378f7eabcb
doc HD <doc.divxm@gmail.com>
2016-05-03 22:50:17 +0300

    shamu: Update fingerprint to MOB30I
    Change-Id: I2a8ccb8803b03fec98d012c71725e0dffb59124f

--- products/shamu.mk
+++ products/shamu.mk
@@ -21,8 +21,8 @@ TARGET_VENDOR := moto
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=shamu \
-    BUILD_FINGERPRINT=google/shamu/shamu:6.0.1/MOB30D/2704746:user/release-keys \
-    PRIVATE_BUILD_DESC="shamu-user 6.0.1 MOB30D 2704746 release-keys"
+    BUILD_FINGERPRINT=google/shamu/shamu:6.0.1/MOB30I/2756745:user/release-keys \
+    PRIVATE_BUILD_DESC="shamu-user 6.0.1 MOB30I 2756745 release-keys"
 
 # AICP Device Maintainers
 PRODUCT_BUILD_PROP_OVERRIDES += \
