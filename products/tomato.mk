6fdd0463343b0e4f17a29c3fd7442b7e66cb7171
Ethan Chen <intervigil@gmail.com>
2016-05-25 06:01:00 +0530

    products: tomato: Update to match msm8916-common
    * msm8916-common now supports msm8939 as well, which required some
      changes. Track those changes here.
    
    Change-Id: I9f9bc5c9fd0904ba563205c6ef85ad399582e155

--- products/tomato.mk
+++ products/tomato.mk
@@ -21,6 +21,9 @@ $(call inherit-product, vendor/aicp/configs/telephony.mk)
 # Inherit from device
 $(call inherit-product, device/yu/tomato/full_tomato.mk)
 
+# Must define platform variant before including any common things
+TARGET_BOARD_PLATFORM_VARIANT := msm8939
+
 PRODUCT_NAME := aicp_tomato
 BOARD_VENDOR := yu
 PRODUCT_DEVICE := tomato
