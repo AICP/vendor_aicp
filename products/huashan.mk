46ecad222c06f219998d70e953e4958bcc6d803c
AdrianDC <radian.dc@gmail.com>
2016-05-25 15:12:41 +0200

    vendor_aicp: Update the Huashan AICP makefile for Marshmallow
     * Prepare the merge of Huashan repos
    
    Change-Id: I0cd2b0d33d3f9337bc128656a1e88ebb444e219f
    Signed-off-by: AdrianDC <radian.dc@gmail.com>

--- products/huashan.mk
+++ products/huashan.mk
@@ -1,4 +1,4 @@
-# Inherit AICP common Phone stuff.
+# Inherit AICP common bits
 $(call inherit-product, vendor/aicp/configs/common.mk)
 
 # Inherit telephony stuff
@@ -7,22 +7,26 @@ $(call inherit-product, vendor/aicp/configs/telephony.mk)
 # Enhanced NFC
 $(call inherit-product, vendor/aicp/configs/nfc_enhanced.mk)
 
-$(call inherit-product, device/sony/huashan/full_huashan.mk)
+# Inherit device configuration for huashan
+$(call inherit-product, device/sony/huashan/huashan.mk)
+
+# Device display
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+
+# Device identification
+PRODUCT_DEVICE := huashan
+PRODUCT_NAME := aicp_huashan
+PRODUCT_BRAND := Sony
+PRODUCT_MANUFACTURER := Sony
+PRODUCT_MODEL := Xperia SP
 
 # Build fingerprints
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C5303 BUILD_FINGERPRINT=Sony/C5303/C5303:4.3/12.1.A.1.207/Nvt_nw:user/release-keys PRIVATE_BUILD_DESC="C5303-user 4.3 JB-MR2-VISKAN-140318-1014 227 test-keys"
-
-PRODUCT_NAME := aicp_huashan
-PRODUCT_BRAND := Sony
-PRODUCT_DEVICE := huashan
-PRODUCT_MODEL := Xperia SP
-PRODUCT_MANUFACTURER := Sony
 
 # AICP Device Maintainers
 PRODUCT_BUILD_PROP_OVERRIDES += \
 	DEVICE_MAINTAINERS="Raienryu, Adrian DC"
 
 # Boot animation
-TARGET_SCREEN_HEIGHT := 1280
-TARGET_SCREEN_WIDTH := 720
 -include vendor/aicp/configs/bootanimation.mk
