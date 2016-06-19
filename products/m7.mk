5c9194909ad9518c55df6bcdc3f086b8cb491bb1
Matt Mower <mowerm@gmail.com>
2016-06-19 22:14:56 +0300

    m7: Clean up makefile inheritance/ordering [2/2]
    * part of http://review.cyanogenmod.org/#/c/150176
    
    Change-Id: I8b37962527a10f581c0f4154ea6a01f9cd5860d6

--- products/m7.mk
+++ products/m7.mk
@@ -1,3 +1,19 @@
+# Copyright (C) 2016 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+$(call inherit-product, device/htc/m7/full_m7.mk)
+
 $(call inherit-product, vendor/aicp/configs/common.mk)
 
 # Inherit telephony stuff
@@ -6,17 +22,18 @@ $(call inherit-product, vendor/aicp/configs/telephony.mk)
 # Enhanced NFC
 $(call inherit-product, vendor/aicp/configs/nfc_enhanced.mk)
 
-$(call inherit-product, device/htc/m7/full_m7.mk)
-
-
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_ID=LRX22C.H3 BUILD_FINGERPRINT="htc/m7_google/m7:5.0.1/LRX22C.H3/434277:user/release-keys" PRIVATE_BUILD_DESC="5.11.1700.3 CL434277 release-keys"
-
+# Device naming
 PRODUCT_NAME := aicp_m7
-PRODUCT_DEVICE := m7
+
+# Override build props
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT="htc/m7_google/m7:5.0.1/LRX22C.H3/434277:user/release-keys" \
+    BUILD_ID=LRX22C.H3 \
+    PRIVATE_BUILD_DESC="5.11.1700.3 CL434277 release-keys"
 
 # AICP Device Maintainers
 PRODUCT_BUILD_PROP_OVERRIDES += \
-	DEVICE_MAINTAINERS="Brian L (Chezbel), doc HD (semdoc)"
+    DEVICE_MAINTAINERS="Brian L (Chezbel), doc HD (semdoc)"
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1920
