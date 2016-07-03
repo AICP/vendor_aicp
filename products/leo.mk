3ff3d82a7e1992b14705fdb2064ecb3dc3f42e75
nian0114 <268078545@qq.com>
2016-07-03 15:43:20 -0400

    add leo config
    Change-Id: Ib00a00d78bf130f13c06ac8b7dd441d8dad398da

---
+++ products/leo.mk
@@ -0,0 +1,28 @@
+# Release name
+PRODUCT_RELEASE_NAME := leo
+
+# Inherit some common AICP stuff.
+$(call inherit-product, vendor/aicp/configs/common.mk)
+
+# Inherit telephony stuff
+$(call inherit-product, vendor/aicp/configs/telephony.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/xiaomi/leo/device_leo.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := leo
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Mi Note Pro
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_NAME := aicp_leo
+
+# AICP Device Maintainers
+PRODUCT_BUILD_PROP_OVERRIDES += \
+        DEVICE_MAINTAINERS="nian0114"
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2560
+TARGET_SCREEN_WIDTH := 1440
+
+-include vendor/aicp/configs/bootanimation.mk
