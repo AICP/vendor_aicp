ed75661791838bbb9374c4665de5ed15aafebe31
HashBang <hashbang173@gmail.com>
2016-07-07 10:09:19 -0400

    flounder: update finderprint to MOB30P
    Change-Id: I9ef0d06a8d5d99569c015bf1d79d58ab381e09c6

--- products/flounder.mk
+++ products/flounder.mk
@@ -42,9 +42,9 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=flounder \
-    BUILD_FINGERPRINT=google/volantis/flounder:6.0.1/MOB30M/2862625:user/release-keys \
-    PRIVATE_BUILD_DESC="volantis-user 6.0.1 MOB30M 2862625 release-keys" \
-    BUILD_ID=MOB30M
+    BUILD_FINGERPRINT=google/volantis/flounder:6.0.1/MOB30P/2960889:user/release-keys \
+    PRIVATE_BUILD_DESC="volantis-user 6.0.1 MOB30P 2960889 release-keys" \
+    BUILD_ID=MOB30P
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2048
