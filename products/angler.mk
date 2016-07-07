7003362865c1ebab0d212c20966430f7b0215178
doc HD <doc.divxm@gmail.com>
2016-07-07 23:38:47 +0300

    angler: Update fingerprint to MTC19X
    Change-Id: I25b06a2d56e802061afb812a7b659137f0023318

--- products/angler.mk
+++ products/angler.mk
@@ -21,8 +21,8 @@ TARGET_VENDOR := huawei
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=angler \
-    BUILD_FINGERPRINT=google/angler/angler:6.0.1/MTC19V/2862947:user/release-keys \
-    PRIVATE_BUILD_DESC="angler-user 6.0.1 MTC19V 2862947 release-keys"
+    BUILD_FINGERPRINT=google/angler/angler:6.0.1/MTC19X/2960136:user/release-keys \
+    PRIVATE_BUILD_DESC="angler-user 6.0.1 MTC19X 2960136 release-keys"
 
 # AICP Device Maintainers
 PRODUCT_BUILD_PROP_OVERRIDES += \
