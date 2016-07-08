90cb9032d07cd983c02ed47e914b0bb3b336c101
Abhisek Devkota <ciwrl@cyanogenmod.com>
2016-07-08 15:33:30 +0200

    Adapt to 6.0.1_r52 backup whitelist
    As of f/b 9b8c6d2df35455ce9e67907edded1e4a2ecb9e28
     we need to whitelist who can be backup services.
    
    Change-Id: I52a004924e6d2651c5ecdec51a8c5a80bdd54a38

--- configs/common.mk
+++ configs/common.mk
@@ -108,6 +108,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.dun.override=0
 
+# Backup Services whitelist
+PRODUCT_COPY_FILES += \
+    vendor/aicp/configs/permissions/backup.xml:system/etc/sysconfig/backup.xml
+
 # Backup Tool
 PRODUCT_COPY_FILES += \
     vendor/aicp/prebuilt/bin/backuptool.sh:install/bin/backuptool.sh \
