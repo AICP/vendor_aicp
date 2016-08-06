aa91748c135dbf81ad23dcddba0e84cb5ea813c6
Omkar Shinde <djomi400@gmail.com>
2016-08-06 11:35:16 +0000

    bullhead: update build fingerprint
    Change-Id: I63e560e547f9cc0b8f456d7519e5b579c206d7ba

--- products/bullhead.mk
+++ products/bullhead.mk
@@ -19,9 +19,9 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=bullhead \
-    BUILD_FINGERPRINT=google/bullhead/bullhead:6.0.1/MTC19Z/2996059:user/release-keys \
-    PRIVATE_BUILD_DESC="bullhead-user 6.0.1 MTC19Z 2996059 release-keys" \
-    BUILD_ID=MTC19Z
+    BUILD_FINGERPRINT=google/bullhead/bullhead:6.0.1/MTC20F/3031278:user/release-keys \
+    PRIVATE_BUILD_DESC="bullhead-user 6.0.1 MTC20F 3031278 release-keys" \
+    BUILD_ID=MTC20F
 
 # AICP Device Maintainers
 PRODUCT_BUILD_PROP_OVERRIDES += \
