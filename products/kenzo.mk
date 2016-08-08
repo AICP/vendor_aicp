ee56cd31131a5f53deca75130ef1121b301bbd1f
zeeshanhussain <zeeshanhussain12@gmail.com>
2016-08-08 20:36:23 +0530

    Kenzo: add bootanimation
    Change-Id: Id99fd99d685b93f3ca58274e18eb60a303a88263

--- products/kenzo.mk
+++ products/kenzo.mk
@@ -39,6 +39,11 @@ TARGET_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+-include vendor/aicp/configs/bootanimation.mk
+
 # AICP Device Maintainers
 PRODUCT_BUILD_PROP_OVERRIDES += \
         DEVICE_MAINTAINERS="Vatsal K (Vatsal)"
