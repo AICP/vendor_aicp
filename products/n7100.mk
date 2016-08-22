74b4736585a5978103439e3c19f7f06ae3f3a6c6
Trafalgar-Square <schneller.demian@gmail.com>
2016-08-22 17:57:26 +0200

    vendor: Update makefile for GT-N7100
    Change-Id: Iba73905cdb2c63513cc31eaa677e120dba494ac3

--- products/n7100.mk
+++ products/n7100.mk
@@ -13,6 +13,9 @@ $(call inherit-product, vendor/aicp/configs/nfc_enhanced.mk)
 # Inherit device configuration
 $(call inherit-product, device/samsung/n7100/full_n7100.mk)
 
+# Configure dalvik heap
+$(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
+
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := n7100
 PRODUCT_NAME := aicp_n7100
@@ -21,7 +24,9 @@ PRODUCT_MODEL := GT-N7100
 PRODUCT_MANUFACTURER := samsung
 
 # Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=m0xx TARGET_DEVICE=m0 BUILD_FINGERPRINT="samsung/t03gxx/t03g:4.3/JSS15J/N7100XXUEMK4:user/release-keys" PRIVATE_BUILD_DESC="t03gxx-user 4.3 JSS15J N7100XXUEMK4 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=m0xx TARGET_DEVICE=m0 BUILD_FINGERPRINT="samsung/t03gxx/t03g:4.4.2/KOT49H/N7100XXUFND3:user/release-keys" PRIVATE_BUILD_DESC="t03gxx-user 4.4.2 KOT49H N7100XXUFND3 release-keys" 
+PRODUCT_NAME=t03gxx \
+TARGET_DEVICE=t03g \
 
 # AICP Device Maintainers
 PRODUCT_BUILD_PROP_OVERRIDES += \
