42301f674455511f662b58046cd7ebc933f43c27
doc HD <doc.divxm@gmail.com>
2016-09-04 12:45:06 +0300

    angler: Update fingerprint to NRD90M
    Change-Id: Id00ae02dc7f72e672f8ff52cdc60016287beb9c3

--- products/angler.mk
+++ products/angler.mk
@@ -21,8 +21,8 @@ TARGET_VENDOR := huawei
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=angler \
-    BUILD_FINGERPRINT=google/angler/angler:6.0.1/MTC20F/3031278:user/release-keys \
-    PRIVATE_BUILD_DESC="angler-user 6.0.1 MTC20F 3031278 release-keys"
+    BUILD_FINGERPRINT="google/angler/angler:7.0/NRD90M/3085278:user/release-keys" \
+    PRIVATE_BUILD_DESC="angler-user 7.0 NRD90M 3085278 release-keys"
 
 # AICP Device Maintainers
 PRODUCT_BUILD_PROP_OVERRIDES += \
