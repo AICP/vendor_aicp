2ee2d48ade67635968c2d70d30cb35dcf31e7991
Trafalgar-Square <schneller.demian@gmail.com>
2016-09-30 14:58:50 +0200

    vendor_aicp: Add PRODUCT_MANUFACTURER and PRODUCT_BRAND to shieldtablet.mk
    Change-Id: Ib5121abac270c62d0de63279372268f9c4699675

--- products/shieldtablet.mk
+++ products/shieldtablet.mk
@@ -4,8 +4,13 @@ $(call inherit-product, vendor/aicp/configs/common.mk)
 # Inherit device configuration for wx_na_wf.
 $(call inherit-product, device/nvidia/shieldtablet/full_shieldtablet.mk)
 
+# Inherit telephony stuff
+$(call inherit-product, vendor/aicp/configs/telephony.mk)
+
 PRODUCT_NAME := aicp_shieldtablet
 PRODUCT_DEVICE := shieldtablet
+PRODUCT_MANUFACTURER := Nvidia
+PRODUCT_BRAND := Nvidia
 
 # AICP Device Maintainers
 PRODUCT_BUILD_PROP_OVERRIDES += \
