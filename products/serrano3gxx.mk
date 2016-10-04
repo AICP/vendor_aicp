b5148ff0b35df4686fb713f7b5566b19d46c2791
doc HD <doc.divxm@gmail.com>
2016-10-05 00:39:18 +0300

    serrano: get ready for AICP N
    Change-Id: I252dc97fe3e59b7cc212b9a3ce2de102f88c8388

--- products/serrano3gxx.mk
+++ products/serrano3gxx.mk
@@ -1,11 +1,28 @@
+# Copyright (C) 2016 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit AOSP device configuration for serrano3gxx.
+$(call inherit-product, device/samsung/serrano3gxx/device.mk)
+
 # Inherit common product files.
 $(call inherit-product, vendor/aicp/configs/common.mk)
 
 # Inherit telephony stuff
 $(call inherit-product, vendor/aicp/configs/telephony.mk)
-
-# Inherit AOSP device configuration for serrano3gxx.
-$(call inherit-product, device/samsung/serrano3gxx/full_serrano3gxx.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=serrano3gxx \
@@ -13,12 +30,16 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT="samsung/serrano3gxx/serrano3g:4.4.2/KOT49H/I9190XXUCNG1:user/release-keys" \
     PRIVATE_BUILD_DESC="serrano3gxx-user 4.4.2 KOT49H I9190XXUCNG1 release-keys"
 
-PRODUCT_DEVICE := serrano3gxx
+# Setup device specific product configuration.
 PRODUCT_NAME := aicp_serrano3gxx
+PRODUCT_BRAND := samsung
+PRODUCT_DEVICE := serrano3gxx
+PRODUCT_MODEL := GT-I9190
+PRODUCT_MANUFACTURER := Samsung
 
 # AICP Device Maintainers
 PRODUCT_BUILD_PROP_OVERRIDES += \
-	DEVICE_MAINTAINERS="doc HD (semdoc)"
+    DEVICE_MAINTAINERS="doc HD (semdoc)"
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 960
--- products/serranoltexx.mk
+++ products/serranoltexx.mk
@@ -1,3 +1,23 @@
+# Copyright (C) 2016 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit AOSP device configuration for serranoltexx.
+$(call inherit-product, device/samsung/serranoltexx/device.mk)
+
 # Inherit common product files.
 $(call inherit-product, vendor/aicp/configs/common.mk)
 
@@ -6,9 +26,6 @@ $(call inherit-product, vendor/aicp/configs/telephony.mk)
 
 # Enhanced NFC
 $(call inherit-product, vendor/aicp/configs/nfc_enhanced.mk)
-
-# Inherit AOSP device configuration for serranoltexx.
-$(call inherit-product, device/samsung/serranoltexx/full_serranoltexx.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=serranoltexx \
@@ -23,7 +40,7 @@ PRODUCT_MANUFACTURER := samsung
 
 # AICP Device Maintainers
 PRODUCT_BUILD_PROP_OVERRIDES += \
-	DEVICE_MAINTAINERS="doc HD (semdoc)"
+    DEVICE_MAINTAINERS="doc HD (semdoc)"
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 960
