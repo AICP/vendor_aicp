4624671bb066c0fd05f197cb7ff421037d421053
Trafalgar-Square <schneller.demian@gmail.com>
2016-10-06 17:55:59 +0200

    i9300: Get ready for AICP N
    Change-Id: I687a259b85a803e27c40fb7b3c288308dec4973b

--- products/i9300.mk
+++ products/i9300.mk
@@ -1,17 +1,19 @@
 # Release name
 PRODUCT_RELEASE_NAME := i9300
 
-# Inherit from our custom product configuration
-$(call inherit-product, vendor/aicp/configs/common.mk)
-
-# Inherit telephony stuff
-$(call inherit-product, vendor/aicp/configs/telephony.mk)
-
-# Enhanced NFC
-$(call inherit-product, vendor/aicp/configs/nfc_enhanced.mk)
-
 # Inherit device configuration
-$(call inherit-product, device/samsung/i9300/full_i9300.mk)
+$(call inherit-product, device/samsung/smdk4412-common/common.mk)
+$(call inherit-product, device/samsung/i9300/i9300.mk)
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+
+# Inherit some common AICP stuff.
+$(call inherit-product, vendor/aicp/configs/common.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := i9300
@@ -20,9 +22,13 @@ PRODUCT_BRAND := samsung
 PRODUCT_MODEL := GT-I9300
 PRODUCT_MANUFACTURER := samsung
 
-# Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=m0xx TARGET_DEVICE=m0 BUILD_FINGERPRINT="samsung/m0xx/m0:4.3/JSS15J/I9300XXUGMJ9:user/release-keys" PRIVATE_BUILD_DESC="m0xx-user 4.3 JSS15J I9300XXUGMJ9 release-keys"
-
+# Set build fingerprint / ID / Product Name etc.
+PRODUCT_BUILD_PROP_OVERRIDES += \
+		PRODUCT_NAME=m0xx \
+		TARGET_DEVICE=m0 \
+		BUILD_FINGERPRINT="samsung/m0xx/m0:4.3/JSS15J/I9300XXUGMJ9:user/release-keys" \
+		PRIVATE_BUILD_DESC="m0xx-user 4.3 JSS15J I9300XXUGMJ9 release-keys"
+		
 # AICP Device Maintainers
 PRODUCT_BUILD_PROP_OVERRIDES += \
 	DEVICE_MAINTAINERS="Trafalgar Square"
