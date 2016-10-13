81550a1c922bc26e09a683a5d9caee9b2447e77d
Trafalgar-Square <schneller.demian@gmail.com>
2016-10-13 13:43:52 +0200

    oneplus2: Get ready for AICP N
    Change-Id: Ieeddec072f02a017f382a784203b415ed34882c1

--- products/oneplus2.mk
+++ products/oneplus2.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2015 The CyanogenMod Project
+# Copyright (C) 2016 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,13 +12,34 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-$(call inherit-product, device/oneplus/oneplus2/full_oneplus2.mk)
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from oneplus2 device
+$(call inherit-product, device/oneplus/oneplus2/device.mk)
 
 # Inherit some common AICP stuff.
 $(call inherit-product, vendor/aicp/configs/common.mk)
 
-# Inherit telephony stuff
-$(call inherit-product, vendor/aicp/configs/telephony.mk)
+PRODUCT_NAME := aicp_oneplus2
+PRODUCT_DEVICE := oneplus2
+PRODUCT_MANUFACTURER := OnePlus
+PRODUCT_BRAND := OnePlus
+
+PRODUCT_GMS_CLIENTID_BASE := android-oneplus
+
+TARGET_VENDOR_PRODUCT_NAME := OnePlus2
+TARGET_VENDOR_DEVICE_NAME := OnePlus2
+PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=OnePlus2 PRODUCT_NAME=OnePlus2
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT=OnePlus/OnePlus2/OnePlus2:6.0.1/MMB29M/1447840920:user/release-keys \
+    PRIVATE_BUILD_DESC="OnePlus2-user 6.0.1 MMB29M 20 dev-keys"
+
+PRODUCT_SYSTEM_PROPERTY_BLACKLIST += ro.product.model
+
+TARGET_VENDOR := oneplus
 
 # AICP Device Maintainers
 PRODUCT_BUILD_PROP_OVERRIDES += \
