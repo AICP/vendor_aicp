30d5f1f954eb72d4e98fd15f9fa03c474ccaded9
doc HD <doc.divxm@gmail.com>
2016-10-14 00:41:34 +0300

    angler: Define TARGET_VENDOR
    Change-Id: Ibbf39fb505fc94a486fe5b0ac69ffb47d93a60b3

--- products/angler.mk
+++ products/angler.mk
@@ -1,22 +1,3 @@
-#
-# Copyright 2013 The Android Open Source Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-# Inherit from the common Open Source product configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
-$(call inherit-product, device/huawei/angler/aosp_angler.mk)
-
 # Inherit some common AICP stuff.
 $(call inherit-product, vendor/aicp/configs/common.mk)
 
@@ -26,6 +7,9 @@ $(call inherit-product, vendor/aicp/configs/telephony.mk)
 # Enhanced NFC
 $(call inherit-product, vendor/aicp/configs/nfc_enhanced.mk)
 
+# Inherit device configuration
+$(call inherit-product, device/huawei/angler/aosp_angler.mk)
+
 DEVICE_PACKAGE_OVERLAYS += device/huawei/angler/overlay-cm
 
 ## Device identifier. This must come after all inclusions
@@ -34,6 +18,8 @@ PRODUCT_DEVICE := angler
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 6P
 PRODUCT_MANUFACTURER := Huawei
+
+TARGET_VENDOR := huawei
 
 # Device Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
