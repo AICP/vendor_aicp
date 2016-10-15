3e1b163921ae59122da9f2c5119bda36f0ecbe01
Omkar <djomi400@gmail.com>
2016-10-15 19:57:01 +0000

    bullhead: Update build fingerprint for N
    Change-Id: Iecb67ee8a5a079b69cf6824aaf241b35eb003b74

--- products/bullhead.mk
+++ products/bullhead.mk
@@ -19,9 +19,9 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=bullhead \
-    BUILD_FINGERPRINT=google/bullhead/bullhead:6.0.1/MTC20F/3031278:user/release-keys \
-    PRIVATE_BUILD_DESC="bullhead-user 6.0.1 MTC20F 3031278 release-keys" \
-    BUILD_ID=MTC20F
+    BUILD_FINGERPRINT=google/bullhead/bullhead:7.0/NBD90W/3239497:user/release-keys \
+    PRIVATE_BUILD_DESC="bullhead-user 7.0 NBD90W 3239497 release-keys" \
+    BUILD_ID=NBD90W
 
 # AICP Device Maintainers
 PRODUCT_BUILD_PROP_OVERRIDES += \
