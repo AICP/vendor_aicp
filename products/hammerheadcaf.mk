1209bf8fb37f2f80848f7afb41caf76adfb926ee
droidfivex <droidfivex@gmail.com>
2016-10-30 04:32:07 +0000

    products: change hammerhead & hammerheadcaf maintainers
    * Remove Davor following the advice at http://gerrit.aicp-rom.com/#/c/29796/
    * Rename 5x to Izumi Inami (match with Google+)
    
    Change-Id: Id6b22455822a0f0dca6c35df74b5341c59e6d162
    Signed-off-by: droidfivex <droidfivex@gmail.com>

--- products/hammerheadcaf.mk
+++ products/hammerheadcaf.mk
@@ -25,7 +25,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 # AICP Device Maintainers
 PRODUCT_BUILD_PROP_OVERRIDES += \
-	DEVICE_MAINTAINERS="Davor B (LorD CloCKaN), 5x (droidfivex)"
+	DEVICE_MAINTAINERS="Izumi Inami (droidfivex)"
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1920
