b86ca6c3b7ad516d071fc093ccb471647faf3f1b
Omkar <djomi400@gmail.com>
2016-11-19 07:54:18 +0000

    bullhead: update fingerprint/description to N5D91L
    * #blamegoogle for 2 release in nov.
    
    Change-Id: I0dced11cf835a968fcabcb105052912471789627

--- products/bullhead.mk
+++ products/bullhead.mk
@@ -16,8 +16,8 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
   PRODUCT_NAME=bullhead \
-  BUILD_FINGERPRINT=google/bullhead/bullhead:7.0/NRD91N/3318108:user/release-keys \
-  PRIVATE_BUILD_DESC="bullhead-user 7.0 NRD91N 3318108 release-keys"
+  BUILD_FINGERPRINT=google/bullhead/bullhead:7.0/N5D91L/3425233:user/release-keys \
+  PRIVATE_BUILD_DESC="bullhead-user 7.0 N5D91L 3425233 release-keys"
 
 # AICP Device Maintainers
 PRODUCT_BUILD_PROP_OVERRIDES += \
