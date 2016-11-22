7faa378494031c7170f9a3d3f902649656e6e2cf
Hashbang173 <hashbang173@gmail.com>
2016-11-22 18:07:21 -0500

    flounder: use cyanogenmod_flounder_defconfig
    Change-Id: Ieac2225c5cefd0e7f46f55bc5a19827208cee008

--- products/flounder.mk
+++ products/flounder.mk
@@ -23,7 +23,7 @@ BOARD_NEEDS_VENDORIMAGE_SYMLINK := true
 KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-4.9/bin
 KERNEL_TOOLCHAIN_PREFIX := aarch64-linux-android-
 TARGET_KERNEL_SOURCE := kernel/htc/flounder
-TARGET_KERNEL_CONFIG := flounder_defconfig
+TARGET_KERNEL_CONFIG := cyanogenmod_flounder_defconfig
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_CMDLINE := androidboot.selinux=enforcing
 TARGET_PREBUILT_KERNEL := false
