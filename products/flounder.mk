c2fc8f52ad40f81e9e38738f39b452ca5e761124
Hashbang173 <hashbang173@gmail.com>
2016-12-05 17:59:18 -0500

    flounder: update fingerprint to NMF26F
    Change-Id: I65cb38ad300667a70eaed002ca6cfa2c4873d6cc

--- products/flounder.mk
+++ products/flounder.mk
@@ -50,9 +50,9 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=flounder \
-    BUILD_FINGERPRINT=google/volantis/flounder:7.0/NRD91N/3318108:user/release-keys \
-    PRIVATE_BUILD_DESC="volantis-user 7.0 NRD91N 3318108 release-keys" \
-    BUILD_ID=NRD91N
+    BUILD_FINGERPRINT=google/volantis/flounder:7.1.1/NMF26F/3425388:user/release-keys \
+    PRIVATE_BUILD_DESC="volantis-user 7.1.1 NMF26F 3425388 release-keys" \
+    BUILD_ID=NMF26F
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2048
