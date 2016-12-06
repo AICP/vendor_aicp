0d2da6bd33e4eebeeb7b0575bf1940b325f80c3f
Vatsal <koradiavatsal@gmail.com>
2016-12-06 20:05:19 +0530

    Reorganise a bit
    Change-Id: I1548ec0ff14a1215e570917739dee39c2d37577e

--- products/kenzo.mk
+++ products/kenzo.mk
@@ -19,14 +19,14 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
+# Inherit from kenzo device
+$(call inherit-product, device/xiaomi/kenzo/device.mk)
+
 # Inherit some common AICP stuff.
 $(call inherit-product, vendor/aicp/configs/common.mk)
 
 # Inherit telephony stuff
 $(call inherit-product, vendor/aicp/configs/telephony.mk)
-
-# Inherit from kenzo device
-$(call inherit-product, device/xiaomi/kenzo/device.mk)
 
 # Set those variables here to overwrite the inherited values.
 BOARD_VENDOR := Xiaomi
