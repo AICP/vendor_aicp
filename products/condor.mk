68cbde3fc7ab3565138e0b8d0854f350d6ee3dcd
Trafalgar Square <schneller.demian@gmail.com>
2016-12-16 13:13:17 +0100

    condor: Change maintainer
    Change-Id: Id75d928f311bdc17e63bd2ac8e8b9deb06a41e67

--- products/condor.mk
+++ products/condor.mk
@@ -14,7 +14,7 @@ PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
 # AICP Device Maintainers
 PRODUCT_BUILD_PROP_OVERRIDES += \
-   DEVICE_MAINTAINERS="Vatsal K (Vatsal)"
+   DEVICE_MAINTAINERS="Trafalgar Square"
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 960
