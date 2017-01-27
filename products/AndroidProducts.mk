315e550eef97a481cb1ec993b9d5adc49579eb0c
LorDClockaN <lordclockan@gmail.com>
2017-01-27 15:18:58 +0100

    Add marlin
    Change-Id: Ief84667a78facfe70026cec6ca2a3e1e80dbd359

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -64,6 +64,7 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/m7vzw.mk \
     $(LOCAL_DIR)/mako.mk \
     $(LOCAL_DIR)/manta.mk \
+    $(LOCAL_DIR)/marlin.mk \
     $(LOCAL_DIR)/nicki.mk \
     $(LOCAL_DIR)/oneplus2.mk \
     $(LOCAL_DIR)/oneplus3.mk \
---
+++ products/marlin.mk
@@ -0,0 +1,36 @@
+# Inherit some common AICP stuff.
+$(call inherit-product, vendor/aicp/configs/common.mk)
+
+# Inherit telephony stuff
+$(call inherit-product, vendor/aicp/configs/telephony.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/google/marlin/aosp_marlin.mk)
+
+-include device/google/marlin/marlin/device-lineage.mk
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := aicp_marlin
+PRODUCT_DEVICE := marlin
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Pixel XL
+PRODUCT_MANUFACTURER := HTC
+
+PRODUCT_RESTRICT_VENDOR_FILES := false
+
+# Device Fingerprint
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=marlin \
+    BUILD_FINGERPRINT="google/marlin/marlin:7.1.1/NMF26Q/3537182:user/release-keys" \
+    PRIVATE_BUILD_DESC="marlin-user 7.1.1 NMF26Q 3537182 release-keys"
+
+# AICP Device Maintainers
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    DEVICE_MAINTAINERS="Davor B (LorD ClockaN)"
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2560
+TARGET_SCREEN_WIDTH := 1440
+-include vendor/aicp/configs/bootanimation.mk
+
+$(call inherit-product-if-exists, vendor/google/marlin/marlin-vendor.mk)
