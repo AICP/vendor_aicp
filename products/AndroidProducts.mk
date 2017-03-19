5997094176aa66ef7975f54d111ebac3e89add57
mosimchah <mosimchah@gmail.com>
2017-03-18 21:29:08 -0400

    vendor_aicp add zl1 (leeco le pro 3)
    Change-Id: Ib1fc9cbb6b46339dce0bed9df9de9fe87ddc1586

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -112,4 +112,5 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/Z00A.mk \
     $(LOCAL_DIR)/Z00L.mk \
     $(LOCAL_DIR)/z3c.mk \
-    $(LOCAL_DIR)/Z00ED.mk 
+    $(LOCAL_DIR)/Z00ED.mk \
+    $(LOCAL_DIR)/zl1.mk
---
+++ products/zl1.mk
@@ -0,0 +1,55 @@
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017 The The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from zl1 device
+$(call inherit-product, device/leeco/zl1/device.mk)
+
+# Inherit some common AICP stuff.
+$(call inherit-product, vendor/aicp/configs/common.mk)
+
+# Inherit telephony stuff
+$(call inherit-product, vendor/aicp/configs/telephony.mk)
+
+PRODUCT_NAME := aicp_zl1
+PRODUCT_DEVICE := zl1
+PRODUCT_MANUFACTURER := LeEco
+PRODUCT_BRAND := LeEco
+
+PRODUCT_GMS_CLIENTID_BASE := android-leeco
+
+TARGET_VENDOR_PRODUCT_NAME := LePro3
+TARGET_VENDOR_DEVICE_NAME := le_zl1
+PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=le_zl1 PRODUCT_NAME=LePro3
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT=LeEco/le_zl1/LePro3:6.0.1/MMB29M/362280:user/release-keys \
+    PRIVATE_BUILD_DESC="LePro3-user 6.0.1 MMB29M 24 dev-keys"
+
+PRODUCT_SYSTEM_PROPERTY_BLACKLIST += ro.product.model
+
+TARGET_VENDOR := leeco
+
+# AICP Device Maintainers
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    DEVICE_MAINTAINERS="Moshe Barash (mosimchah)"
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+-include vendor/aicp/configs/bootanimation.mk
