a37d924dbf51ea7d21ee302ee9a75608a0d93c36
Suraj Das <surajdazz@gmail.com>
2017-04-02 17:19:27 +0000

    find7 : get ready for AICP
    Change-Id: I08482d51fcdc1a89ca83ce003fc415a42c98e70d

--- products/find7.mk
+++ products/find7.mk
@@ -1,19 +1,45 @@
-$(call inherit-product, vendor/aicp/configs/common.mk)
+# Copyright (C) 2014-2016 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Inherit framework first
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from find7 device
+$(call inherit-product, device/oppo/find7/find7.mk)
 
 # Inherit telephony stuff
 $(call inherit-product, vendor/aicp/configs/telephony.mk)
 
-$(call inherit-product, device/oppo/find7/full_find7.mk)
+# Inherit some common AICP stuff
+$(call inherit-product, vendor/aicp/configs/common.mk)
 
 PRODUCT_NAME := aicp_find7
 PRODUCT_DEVICE := find7
-PRODUCT_BRAND := Oppo
+PRODUCT_BRAND := OPPO
+PRODUCT_MANUFACTURER := OPPO
 PRODUCT_MODEL := Find7
-PRODUCT_MANUFACTURER := Oppo
+
+PRODUCT_GMS_CLIENTID_BASE := android-oppo
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_FINGERPRINT=4.4.2/KVT49L/1390465867:user/release-keys \
-    PRIVATE_BUILD_DESC="msm8974-user 4.4.2 KVT49L eng.root.20141017.144947 release-keys"
+    BUILD_FINGERPRINT=5.1.1/LMY47V/1441693976:user/release-keys \
+    PRIVATE_BUILD_DESC="msm8974-user 5.1.1 LMY47V 153 release-keys" \
+    TARGET_DEVICE="FIND7"
+
+# AICP Device Maintainers
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    DEVICE_MAINTAINERS="Suraj Das"
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1920
