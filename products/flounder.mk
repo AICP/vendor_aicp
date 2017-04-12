2bb1a35a3727c85d0a22d6fb9019cdd6dd97c44a
Hashbang173 <hashbang173@gmail.com>
2017-04-12 17:29:15 -0400

    flounder: update fingerprint to N4F26X
    Change-Id: Iba9c70c7e601bacf249c8ed78efb8ef9ea9d9489

--- products/flounder.mk
+++ products/flounder.mk
@@ -50,8 +50,8 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=flounder \
-    BUILD_FINGERPRINT=google/volantis/flounder:7.1.1/N4F26T/3687331:user/release-keys \
-    PRIVATE_BUILD_DESC="volantis-user 7.1.1 N4F26T 3687331 release-keys"
+    BUILD_FINGERPRINT=google/volantis/flounder:7.1.1/N4F26X/3821951:user/release-keys \
+    PRIVATE_BUILD_DESC="volantis-user 7.1.1 N4F26X 3821951 release-keys"
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2048
