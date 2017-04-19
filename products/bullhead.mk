fb3b4792807e86976d64b0ab84e87c623d5ec3a7
underscoremone <underscoremone@gmail.com>
2017-04-19 01:02:19 +0100

    bullhead: Update build fingerprint & description to N2G47F
    Change-Id: I85fef1ec1f4f3a4821770a93c3caad27d97cc196

--- products/bullhead.mk
+++ products/bullhead.mk
@@ -16,8 +16,8 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
   PRODUCT_NAME=bullhead \
-  BUILD_FINGERPRINT=google/bullhead/bullhead:7.1.1/N4F26T/3687331:user/release-keys \
-  PRIVATE_BUILD_DESC="bullhead-user 7.1.1 N4F26T 3687331 release-keys"
+  BUILD_FINGERPRINT=google/bullhead/bullhead:7.1.2/N2G47F/3769476:user/release-keys \
+  PRIVATE_BUILD_DESC="bullhead-user 7.1.2 N2G47F 3769476 release-keys"
 
 # AICP Device Maintainers
 PRODUCT_BUILD_PROP_OVERRIDES += \
