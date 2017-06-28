3f0d695188ac2728d69fa7df647a276b3df7c631
Ondrej Hlavac <hlavaco@gmail.com>
2017-06-28 19:55:39 +0200

    amami: update fingerprint to .236
    Change-Id: If0b9e01250dff1482a1ebda2f16154857752d961

--- products/amami.mk
+++ products/amami.mk
@@ -6,7 +6,9 @@ $(call inherit-product, vendor/aicp/configs/telephony.mk)
 
 $(call inherit-product, device/sony/amami/aicp.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=D5503 BUILD_FINGERPRINT=Sony/D5503/D5503:4.4.4/14.4.A.0.157/ev__jQ:user/release-keys PRIVATE_BUILD_DESC="D5503-user 4.4.4 14.4.A.0.157 ev__jQ release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=D5503
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=Sony/D5503/D5503:5.1.1/14.6.A.1.236/2031203603:user/release-keys
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="D5503-user 5.1.1 14.6.A.1.236 2031203603 release-keys"
 
 # Setup device specific product configuration.
 PRODUCT_NAME := aicp_amami
