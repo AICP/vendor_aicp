34e800f8518fb9e3892bd7a9b317e43cd22df0fa
razorloves <razorloves@gmail.com>
2017-08-08 09:05:53 +0300

    bullhead: update build fingerprint & description to N2G48C
    Change-Id: Iafb3096b214884b0810d16c496ddc8ad6650bf28

--- products/bullhead.mk
+++ products/bullhead.mk
@@ -16,12 +16,12 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
   PRODUCT_NAME=bullhead \
-    BUILD_FINGERPRINT="google/bullhead/bullhead:7.1.2/N2G47Z/4045513:user/release-keys" \
-    PRIVATE_BUILD_DESC="bullhead-user 7.1.2 N2G47Z 4045513 release-keys"
+    BUILD_FINGERPRINT="google/bullhead/bullhead:7.1.2/N2G48C/4104010:user/release-keys" \
+    PRIVATE_BUILD_DESC="bullhead-user 7.1.2 N2G48C 4104010 release-keys"
 
 # AICP Device Maintainers
 PRODUCT_BUILD_PROP_OVERRIDES += \
-  DEVICE_MAINTAINERS="Omkar Shinde (Megatron007),doc HD (semdoc)"
+  DEVICE_MAINTAINERS="doc HD (semdoc)"
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1920
