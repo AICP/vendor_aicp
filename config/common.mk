f5c4ec305c5882db8f4cf4262183d4ab07aba22c
viperusgit <viperusgit@gmail.com>
2017-08-30 19:56:21 -0500

    Add in dialer_experience.xml to fix google phone bitching at us
    Change-Id: Ia188841ea2a211acd7205c6836d2423040cc143c

--- config/common.mk
+++ config/common.mk
@@ -53,6 +53,10 @@ PRODUCT_COPY_FILES += \
     vendor/gzosp/prebuilt/common/etc/mkshrc:system/etc/mkshrc \
     vendor/gzosp/prebuilt/common/etc/sysctl.conf:system/etc/sysctl.conf
 
+# Fix Dialer
+PRODUCT_COPY_FILES +=  \
+    vendor/gzosp/prebuilt/common/sysconfig/dialer_experience.xml:system/etc/sysconfig/dialer_experience.xml 
+
 # Gzosp-specific startup services
 PRODUCT_COPY_FILES += \
     vendor/gzosp/prebuilt/common/etc/init.d/00banner:system/etc/init.d/00banner \
