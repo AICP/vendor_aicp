LineageOS/android_vendor_lineage

vendor/lineage: sdk rebrand: step 2: update file contents
Change-Id: Ib61b9b559c4b0e536979e5cb25fba50aa56c138d

build/core/config.mk
@@ -13,12 +13,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-CM_SRC_API_DIR := $(TOPDIR)prebuilts/cmsdk/api
-INTERNAL_CM_PLATFORM_API_FILE := $(TARGET_OUT_COMMON_INTERMEDIATES)/PACKAGING/cm_public_api.txt
-INTERNAL_CM_PLATFORM_REMOVED_API_FILE := $(TARGET_OUT_COMMON_INTERMEDIATES)/PACKAGING/cm_removed.txt
-FRAMEWORK_CM_PLATFORM_API_FILE := $(TOPDIR)vendor/cmsdk/api/cm_current.txt
-FRAMEWORK_CM_PLATFORM_REMOVED_API_FILE := $(TOPDIR)vendor/cmsdk/api/cm_removed.txt
-FRAMEWORK_CM_API_NEEDS_UPDATE_TEXT := $(TOPDIR)vendor/lineage/build/core/apicheck_msg_current.txt
+LINEAGE_SRC_API_DIR := $(TOPDIR)prebuilts/lineagesdk/api
+INTERNAL_LINEAGE_PLATFORM_API_FILE := $(TARGET_OUT_COMMON_INTERMEDIATES)/PACKAGING/lineage_public_api.txt
+INTERNAL_LINEAGE_PLATFORM_REMOVED_API_FILE := $(TARGET_OUT_COMMON_INTERMEDIATES)/PACKAGING/lineage_removed.txt
+FRAMEWORK_LINEAGE_PLATFORM_API_FILE := $(TOPDIR)lineage-sdk/api/lineage_current.txt
+FRAMEWORK_LINEAGE_PLATFORM_REMOVED_API_FILE := $(TOPDIR)lineage-sdk/api/lineage_removed.txt
+FRAMEWORK_LINEAGE_API_NEEDS_UPDATE_TEXT := $(TOPDIR)vendor/lineage/build/core/apicheck_msg_current.txt
 
 BUILD_MAVEN_PREBUILT := $(TOP)/vendor/lineage/build/core/maven_artifact.mk
 PUBLISH_MAVEN_PREBUILT := $(TOP)/vendor/lineage/build/core/maven_artifact_publish.mk

build/tasks/apicheck.mk
@@ -18,75 +18,75 @@
 #
 
 # skip api check for PDK buid
-ifeq (,$(filter true, $(WITHOUT_CHECK_API) $(TARGET_BUILD_PDK) $(TARGET_DISABLE_CMSDK)))
+ifeq (,$(filter true, $(WITHOUT_CHECK_API) $(TARGET_BUILD_PDK) $(TARGET_DISABLE_LINEAGESDK)))
 
-.PHONY: checkapi-cm
+.PHONY: checkapi-lineage
 
 # Run the checkapi rules by default.
-droidcore: checkapi-cm
+droidcore: checkapi-lineage
 
 # Validate against previous release platform sdk version api text within prebuilts
-cm_last_released_sdk_version := $(CM_PLATFORM_SDK_VERSION)
+lineage_last_released_sdk_version := $(LINEAGE_PLATFORM_SDK_VERSION)
 
-.PHONY: check-cm-public-api
-checkapi-cm : check-cm-public-api
+.PHONY: check-lineage-public-api
+checkapi-lineage : check-lineage-public-api
 
-.PHONY: update-cm-api
+.PHONY: update-lineage-api
 
-# INTERNAL_CM_PLATFORM_API_FILE is the one build by droiddoc.
-# Note that since INTERNAL_CM_PLATFORM_API_FILE  is the byproduct of api-stubs module,
-# (See vendor/cmsdk/Android.mk)
+# INTERNAL_LINEAGE_PLATFORM_API_FILE is the one build by droiddoc.
+# Note that since INTERNAL_LINEAGE_PLATFORM_API_FILE  is the byproduct of api-stubs module,
+# (See lineage-sdk/Android.mk)
 # we need to add api-stubs as additional dependency of the api check.
 
-$(INTERNAL_CM_PLATFORM_API_FILE): cm-api-stubs-docs
+$(INTERNAL_LINEAGE_PLATFORM_API_FILE): lineage-api-stubs-docs
 
 # Check that the API we're building hasn't broken the last-released
 # SDK version.
 $(eval $(call check-api, \
-    checkpublicapi-cm-last, \
-    $(CM_SRC_API_DIR)/$(cm_last_released_sdk_version).txt, \
-    $(INTERNAL_CM_PLATFORM_API_FILE), \
-    $(FRAMEWORK_CM_PLATFORM_REMOVED_API_FILE), \
-    $(INTERNAL_CM_PLATFORM_REMOVED_API_FILE), \
+    checkpublicapi-lineage-last, \
+    $(LINEAGE_SRC_API_DIR)/$(lineage_last_released_sdk_version).txt, \
+    $(INTERNAL_LINEAGE_PLATFORM_API_FILE), \
+    $(FRAMEWORK_LINEAGE_PLATFORM_REMOVED_API_FILE), \
+    $(INTERNAL_LINEAGE_PLATFORM_REMOVED_API_FILE), \
     -hide 2 -hide 3 -hide 4 -hide 5 -hide 6 -hide 24 -hide 25 -hide 26 -hide 27 \
     -error 7 -error 8 -error 9 -error 10 -error 11 -error 12 -error 13 -error 14 -error 15 \
     -error 16 -error 17 -error 18 , \
-    cat $(FRAMEWORK_CM_API_NEEDS_UPDATE_TEXT), \
-    check-cm-public-api, \
-    $(call doc-timestamp-for,cm-api-stubs) \
+    cat $(FRAMEWORK_LINEAGE_API_NEEDS_UPDATE_TEXT), \
+    check-lineage-public-api, \
+    $(call doc-timestamp-for,lineage-api-stubs) \
     ))
 
 # Check that the API we're building hasn't changed from the not-yet-released
 # SDK version.
 $(eval $(call check-api, \
-    checkpublicapi-cm-current, \
-    $(FRAMEWORK_CM_PLATFORM_API_FILE), \
-    $(INTERNAL_CM_PLATFORM_API_FILE), \
-    $(FRAMEWORK_CM_PLATFORM_REMOVED_API_FILE), \
-    $(INTERNAL_CM_PLATFORM_REMOVED_API_FILE), \
+    checkpublicapi-lineage-current, \
+    $(FRAMEWORK_LINEAGE_PLATFORM_API_FILE), \
+    $(INTERNAL_LINEAGE_PLATFORM_API_FILE), \
+    $(FRAMEWORK_LINEAGE_PLATFORM_REMOVED_API_FILE), \
+    $(INTERNAL_LINEAGE_PLATFORM_REMOVED_API_FILE), \
     -error 2 -error 3 -error 4 -error 5 -error 6 \
     -error 7 -error 8 -error 9 -error 10 -error 11 -error 12 -error 13 -error 14 -error 15 \
     -error 16 -error 17 -error 18 -error 19 -error 20 -error 21 -error 23 -error 24 \
     -error 25 -error 26 -error 27, \
-    cat $(FRAMEWORK_CM_API_NEEDS_UPDATE_TEXT), \
-    check-cm-public-api, \
-    $(call doc-timestamp-for,cm-api-stubs) \
+    cat $(FRAMEWORK_LINEAGE_API_NEEDS_UPDATE_TEXT), \
+    check-lineage-public-api, \
+    $(call doc-timestamp-for,lineage-api-stubs) \
     ))
 
-.PHONY: update-cm-public-api
-update-cm-public-api: $(INTERNAL_CM_PLATFORM_API_FILE) | $(ACP)
-	@echo "Copying cm_current.txt"
-	$(hide) $(ACP) $(INTERNAL_CM_PLATFORM_API_FILE) $(FRAMEWORK_CM_PLATFORM_API_FILE)
-	@echo "Copying cm_removed.txt"
-	$(hide) $(ACP) $(INTERNAL_CM_PLATFORM_REMOVED_API_FILE) $(FRAMEWORK_CM_PLATFORM_REMOVED_API_FILE)
+.PHONY: update-lineage-public-api
+update-lineage-public-api: $(INTERNAL_LINEAGE_PLATFORM_API_FILE) | $(ACP)
+	@echo "Copying lineage_current.txt"
+	$(hide) $(ACP) $(INTERNAL_LINEAGE_PLATFORM_API_FILE) $(FRAMEWORK_LINEAGE_PLATFORM_API_FILE)
+	@echo "Copying lineage_removed.txt"
+	$(hide) $(ACP) $(INTERNAL_LINEAGE_PLATFORM_REMOVED_API_FILE) $(FRAMEWORK_LINEAGE_PLATFORM_REMOVED_API_FILE)
 
-update-cm-api : update-cm-public-api
+update-lineage-api : update-lineage-public-api
 
-.PHONY: update-cm-prebuilts-latest-public-api
-current_sdk_release_text_file := $(CM_SRC_API_DIR)/$(cm_last_released_sdk_version).txt
+.PHONY: update-lineage-prebuilts-latest-public-api
+current_sdk_release_text_file := $(LINEAGE_SRC_API_DIR)/$(lineage_last_released_sdk_version).txt
 
-update-cm-prebuilts-latest-public-api: $(FRAMEWORK_CM_PLATFORM_API_FILE) | $(ACP)
-	@echo "Publishing cm_current.txt as latest API release"
-	$(hide) $(ACP) $(FRAMEWORK_CM_PLATFORM_API_FILE) $(current_sdk_release_text_file)
+update-lineage-prebuilts-latest-public-api: $(FRAMEWORK_LINEAGE_PLATFORM_API_FILE) | $(ACP)
+	@echo "Publishing lineage_current.txt as latest API release"
+	$(hide) $(ACP) $(FRAMEWORK_LINEAGE_PLATFORM_API_FILE) $(current_sdk_release_text_file)
 
 endif

config/common.mk
@@ -83,14 +83,14 @@ PRODUCT_COPY_FILES += \
 
 # This is Lineage!
 PRODUCT_COPY_FILES += \
-    vendor/lineage/config/permissions/com.cyanogenmod.android.xml:system/etc/permissions/com.cyanogenmod.android.xml
+    vendor/lineage/config/permissions/org.lineageos.android.xml:system/etc/permissions/org.lineageos.android.xml
 
 # Include Lineage audio files
 include vendor/lineage/config/lineage_audio.mk
 
-ifneq ($(TARGET_DISABLE_CMSDK), true)
-# CMSDK
-include vendor/lineage/config/cmsdk_common.mk
+ifneq ($(TARGET_DISABLE_LINEAGESDK), true)
+# LineageSDK
+include vendor/lineage/config/lineagesdk_common.mk
 endif
 
 # TWRP
@@ -106,7 +106,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     BluetoothExt \
     CMAudioService \
-    CMParts \
+    LineageParts \
     Development \
     Profiles \
     WeatherManagerService
@@ -126,7 +126,7 @@ PRODUCT_PACKAGES += \
 # Custom Lineage packages
 PRODUCT_PACKAGES += \
     AudioFX \
-    CMSettingsProvider \
+    LineageSettingsProvider \
     LineageSetupWizard \
     Eleven \
     ExactCalculator \

AICP/vendor_aicp

build/core/config.mk
@@ -13,12 +13,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-CM_SRC_API_DIR := $(TOPDIR)prebuilts/cmsdk/api
-INTERNAL_CM_PLATFORM_API_FILE := $(TARGET_OUT_COMMON_INTERMEDIATES)/PACKAGING/cm_public_api.txt
-INTERNAL_CM_PLATFORM_REMOVED_API_FILE := $(TARGET_OUT_COMMON_INTERMEDIATES)/PACKAGING/cm_removed.txt
-FRAMEWORK_CM_PLATFORM_API_FILE := $(TOPDIR)vendor/cmsdk/api/cm_current.txt
-FRAMEWORK_CM_PLATFORM_REMOVED_API_FILE := $(TOPDIR)vendor/cmsdk/api/cm_removed.txt
-FRAMEWORK_CM_API_NEEDS_UPDATE_TEXT := $(TOPDIR)vendor/aicp/build/core/apicheck_msg_current.txt
+LINEAGE_SRC_API_DIR := $(TOPDIR)prebuilts/lineage-sdk/api
+INTERNAL_LINEAGE_PLATFORM_API_FILE := $(TARGET_OUT_COMMON_INTERMEDIATES)/PACKAGING/lineage_public_api.txt
+INTERNAL_LINEAGE_PLATFORM_REMOVED_API_FILE := $(TARGET_OUT_COMMON_INTERMEDIATES)/PACKAGING/lineage_removed.txt
+FRAMEWORK_LINEAGE_PLATFORM_API_FILE := $(TOPDIR)lineage-sdk/api/lineage_current.txt
+FRAMEWORK_LINEAGE_PLATFORM_REMOVED_API_FILE := $(TOPDIR)lineage-sdk/api/lineage_removed.txt
+FRAMEWORK_LINEAGE_API_NEEDS_UPDATE_TEXT := $(TOPDIR)vendor/aicp/build/core/apicheck_msg_current.txt
 
 BUILD_MAVEN_PREBUILT := $(TOP)/vendor/aicp/build/core/maven_artifact.mk
 PUBLISH_MAVEN_PREBUILT := $(TOP)/vendor/aicp/build/core/maven_artifact_publish.mk

build/tasks/apicheck.mk
@@ -18,75 +18,75 @@
 #
 
 # skip api check for PDK buid
-ifeq (,$(filter true, $(WITHOUT_CHECK_API) $(TARGET_BUILD_PDK) $(TARGET_DISABLE_CMSDK)))
+ifeq (,$(filter true, $(WITHOUT_CHECK_API) $(TARGET_BUILD_PDK) $(TARGET_DISABLE_LINEAGE_SDK)))
 
-.PHONY: checkapi-cm
+.PHONY: checkapi-lineage
 
 # Run the checkapi rules by default.
-droidcore: checkapi-cm
+droidcore: checkapi-lineage
 
 # Validate against previous release platform sdk version api text within prebuilts
-cm_last_released_sdk_version := $(CM_PLATFORM_SDK_VERSION)
+lineage_last_released_sdk_version := $(LINEAGE_PLATFORM_SDK_VERSION)
 
-.PHONY: check-cm-public-api
-checkapi-cm : check-cm-public-api
+.PHONY: check-lineage-public-api
+checkapi-lineage : check-lineage-public-api
 
-.PHONY: update-cm-api
+.PHONY: update-lineage-api
 
-# INTERNAL_CM_PLATFORM_API_FILE is the one build by droiddoc.
-# Note that since INTERNAL_CM_PLATFORM_API_FILE  is the byproduct of api-stubs module,
-# (See vendor/cmsdk/Android.mk)
+# INTERNAL_LINEAGE_PLATFORM_API_FILE is the one build by droiddoc.
+# Note that since INTERNAL_LINEAGE_PLATFORM_API_FILE  is the byproduct of api-stubs module,
+# (See lineage-sdk/Android.mk)
 # we need to add api-stubs as additional dependency of the api check.
 
-$(INTERNAL_CM_PLATFORM_API_FILE): cm-api-stubs-docs
+$(INTERNAL_LINEAGE_PLATFORM_API_FILE): lineage-api-stubs-docs
 
 # Check that the API we're building hasn't broken the last-released
 # SDK version.
 $(eval $(call check-api, \
-    checkpublicapi-cm-last, \
-    $(CM_SRC_API_DIR)/$(cm_last_released_sdk_version).txt, \
-    $(INTERNAL_CM_PLATFORM_API_FILE), \
-    $(FRAMEWORK_CM_PLATFORM_REMOVED_API_FILE), \
-    $(INTERNAL_CM_PLATFORM_REMOVED_API_FILE), \
+    checkpublicapi-lineage-last, \
+    $(LINEAGE_SRC_API_DIR)/$(lineage_last_released_sdk_version).txt, \
+    $(INTERNAL_LINEAGE_PLATFORM_API_FILE), \
+    $(FRAMEWORK_LINEAGE_PLATFORM_REMOVED_API_FILE), \
+    $(INTERNAL_LINEAGE_PLATFORM_REMOVED_API_FILE), \
     -hide 2 -hide 3 -hide 4 -hide 5 -hide 6 -hide 24 -hide 25 -hide 26 -hide 27 \
     -error 7 -error 8 -error 9 -error 10 -error 11 -error 12 -error 13 -error 14 -error 15 \
     -error 16 -error 17 -error 18 , \
-    cat $(FRAMEWORK_CM_API_NEEDS_UPDATE_TEXT), \
-    check-cm-public-api, \
-    $(call doc-timestamp-for,cm-api-stubs) \
+    cat $(FRAMEWORK_LINEAGE_API_NEEDS_UPDATE_TEXT), \
+    check-lineage-public-api, \
+    $(call doc-timestamp-for,lineage-api-stubs) \
     ))
 
 # Check that the API we're building hasn't changed from the not-yet-released
 # SDK version.
 $(eval $(call check-api, \
-    checkpublicapi-cm-current, \
-    $(FRAMEWORK_CM_PLATFORM_API_FILE), \
-    $(INTERNAL_CM_PLATFORM_API_FILE), \
-    $(FRAMEWORK_CM_PLATFORM_REMOVED_API_FILE), \
-    $(INTERNAL_CM_PLATFORM_REMOVED_API_FILE), \
+    checkpublicapi-lineage-current, \
+    $(FRAMEWORK_LINEAGE_PLATFORM_API_FILE), \
+    $(INTERNAL_LINEAGE_PLATFORM_API_FILE), \
+    $(FRAMEWORK_LINEAGE_PLATFORM_REMOVED_API_FILE), \
+    $(INTERNAL_LINEAGE_PLATFORM_REMOVED_API_FILE), \
     -error 2 -error 3 -error 4 -error 5 -error 6 \
     -error 7 -error 8 -error 9 -error 10 -error 11 -error 12 -error 13 -error 14 -error 15 \
     -error 16 -error 17 -error 18 -error 19 -error 20 -error 21 -error 23 -error 24 \
     -error 25 -error 26 -error 27, \
-    cat $(FRAMEWORK_CM_API_NEEDS_UPDATE_TEXT), \
-    check-cm-public-api, \
-    $(call doc-timestamp-for,cm-api-stubs) \
+    cat $(FRAMEWORK_LINEAGE_API_NEEDS_UPDATE_TEXT), \
+    check-lineage-public-api, \
+    $(call doc-timestamp-for,lineage-api-stubs) \
     ))
 
-.PHONY: update-cm-public-api
-update-cm-public-api: $(INTERNAL_CM_PLATFORM_API_FILE) | $(ACP)
-	@echo "Copying cm_current.txt"
-	$(hide) $(ACP) $(INTERNAL_CM_PLATFORM_API_FILE) $(FRAMEWORK_CM_PLATFORM_API_FILE)
-	@echo "Copying cm_removed.txt"
-	$(hide) $(ACP) $(INTERNAL_CM_PLATFORM_REMOVED_API_FILE) $(FRAMEWORK_CM_PLATFORM_REMOVED_API_FILE)
+.PHONY: update-lineage-public-api
+update-lineage-public-api: $(INTERNAL_LINEAGE_PLATFORM_API_FILE) | $(ACP)
+	@echo "Copying lineage_current.txt"
+	$(hide) $(ACP) $(INTERNAL_LINEAGE_PLATFORM_API_FILE) $(FRAMEWORK_LINEAGE_PLATFORM_API_FILE)
+	@echo "Copying lineage_removed.txt"
+	$(hide) $(ACP) $(INTERNAL_LINEAGE_PLATFORM_REMOVED_API_FILE) $(FRAMEWORK_LINEAGE_PLATFORM_REMOVED_API_FILE)
 
-update-cm-api : update-cm-public-api
+update-lineage-api : update-lineage-public-api
 
-.PHONY: update-cm-prebuilts-latest-public-api
-current_sdk_release_text_file := $(CM_SRC_API_DIR)/$(cm_last_released_sdk_version).txt
+.PHONY: update-lineage-prebuilts-latest-public-api
+current_sdk_release_text_file := $(LINEAGE_SRC_API_DIR)/$(lineage_last_released_sdk_version).txt
 
-update-cm-prebuilts-latest-public-api: $(FRAMEWORK_CM_PLATFORM_API_FILE) | $(ACP)
-	@echo "Publishing cm_current.txt as latest API release"
-	$(hide) $(ACP) $(FRAMEWORK_CM_PLATFORM_API_FILE) $(current_sdk_release_text_file)
+update-lineage-prebuilts-latest-public-api: $(FRAMEWORK_LINEAGE_PLATFORM_API_FILE) | $(ACP)
+	@echo "Publishing lineage_current.txt as latest API release"
+	$(hide) $(ACP) $(FRAMEWORK_LINEAGE_PLATFORM_API_FILE) $(current_sdk_release_text_file)
 
 endif

config/common.mk
@@ -83,14 +83,14 @@ PRODUCT_COPY_FILES += \
 
 # This is AICP!
 PRODUCT_COPY_FILES += \
-    vendor/aicp/config/permissions/com.cyanogenmod.android.xml:system/etc/permissions/com.cyanogenmod.android.xml
+    vendor/aicp/config/permissions/org.lineageos.android.xml:system/etc/permissions/org.lineageos.android.xml
 
 # Include AICP audio files
 include vendor/aicp/config/aicp_audio.mk
 
-ifneq ($(TARGET_DISABLE_CMSDK), true)
-# CMSDK
-include vendor/aicp/config/cmsdk_common.mk
+ifneq ($(TARGET_DISABLE_LINEAGE_SDK), true)
+# Lineage SDK
+include vendor/aicp/config/lineage_sdk_common.mk
 endif
 
 # TWRP
@@ -106,7 +106,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     BluetoothExt \
     CMAudioService \
-    CMParts \
+    LineageParts \
     Development \
     Profiles \
     WeatherManagerService
@@ -126,7 +126,8 @@ PRODUCT_PACKAGES += \
 # Custom Lineage packages
 PRODUCT_PACKAGES += \
     AudioFX \
-    CMSettingsProvider \
+    LineageSettingsProvider \
+    LineageSetupWizard \
     Eleven \
     ExactCalculator \
     Jelly \