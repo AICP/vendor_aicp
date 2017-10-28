007edaa93cd0cc75e2fc993e983f34b2fa57fe24
Andersonmendes2016 <lge500meu@gmail.com>
2017-10-28 21:01:59 -0200

    build: Fix soong flags
    Change-Id: Ic754b8a13658ddc40620f985583bd1754b732b19

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -2,7 +2,9 @@ gzosp_soong:
 	$(hide) mkdir -p $(dir $@)
 	$(hide) (\
 	echo '{'; \
+        echo '"Gzosp": {'; \
 	echo '    "Has_legacy_camera_hal1": $(if $(filter true,$(TARGET_HAS_LEGACY_CAMERA_HAL1)),true,false),'; \
 	echo '    "Uses_media_extensions": $(if $(filter true,$(TARGET_USES_MEDIA_EXTENSIONS)),true,false),'; \
-	echo '    "Needs_text_relocations": $(if $(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)),true,false),'; \
+	echo '    "Needs_text_relocations": $(if $(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)),true,false)'; \
+        echo '},'; \
 	echo '') > $(SOONG_VARIABLES_TMP)
