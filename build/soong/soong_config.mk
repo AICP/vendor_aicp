0524c41f2514e220150872e7d338df727a397893
Rashed Abdel-Tawab <rashed@linux.com>
2017-10-31 20:01:35 -0700

    gzosp: Add TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY variable handling
    Change-Id: I6f1cbcd883e91c8c3724dbe53732c7e777e21657

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -2,11 +2,11 @@ gzosp_soong:
 	$(hide) mkdir -p $(dir $@)
 	$(hide) (\
 	echo '{'; \
-        echo '"Gzosp": {'; \
-    echo '    "Uses_generic_camera_parameter_library": $(if $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY),false,true),'; \
+	echo '"Gzosp": {'; \
+	echo '    "Uses_generic_camera_parameter_library": $(if $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY),false,true),'; \
 	echo '    "Specific_camera_parameter_library": "$(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY)",'; \
 	echo '    "Has_legacy_camera_hal1": $(if $(filter true,$(TARGET_HAS_LEGACY_CAMERA_HAL1)),true,false),'; \
 	echo '    "Uses_media_extensions": $(if $(filter true,$(TARGET_USES_MEDIA_EXTENSIONS)),true,false),'; \
 	echo '    "Needs_text_relocations": $(if $(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)),true,false)'; \
-        echo '},'; \
+	echo '},'; \
 	echo '') > $(SOONG_VARIABLES_TMP)
