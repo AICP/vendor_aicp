af7965948bc5fcf0a55ec6b9d3975a1878ec3c1c
Joey <joey@lineageos.org>
2018-01-20 14:06:47 +0100

    vendor: build berry overlay packages
    Change-Id: I5fe01de25947fe260936b3ce8ccb71d590cc3583
    Signed-off-by: Joey <joey@lineageos.org>

--- config/common.mk
+++ config/common.mk
@@ -152,6 +152,19 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     Exchange2
 
+# Berry styles
+PRODUCT_PACKAGES += \
+    LineageDarkTheme \
+    LineageBlackAccent \
+    LineageBlueAccent \
+    LineageBrownAccent \
+    LineageGreenAccent \
+    LineageOrangeAccent \
+    LineagePinkAccent \
+    LineagePurpleAccent \
+    LineageRedAccent \
+    LineageYellowAccent
+
 # Extra tools in AICP
 PRODUCT_PACKAGES += \
     7z \
