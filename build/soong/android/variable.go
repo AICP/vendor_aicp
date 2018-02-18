827d25981e67f43172da90e2869a9ba49f3c18b9
Adrian DC <radian.dc@gmail.com>
2018-02-18 14:51:05 +0100

    gzosp: Sort soong variables and configurations
    Change-Id: I4a191f8ce61123e30f780f8e39411a7723fb6197

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -11,19 +11,19 @@ type Product_variables struct {
 	Needs_text_relocations struct {
 		Cppflags []string
 	}
-	Uses_generic_camera_parameter_library struct {
-		Srcs []string
-	}
 	Target_shim_libs struct {
 		Cppflags []string
+	}
+	Uses_generic_camera_parameter_library struct {
+		Srcs []string
 	}
 }
 
 type ProductVariables struct {
-	Uses_generic_camera_parameter_library  *bool `json:",omitempty"`
-	Specific_camera_parameter_library  *string `json:",omitempty"`
 	Has_legacy_camera_hal1  *bool `json:",omitempty"`
 	Uses_media_extensions   *bool `json:",omitempty"`
 	Needs_text_relocations  *bool `json:",omitempty"`
+	Specific_camera_parameter_library  *string `json:",omitempty"`
 	Target_shim_libs  *string `json:",omitempty"`
+	Uses_generic_camera_parameter_library  *bool `json:",omitempty"`
 }
--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -3,11 +3,11 @@ gzosp_soong:
 	$(hide) (\
 	echo '{'; \
 	echo '"Gzosp": {'; \
-	echo '    "Uses_generic_camera_parameter_library": $(if $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY),false,true),'; \
-	echo '    "Specific_camera_parameter_library": "$(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY)",'; \
 	echo '    "Has_legacy_camera_hal1": $(if $(filter true,$(TARGET_HAS_LEGACY_CAMERA_HAL1)),true,false),'; \
 	echo '    "Uses_media_extensions": $(if $(filter true,$(TARGET_USES_MEDIA_EXTENSIONS)),true,false),'; \
-	echo '    "Needs_text_relocations": $(if $(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)),true,false)'; \
-	echo '    "Target_shim_libs": "$(subst $(space),:,$(TARGET_LD_SHIM_LIBS))"'; \
+	echo '    "Needs_text_relocations": $(if $(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)),true,false),'; \
+	echo '    "Specific_camera_parameter_library": "$(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY)",'; \
+	echo '    "Target_shim_libs": "$(subst $(space),:,$(TARGET_LD_SHIM_LIBS))",'; \
+	echo '    "Uses_generic_camera_parameter_library": $(if $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY),false,true)'; \
 	echo '},'; \
 	echo '') > $(SOONG_VARIABLES_TMP)
