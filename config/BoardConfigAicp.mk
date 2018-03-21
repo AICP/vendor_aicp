cba00b4266910355422adcd27b2f60f958d96375
Michael Bestas <mkbestas@lineageos.org>
2018-03-21 19:29:00 +0300

    lineage: Set TARGET_CRYPTFS_HW_PATH if HW crypto is enabled
    * So TWRP can use it
    
    Change-Id: Ibc1d9d943d6d4e4e5248f623cd06782335f8ae98

--- config/BoardConfigAicp.mk
+++ config/BoardConfigAicp.mk
@@ -2,3 +2,8 @@
 ifeq ($(WITH_AICP_CHARGER),true)
     BOARD_HAL_STATIC_LIBRARIES := libhealthd.aicp
 endif
+
+# QCOM HW crypto
+ifeq ($(TARGET_HW_DISK_ENCRYPTION),true)
+    TARGET_CRYPTFS_HW_PATH ?= vendor/qcom/opensource/cryptfs_hw
+endif
