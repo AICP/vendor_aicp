56dd87a012f2d9ac54ed571fbd0357dd3b14d57e
Zipsnet <hcolmenares@gmail.com>
2018-04-04 21:28:38 -0400

    aicp: Update vendor's script
    Change-Id: I69df851b1c170ca50c45bfc2436af2246f017add

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
-for combo in $(curl -s  https://raw.githubusercontent.com/AICP/vendor_jenkins/o8.1/aicp-{Sunday,Monday,Tuesday,Wednesday,Thursday,Friday,Saturday,nightly}-targets | sed -e 's/#.*$//' | grep o8.1 | awk {'print $2'})
+for combo in $(curl -s https://raw.githubusercontent.com/AICP/vendor_jenkins/o8.1/aicp-build-targets   | sed -e 's/#.*$//' | grep o8.1 | awk {'print $2'})
 do
     add_lunch_combo $combo
 done
