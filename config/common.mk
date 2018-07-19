f55075670bc9a42fe11222d9df0e0ae8040e96f9
Pranav Vashi <neobuddy89@gmail.com>
2018-07-19 23:20:02 +0300

    Berry: Oxygen accent [2/3]
    Change-Id: Ia9c9b499a5d07e37b0f516ab3e2fd6fb52fce432

--- config/common.mk
+++ config/common.mk
@@ -164,6 +164,7 @@ PRODUCT_PACKAGES += \
     LineageGreenAccent \
     LineageGreyAccent \
     LineageOrangeAccent \
+		LineageOxygenAccent \
     LineagePinkAccent \
     LineagePurpleAccent \
     LineageTealAccent \
