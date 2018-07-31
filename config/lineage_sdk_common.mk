c00402fc6ee8cbb046fae67dd793addfa6f46474
LuK1337 <priv.luk@gmail.com>
2018-07-31 21:48:53 +0200

    vendor: Copy org.lineageos.settings permission
    Change-Id: Ia662f7789f77bb9b1abe1198db97a5b1767cd3e6

--- config/lineage_sdk_common.mk
+++ config/lineage_sdk_common.mk
@@ -4,6 +4,7 @@ PRODUCT_COPY_FILES += \
     vendor/aicp/config/permissions/org.lineageos.livedisplay.xml:system/etc/permissions/org.lineageos.livedisplay.xml \
     vendor/aicp/config/permissions/org.lineageos.performance.xml:system/etc/permissions/org.lineageos.performance.xml \
     vendor/aicp/config/permissions/org.lineageos.profiles.xml:system/etc/permissions/org.lineageos.profiles.xml \
+    vendor/aicp/config/permissions/org.lineageos.settings.xml:system/etc/permissions/org.lineageos.settings.xml \
     vendor/aicp/config/permissions/org.lineageos.style.xml:system/etc/permissions/org.lineageos.style.xml \
     vendor/aicp/config/permissions/org.lineageos.trust.xml:system/etc/permissions/org.lineageos.trust.xml \
     vendor/aicp/config/permissions/org.lineageos.weather.xml:system/etc/permissions/org.lineageos.weather.xml
