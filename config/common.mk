219d1fee89681282ec0cdbb59805a9383475079b
Prema Chand Alugu <premaca@gmail.com>
2018-08-21 18:14:26 +0530

    vendor: Fix bootanimation zip evaluation
    Change-Id: I1de9f46710109cd1bbb8a8159f0283a7d6851a9c

--- config/common.mk
+++ config/common.mk
@@ -158,12 +158,12 @@ bootanimation_sizes := $(shell echo -e $(subst $(space),'\n',$(bootanimation_siz
 # find the appropriate size and set
 define check_and_set_bootanimation
 $(eval TARGET_BOOTANIMATION_NAME := $(shell \
-  if [ -z "$(TARGET_BOOTANIMATION_NAME)" ]; then
+  if [ -z "$(TARGET_BOOTANIMATION_NAME)" ]; then \
     if [ $(1) -le $(TARGET_BOOTANIMATION_SIZE) ]; then \
       echo $(1); \
       exit 0; \
-    fi;
-  fi;
+    fi; \
+  fi; \
   echo $(TARGET_BOOTANIMATION_NAME); ))
 endef
 $(foreach size,$(bootanimation_sizes), $(call check_and_set_bootanimation,$(size)))
