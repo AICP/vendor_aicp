909184bb0f305e3192ec5387eb3ac132dad1811a
Rashed Abdel-Tawab <rashed@linux.com>
2018-08-25 11:10:25 -0700

    gzosp: Move QC board variables earlier
    These variables are usually set in a device's BoardConfig, setting them
    in qcom_target is at the very end of the "configuration process" which
    results in them being unavailable to plenty of other configuration
    "things" (ex. soong namespaces or soong config). Move them to right
    after a device's BoardConfig has been found and loaded.
    
    Change-Id: Iddd731202d22ed3f8eb010197ce20d3c75a1f40a

--- config/BoardConfigGZOSP.mk
+++ config/BoardConfigGZOSP.mk
@@ -2,3 +2,7 @@
 #ifneq ($(WITH_CM_CHARGER),false)
 #    BOARD_HAL_STATIC_LIBRARIES := libhealthd.cm
 #endif
+
+ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
+include vendor/gzosp/config/BoardConfigQcom.mk
+endif
