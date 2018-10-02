a0bd054075fd1c62b1b0c6bc9235fb724fb15584
Akhil Narang <akhilnarang.1999@gmail.com>
2018-10-02 13:29:15 +0530

    aicp: repopick: pick only open changes in a topic
    This reduces the pointless spam saying that change X is already merged
    
    Change-Id: If84daeabf00602b1da30543b255097d9335b074f
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- build/tools/repopick.py
+++ build/tools/repopick.py
@@ -294,7 +294,7 @@ if __name__ == '__main__':
     if args.topic:
         for t in args.topic:
             # Store current topic to process for change_numbers
-            topic = fetch_query(args.gerrit, 'topic:{0}'.format(t))
+            topic = fetch_query(args.gerrit, 'status:open topic:{0}'.format(t))
             # Append topic to reviews, for later reference
             reviews += topic
             # Cycle through the current topic to get the change numbers
