LineageOS/android_vendor_lineage

Add rules for non platform apps that use hidden APIs
Change-Id: I483f00cc0735f47665c5cb61ed0a187fb4e7d655

config/common.mk
@@ -93,6 +93,10 @@ PRODUCT_COPY_FILES += \
     vendor/lineage/config/permissions/org.lineageos.android.xml:system/etc/permissions/org.lineageos.android.xml \
     vendor/lineage/config/permissions/privapp-permissions-lineage.xml:system/etc/permissions/privapp-permissions-lineage.xml
 
+# Hidden API whitelist
+PRODUCT_COPY_FILES += \
+    vendor/lineage/config/permissions/lineage-hiddenapi-package-whitelist.xml:system/etc/permissions/lineage-hiddenapi-package-whitelist.xml
+
 # Include Lineage audio files
 include vendor/lineage/config/lineage_audio.mk
 

AICP/vendor_aicp

config/common.mk
@@ -61,6 +61,10 @@ PRODUCT_COPY_FILES += \
     vendor/aicp/prebuilt/common/etc/mkshrc:system/etc/mkshrc \
     vendor/aicp/prebuilt/common/etc/sysctl.conf:system/etc/sysctl.conf
 
+# Hidden API whitelist
+PRODUCT_COPY_FILES += \
+    vendor/aicp/config/permissions/lineage-hiddenapi-package-whitelist.xml:system/etc/permissions/lineage-hiddenapi-package-whitelist.xml
+
 # Fix Dialer
 #PRODUCT_COPY_FILES +=  \
 #    vendor/aicp/prebuilt/common/sysconfig/dialer_experience.xml:system/etc/sysconfig/dialer_experience.xml