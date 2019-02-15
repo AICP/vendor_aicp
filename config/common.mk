65fccdb8f2a0a5f3bbd864c8278f8d65b6bb5704
Carlos Solano <csolano@gmail.com>
2019-02-15 14:41:02 +0100

    aicp: Add vim to the package
    Change-Id: I6b3e85d533c451d14f523934386b66c1b70b95d5

--- config/common.mk
+++ config/common.mk
@@ -176,6 +176,9 @@ PRODUCT_PACKAGES += \
     libffmpeg_omx \
     media_codecs_ffmpeg.xml
 
+PRODUCT_PACKAGES += \
+    vim
+
 PRODUCT_PROPERTY_OVERRIDES += \
     media.sf.omx-plugin=libffmpeg_omx.so \
     media.sf.extractor-plugin=libffmpeg_extractor.so
