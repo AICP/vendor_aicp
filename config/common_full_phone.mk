4b090e7a702801379f84ebfc1277aa656d224f91
Julian Veit <claymore1298@gmail.com>
2019-08-12 10:07:52 +0200

    vendor_aicp: LatinIME: bring dictionaries in line with Lineage
    Change-Id: If6030aa490bdc298009d458640d8894c102cd2d0

--- config/common_full_phone.mk
+++ config/common_full_phone.mk
@@ -2,10 +2,13 @@
 $(call inherit-product, vendor/aicp/config/common.mk)
 $(call inherit-product, vendor/aicp/config/common_apn.mk)
 
-# Telephony 
+# Telephony
 PRODUCT_PACKAGES += \
-    Stk 
+    Stk
 
 # SMS
 PRODUCT_PACKAGES += \
-	messaging
+    messaging
+
+# Include Lineage LatinIME dictionaries
+PRODUCT_PACKAGE_OVERLAYS += vendor/aicp/overlay/dictionaries
