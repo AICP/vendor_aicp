92a494c03550d4e2ae08982791fe19d90825ae1a
celtare21 <celtare21@gmail.com>
2019-08-24 12:36:54 +0000

    vendor: introduce TARGET_BUILD_WITHOUT_VENDOR and let it guard JamesDSP
    It only has issues on devices that don't built the vendor
    partition. Let others enjoy it.
    Also did some format optimizations.
    
    Signed-off-by: celtare21 <celtare21@gmail.com>
    Change-Id: I5d204930cb8d4bc0af1f184284569a5c0c396c64
    Signed-off-by: Julian Veit <claymore1298@gmail.com>

--- config/BoardConfigAICP.mk
+++ config/BoardConfigAICP.mk
@@ -2,13 +2,13 @@
 #ifneq ($(WITH_CM_CHARGER),false)
 #    BOARD_HAL_STATIC_LIBRARIES := libhealthd.cm
 #endif
-# things to be set on non-AB devices
-ifneq ($(TARGET_IS_AB_DEVICE),true)
-     TARGET_USE_JAMESDSP := true
+# things to be set on devices, which build vendor
+ifneq ($(TARGET_BUILD_WITHOUT_VENDOR),true)
+    TARGET_USE_JAMESDSP := true
 endif
 
 ifeq ($(TARGET_USE_MUSICFX),false)
-     TARGET_USE_NO_MUSICFX := true
+    TARGET_USE_NO_MUSICFX := true
 endif
 
 include vendor/aicp/config/BoardConfigKernel.mk
