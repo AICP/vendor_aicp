04fce9300482eec6c253e47b4686ef06dfaeb3a1
Rashed Abdel-Tawab <rashed@linux.com>
2019-09-01 17:44:22 -0700

    aicp: Add goldfish privapp whitelist and move to common aicp makefile
    Aicp GSIs now work.
    
    Test: lunch lineage_arm64-eng; m -j systemimage
    Change-Id: Ic2be5e9e2353b12a6cfd8f002dea0e72280b28ac

--- build/target/product/aicp_generic_target.mk
+++ build/target/product/aicp_generic_target.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2018-2019 The LineageOS Project
+# Copyright (C) 2019 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,8 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-$(call inherit-product, build/target/product/aosp_x86_64.mk)
+$(call inherit-product, vendor/aicp/config/common.mk)
 
-include vendor/aicp/build/target/product/aicp_generic_target.mk
+PRODUCT_COPY_FILES += \
+    device/generic/goldfish/data/etc/permissions/privapp-permissions-goldfish.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-goldfish.xml \
 
-PRODUCT_NAME := aicp_x86_64
+# Allow building otatools
+TARGET_FORCE_OTA_PACKAGE := true
