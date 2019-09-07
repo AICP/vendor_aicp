948f02de74cf8fb1b2d2857e640d933ca74b3dae
Rashed Abdel-Tawab <rashed@linux.com>
2019-09-06 17:35:16 -0700

    aicp: Remove deprecated vendorsetup.sh
    Change-Id: Ia789184526b4488c11f50490322dff29d2a935be

--- vendorsetup.sh
+++
@@ -1,4 +0,0 @@
-for combo in $(curl -s https://raw.githubusercontent.com/AICP/vendor_jenkins/p9.0/aicp-build-targets   | sed -e 's/#.*$//' | grep p9.0 | awk {'print $2'})
-do
-    add_lunch_combo $combo
-done
