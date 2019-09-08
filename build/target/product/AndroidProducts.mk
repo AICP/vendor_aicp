957002e29856e75d21a33daad817327f7c4bc84e
Tim Schumacher <timschumi@gmx.de>
2019-09-08 15:32:39 +0200

    Add generic aicp targets to lunch choices
    Change-Id: I80d77d7cdbdfa156e951003a218ba8a58d51b3d4

--- build/target/product/AndroidProducts.mk
+++ build/target/product/AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2018 The LineageOS Project
+# Copyright (C) 2018-2019 The aicpOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -19,3 +19,9 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aicp_arm64.mk \
     $(LOCAL_DIR)/aicp_x86.mk \
     $(LOCAL_DIR)/aicp_x86_64.mk
+
+COMMON_LUNCH_CHOICES := \
+    aicp_arm-userdebug \
+    aicp_arm64-userdebug \
+    aicp_x86-userdebug \
+    aicp_x86_64-userdebug
