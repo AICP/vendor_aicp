15294eb6c83c3993ebf7c6ef3b2bc8c9678d6462
Michele Bono <bono.michele94@gmail.com>
2020-01-29 17:58:13 +0100

    vendor: Fix props
    Change-Id: Iac1d772a13702f6fe683d339d462c3cc6d9ac5fc

---
+++ build/core/main_version.mk
@@ -0,0 +1,23 @@
+# Build fingerprint
+ifneq ($(BUILD_FINGERPRINT),)
+ADDITIONAL_BUILD_PROPERTIES += \
+    ro.build.fingerprint=$(BUILD_FINGERPRINT)
+endif
+
+# AICP System Version
+ADDITIONAL_BUILD_PROPERTIES += \
+    ro.aicp.display.version=$(AICP_VERSION) \
+    ro.aicp.buildtype=$(AICP_BUILDTYPE) \
+    ro.aicp.version.update=$(AICP_BRANCH)-$(VERSION) \
+    ro.modversion=$(AICP_VERSION) \
+    ro.aicp.version=$(VERSION)-$(AICP_BUILDTYPE)
+
+# AICP Stats
+ADDITIONAL_BUILD_PROPERTIES += \
+    ro.aicp.branch=$(AICP_BRANCH) \
+    ro.romstats.url=https://stats.aicp-rom.com/ \
+    ro.romstats.name=AICP \
+    ro.romstats.buildtype=$(AICP_BUILDTYPE) \
+    ro.romstats.version=$(VERSION) \
+    ro.romstats.tframe=1 \
+    ro.romstats.askfirst=1
