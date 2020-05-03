67e42478cb4541fa5ab4f87b39b46a880316df46
Michele Bono <bono.michele94@gmail.com>
2020-05-03 12:26:51 +0200

    vendor: Add StevenBlack source
    Change-Id: I89eedb7ad2a34b769b0a029dd1e29893a157f24f

--- adaway/generate.py
+++ adaway/generate.py
@@ -3,6 +3,22 @@
 from datetime import datetime
 import sys
 
+# Don't add these from adaway sources, but handle manually if needed
+BLACKLIST = [
+    "localhost",
+    "localhost.localdomain",
+    "local",
+    "broadcasthost",
+    "ip6-localhost",
+    "ip6-loopback",
+    "ip6-localnet",
+    "ip6-mcastprefix",
+    "ip6-allnodes",
+    "ip6-allrouters",
+    "ip6-allhosts",
+    "0.0.0.0",
+]
+
 def write_header(f, sources):
     f.write("# This hosts file has been generated on:\n# {}\n# This file is generated from the following sources:\n".format(datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
     for s in sources:
@@ -49,6 +65,8 @@ if __name__ == "__main__":
         redirects = sorted(list(set(redirects)))
         # Write it
         for redirect in redirects:
-            if redirect[1] == "localhost":
+            if redirect[1] in BLACKLIST:
+                continue
+            if redirect[0] != "127.0.0.1" and redirect[0] != "0.0.0.0":
                 continue
             fout.write("127.0.0.1 {}\n".format(redirect[1]))
