19ac50fd4d138e5608e88a34f0dd4c7d5e8267ae
LuK1337 <priv.luk@gmail.com>
2020-06-24 17:14:42 +0200

    vendor_aicp: config: Move default alarm/notification/ringtone sound props to /product
    * Allows us to override /vendor properties.
    
    Change-Id: Iea344d347d8f10094f04cfb4a0bf1d4352fb667b

--- google/GoogleAudio.mk
+++ google/GoogleAudio.mk
@@ -80,7 +80,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/ringtones/The_big_adventure.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/The_big_adventure.ogg \
     $(LOCAL_PATH)/ringtones/Zen_too.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/Zen_too.ogg
 
-PRODUCT_PROPERTY_OVERRIDES += \
+PRODUCT_PRODUCT_PROPERTIES += \
    ro.config.ringtone=The_big_adventure.ogg \
    ro.config.notification_sound=Duet.ogg \
    ro.config.alarm_alert=Sunshower.ogg
