80dd63f88df66b0852666de57baf9896e9f8da5b
George Zacharia <george.zcharia@gmail.com>
2020-07-31 16:51:08 +0530

    aicp: adblock: Allow overriding the input hosts file
    Because of the modular nature of the StevenBlack's hosts, the user may be tempted to modify the hosts to match personal preferences. Accommodate such changes.
    Retains the earlier behaviour to pass multiple files to be conbined.
    
    Also remove the shell script "middleman", earlier shell script was used to fetch the file versions, which now is handled within the python script itself
    
    Change-Id: I4a348f29c9dbec6eb9eafe387a14be7280ce2412

--- adaway/Android.mk
+++ adaway/Android.mk
@@ -1,7 +1,14 @@
+# Use 'export INPUT_ADBLOCK_FILE=<custom host file location>' before build/envsetup.sh to build custom adblock
+ifeq ($(INPUT_ADBLOCK_FILE),)
+    INPUT_ADBLOCK_FILE := external/hosts/hosts
+endif
+
+ADBLOCK_SCRIPT := vendor/aicp/adaway/generate.py
+
 TARGET_GENERATED_HOSTS_BLOCKER := $(TARGET_OUT_INTERMEDIATES)/ADBLOCK/hosts.aicp_adblock
 $(TARGET_GENERATED_HOSTS_BLOCKER): INTERMEDIATES := $(TARGET_OUT_INTERMEDIATES)/ADBLOCK
-$(TARGET_GENERATED_HOSTS_BLOCKER): vendor/aicp/adaway/generate.sh vendor/aicp/adaway/generate.py external/hosts/hosts
-	vendor/aicp/adaway/generate.sh $@
+$(TARGET_GENERATED_HOSTS_BLOCKER): $(ADBLOCK_SCRIPT) $(INPUT_ADBLOCK_FILE)
+	python3 $(ADBLOCK_SCRIPT) $@ $(INPUT_ADBLOCK_FILE)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := hosts.aicp_adblock
