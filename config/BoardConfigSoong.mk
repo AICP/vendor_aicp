12d0498df5b529d290a7a8edc54ebd8932c57cf5
Michele Bono <bono.michele94@gmail.com>
2020-10-05 18:50:15 +0200

    vendor: rename soong var
    Change-Id: I722b8c522c9fa28d3aa3a40b2120e73e2598eccf

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -15,13 +15,13 @@ EXPORT_TO_SOONG := \
 # Documentation here:
 # https://github.com/LineageOS/android_build_soong/commit/8328367c44085b948c003116c0ed74a047237a69
 
-SOONG_CONFIG_NAMESPACES += aicpVarsPlugin
+SOONG_CONFIG_NAMESPACES += lineageVarsPlugin
 
-SOONG_CONFIG_aicpVarsPlugin :=
+SOONG_CONFIG_lineageVarsPlugin :=
 
 define addVar
-  SOONG_CONFIG_aicpVarsPlugin += $(1)
-  SOONG_CONFIG_aicpVarsPlugin_$(1) := $$(subst ",\",$$($1))
+  SOONG_CONFIG_lineageVarsPlugin += $(1)
+  SOONG_CONFIG_lineageVarsPlugin_$(1) := $$(subst ",\",$$($1))
 endef
 
 $(foreach v,$(EXPORT_TO_SOONG),$(eval $(call addVar,$(v))))
