498ce7715550bf2b5a5c85ad26a1f0e17015a5c0
Joel Stein <js@funkturm.net>
2020-10-13 01:45:59 +0200

    backuptool.sh Update for R
    Change-Id: I704eb02ef1e4d510e8d1daf7330bb580db6e0a11

--- prebuilt/common/bin/backuptool.sh
+++ prebuilt/common/bin/backuptool.sh
@@ -6,7 +6,7 @@
 export C=/tmp/backupdir
 export SYSDEV="$(readlink -nf "$2")"
 export SYSFS="$3"
-export V=10
+export V=11
 
 # Scripts in /system/addon.d expect to find backuptool.functions in /tmp
 cp -f /tmp/install/bin/backuptool.functions /tmp
