31db06b4b2431b44bdb04e65f35c6af790048787
Volodymyr Zhdanov <wight554@gmail.com>
2020-10-22 01:33:24 +0300

    extract_utils: implement patchelf
    Change-Id: I406f70ef1ce9ec7a0998f77439b7d3fa3ec4e62a

--- build/tools/extract_utils.sh
+++ build/tools/extract_utils.sh
@@ -1263,6 +1263,10 @@ function oat2dex() {
         export CDEXCONVERTER="$AICP_ROOT"/prebuilts/tools-aicp/${HOST}-x86/bin/compact_dex_converter
     fi
 
+    if [ -z "$PATCHELF" ]; then
+        export PATCHELF="$AICP_ROOT"/prebuilts/tools-aicp/${HOST}-x86/bin/patchelf
+    fi
+
     # Extract existing boot.oats to the temp folder
     if [ -z "$ARCHES" ]; then
         echo "Checking if system is odexed and locating boot.oats..."
