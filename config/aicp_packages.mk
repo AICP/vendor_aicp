ea34cd51f925c7c3a773cf5ef09a89f22b12b451
Dyneteve <dyneteve@hentaios.com>
2020-12-13 00:57:11 +0900

    config: Build the textclassifier models
    Change-Id: I9d7d582ddc427998e68e668cc0375958325d8052

--- config/aicp_packages.mk
+++ config/aicp_packages.mk
@@ -68,6 +68,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     QuickAccessWallet
 
+# TextClassifier
+PRODUCT_PACKAGES += \
+    libtextclassifier_annotator_en_model \
+    libtextclassifier_annotator_universal_model \
+    libtextclassifier_actions_suggestions_universal_model \
+    libtextclassifier_lang_id_model
+
 # su
 PRODUCT_PACKAGES += \
     adb_root
