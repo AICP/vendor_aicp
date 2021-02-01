5ce13dc8827bc8cbe8589612a8d68bfe4edc5a6a
Danny Lin <danny@kdrag0n.dev>
2021-02-01 09:25:13 -0500

    vendor: More prop overrides for Google Play Services and SafetyNet
    - Add support for overriding product info
    
    Google Play Store will serve debug versions of their apps if it detects
    an aosp_<devicename> device name because they use AOSP device trees
    internally for testing and debugging.
    
    We also need to override the fingerprint and other information to match
    stock in order for Google SafetyNet to pass.
    
    ****************************************
    
    - Remove support for overriding product name
    
    Overriding the product name will no longer be supported; device trees
    should instead be debranded to make the product name match the stock
    device codename.
    
    ****************************************
    
    - Fake build tags as release-keys
    
    While SafetyNet doesn't check this property, some banking apps
    (including the RootBeer library) do and flag the device as rooted if
    test-keys is detected.
    
    Change-Id: I48d61be98b95deeb03a38b74f939ccedf8644a0b

--- build/core/main.mk
+++ build/core/main.mk
@@ -1,2 +1,19 @@
 # Include AICP versions
 include $(TOPDIR)vendor/aicp/build/core/main_version.mk
+
+# Override product info for Google Play Services and SafetyNet
+ifeq ($(PRODUCT_OVERRIDE_INFO),true)
+ADDITIONAL_BUILD_PROPERTIES += \
+    ro.build.tags=release-keys \
+    ro.build.flavor=$(PRODUCT_NAME)-user \
+    ro.build.fingerprint=$(PRODUCT_OVERRIDE_FINGERPRINT) \
+    ro.system.build.fingerprint=$(PRODUCT_OVERRIDE_FINGERPRINT) \
+    ro.product.build.fingerprint=$(PRODUCT_OVERRIDE_FINGERPRINT) \
+    ro.system_ext.build.fingerprint=$(PRODUCT_OVERRIDE_FINGERPRINT) \
+    ro.bootimage.build.fingerprint=$(PRODUCT_OVERRIDE_FINGERPRINT) \
+    ro.odm.build.fingerprint=$(PRODUCT_OVERRIDE_FINGERPRINT) \
+    ro.vendor.build.fingerprint=$(PRODUCT_OVERRIDE_FINGERPRINT)
+
+# Description needs special treatment because it contains spaces
+PRIVATE_BUILD_DESC := $(PRODUCT_OVERRIDE_DESC)
+endif
