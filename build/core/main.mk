d94f0abdb84e6f5ac3a3c1818729641d1751ab6d
Danny Lin <danny@kdrag0n.dev>
2021-04-15 22:36:39 -0700

    Only use stock build fingerprint for Google Play Services
    SafetyNet's CTS profile attestation checks whether Build.FINGERPRINT
    matches that of the device's stock OS, which has passed CTS testing.
    This means that we need to spoof the fingerprint for Google Play
    Services in order to pass SafetyNet.
    
    We used to set the real system build fingerprint to the stock one, but
    Android relies on each build having a unique fingerprint in order to
    clear the correct caches and update persistent state for system changes.
    On devices that no longer receive updates from the OEM, the build
    fingerprint never changes and Android doesn't account for updates
    correctly, which causes issues when updating without wiping data.
    Only spoofing the fingerprint for Google Play Services fixes this issue.
    
    Corresponding frameworks/base commit:
    "Spoof build fingerprint for Google Play Services"
    
    Change-Id: I6bc5bf4f93758e0fac6073957f051d69c1a1bc95

--- build/core/main.mk
+++ build/core/main.mk
@@ -6,13 +6,7 @@ ifeq ($(PRODUCT_OVERRIDE_INFO),true)
 ADDITIONAL_BUILD_PROPERTIES += \
     ro.build.tags=release-keys \
     ro.build.flavor=$(PRODUCT_NAME)-user \
-    ro.build.fingerprint=$(PRODUCT_OVERRIDE_FINGERPRINT) \
-    ro.system.build.fingerprint=$(PRODUCT_OVERRIDE_FINGERPRINT) \
-    ro.product.build.fingerprint=$(PRODUCT_OVERRIDE_FINGERPRINT) \
-    ro.system_ext.build.fingerprint=$(PRODUCT_OVERRIDE_FINGERPRINT) \
-    ro.bootimage.build.fingerprint=$(PRODUCT_OVERRIDE_FINGERPRINT) \
-    ro.odm.build.fingerprint=$(PRODUCT_OVERRIDE_FINGERPRINT) \
-    ro.vendor.build.fingerprint=$(PRODUCT_OVERRIDE_FINGERPRINT)
+    ro.build.stock_fingerprint=$(PRODUCT_OVERRIDE_FINGERPRINT) \
 
 # Description needs special treatment because it contains spaces
 PRIVATE_BUILD_DESC := $(PRODUCT_OVERRIDE_DESC)
