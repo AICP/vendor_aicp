b5bb3a1f48130a9fd50f0c289446d1912eddca57
Julian Veit <claymore1298@gmail.com>
2022-01-03 17:22:43 +0100

    aicp: s/-ADDITIONAL_BUILD_PROPERTIES/ADDITIONAL_SYSTEM_PROPERTIES
    Change-Id: I10e4974cc4649abfea61d3b9ba7715f56f1c795c

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -1,11 +1,11 @@
 # Build fingerprint
 ifneq ($(BUILD_FINGERPRINT),)
-ADDITIONAL_BUILD_PROPERTIES += \
+ADDITIONAL_SYSTEM_PROPERTIES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
 endif
 
 # AICP System Version
-ADDITIONAL_BUILD_PROPERTIES += \
+ADDITIONAL_SYSTEM_PROPERTIES += \
     ro.aicp.display.version=$(AICP_VERSION) \
     ro.aicp.buildtype=$(AICP_BUILDTYPE) \
     ro.aicp.version.update=$(AICP_BRANCH)-$(VERSION) \
@@ -13,12 +13,12 @@ ADDITIONAL_BUILD_PROPERTIES += \
     ro.aicp.version=$(VERSION)-$(AICP_BUILDTYPE)
 
 # additions for LOS-recovery
-ADDITIONAL_BUILD_PROPERTIES += \
+ADDITIONAL_SYSTEM_PROPERTIES += \
     ro.lineage.build.version=$(VERSION) \
     ro.lineage.version=-$(shell date +%Y%m%d)_$(shell date +%H%M%S)-
 
 # AICP Stats
-ADDITIONAL_BUILD_PROPERTIES += \
+ADDITIONAL_SYSTEM_PROPERTIES += \
     ro.aicp.branch=$(AICP_BRANCH) \
     ro.romstats.url=https://stats.aicp-rom.com/ \
     ro.romstats.name=AICP \
