f00dba9928c8c7738bc710a3abff42242d029f5d
Danny Lin <danny@kdrag0n.dev>
2022-02-25 23:06:45 +0300

    vendor_aicp: main.mk: Remove build flavor override
    Multiple conflicting values for the same property fails the build on
    Android 12, and we don't need SafetyNet to pass on userdebug/eng builds
    anyway.
    
    Signed-off-by: Semavi Ulusoy <doc.divxm@gmail.com>
    Change-Id: Id5414ab055ebfdf4146c0e20f74ed00bff17a2f3

--- build/core/main.mk
+++ build/core/main.mk
@@ -5,7 +5,6 @@ include $(TOPDIR)vendor/aicp/build/core/main_version.mk
 ifeq ($(PRODUCT_OVERRIDE_INFO),true)
 ADDITIONAL_SYSTEM_PROPERTIES += \
     ro.build.tags=release-keys \
-    ro.build.flavor=$(PRODUCT_NAME)-user \
     ro.build.stock_fingerprint=$(PRODUCT_OVERRIDE_FINGERPRINT) \
 
 # Description needs special treatment because it contains spaces
