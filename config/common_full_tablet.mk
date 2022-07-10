676463d7fa3daa2f0bcf69826ca56c3d5f5ebc9f
George Zacharia <george.zcharia@gmail.com>
2022-07-10 15:00:59 +0530

    config: Inherit from within AICP itself
    Yikes
    Change-Id: I9394c3821abd59d43fca38abc1ab9c6412338f5e

--- config/common_full_tablet.mk
+++ config/common_full_tablet.mk
@@ -1,11 +1,11 @@
 # Inherit full common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full.mk)
+$(call inherit-product, vendor/aicp/config/common_full.mk)
 
 # Required packages
 PRODUCT_PACKAGES += \
     LatinIME
 
 # Include Lineage LatinIME dictionaries
-PRODUCT_PACKAGE_OVERLAYS += vendor/lineage/overlay/dictionaries
+PRODUCT_PACKAGE_OVERLAYS += vendor/aicp/overlay/dictionaries
 
-$(call inherit-product, vendor/lineage/config/telephony.mk)
+$(call inherit-product, vendor/aicp/config/telephony.mk)
