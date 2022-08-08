LineageOS/android_vendor_lineage

config: Exclude LatinIME dictionaries from RRO overlays
Fixes: https://gitlab.com/LineageOS/issues/android/-/issues/4991
Change-Id: I7722ac86d9d3f02f393b2c120341db5a2b648ff5

config/common_full_phone.mk
@@ -7,6 +7,7 @@ PRODUCT_PACKAGES += \
 
 # Include Lineage LatinIME dictionaries
 PRODUCT_PACKAGE_OVERLAYS += vendor/lineage/overlay/dictionaries
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/lineage/overlay/dictionaries
 
 # Enable support of one-handed mode
 PRODUCT_PRODUCT_PROPERTIES += \

config/common_full_tablet.mk
@@ -8,5 +8,6 @@ PRODUCT_PACKAGES += \
 
 # Include Lineage LatinIME dictionaries
 PRODUCT_PACKAGE_OVERLAYS += vendor/lineage/overlay/dictionaries
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/lineage/overlay/dictionaries
 
 $(call inherit-product, vendor/lineage/config/telephony.mk)

config/common_full_tablet_wifionly.mk
@@ -8,3 +8,4 @@ PRODUCT_PACKAGES += \
 
 # Include Lineage LatinIME dictionaries
 PRODUCT_PACKAGE_OVERLAYS += vendor/lineage/overlay/dictionaries
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/lineage/overlay/dictionaries

AICP/vendor_aicp

config/common_full_phone.mk
@@ -7,6 +7,7 @@ PRODUCT_PACKAGES += \
 
 # Include AICP LatinIME dictionaries
 PRODUCT_PACKAGE_OVERLAYS += vendor/aicp/overlay/dictionaries
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/aicp/overlay/dictionaries
 
 # Enable support of one-handed mode
 PRODUCT_PRODUCT_PROPERTIES += \

config/common_full_tablet.mk
@@ -7,5 +7,6 @@ PRODUCT_PACKAGES += \
 
 # Include Lineage LatinIME dictionaries
 PRODUCT_PACKAGE_OVERLAYS += vendor/aicp/overlay/dictionaries
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/aicp/overlay/dictionaries
 
 $(call inherit-product, vendor/aicp/config/telephony.mk)

config/common_full_tablet_wifionly.mk
@@ -7,3 +7,4 @@ PRODUCT_PACKAGES += \
 
 # Include AICP LatinIME dictionaries
 PRODUCT_PACKAGE_OVERLAYS += vendor/aicp/overlay/dictionaries
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/aicp/overlay/dictionaries