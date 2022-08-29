0beaf0b984ed470c8830d01d627ba45aef72075c
Michael Bestas <mkbestas@lineageos.org>
2022-08-30 02:48:39 +0300

    config: Remove TARGET_IGNORES_FTP_PPTP_CONNTRACK_FAILURE
    This hasn't been ported after lineage-17.1
    
    Change-Id: I2aff9bb7bc17fa1444a904d47248d885268bf29b

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -32,7 +32,6 @@ SOONG_CONFIG_lineageGlobalVars += \
     bootloader_message_offset \
     camera_needs_client_info_lib \
     disable_postrender_cleanup \
-    ignores_ftp_pptp_conntrack_failure \
     needs_camera_boottime \
     needs_netd_direct_connect_rule \
     target_init_vendor_lib \
@@ -62,7 +61,6 @@ endif
 # Soong bool variables
 SOONG_CONFIG_lineageGlobalVars_camera_needs_client_info_lib := $(TARGET_CAMERA_NEEDS_CLIENT_INFO_LIB)
 SOONG_CONFIG_lineageGlobalVars_disable_postrender_cleanup := $(TARGET_DISABLE_POSTRENDER_CLEANUP)
-SOONG_CONFIG_lineageGlobalVars_ignores_ftp_pptp_conntrack_failure := $(TARGET_IGNORES_FTP_PPTP_CONNTRACK_FAILURE)
 SOONG_CONFIG_lineageGlobalVars_needs_camera_boottime := $(TARGET_CAMERA_BOOTTIME_TIMESTAMP)
 SOONG_CONFIG_lineageGlobalVars_needs_netd_direct_connect_rule := $(TARGET_NEEDS_NETD_DIRECT_CONNECT_RULE)
 SOONG_CONFIG_lineageGlobalVars_uses_egl_display_array := $(TARGET_USES_EGL_DISPLAY_ARRAY)
