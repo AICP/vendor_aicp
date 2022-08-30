29361ff3fe6c849da81b6c72d98d94c3a4feb0ed
Michael Bestas <mkbestas@lineageos.org>
2022-08-30 03:00:53 +0300

    config: Remove TARGET_DISABLE_POSTRENDER_CLEANUP
    This hasn't been ported after lineage-18.1
    
    Change-Id: Ib1bca251c6de7529c58ea2fb9b439e37b55be6c5

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -31,7 +31,6 @@ SOONG_CONFIG_lineageGlobalVars += \
     additional_gralloc_10_usage_bits \
     bootloader_message_offset \
     camera_needs_client_info_lib \
-    disable_postrender_cleanup \
     needs_camera_boottime \
     target_init_vendor_lib \
     target_ld_shim_libs \
@@ -59,7 +58,6 @@ endif
 
 # Soong bool variables
 SOONG_CONFIG_lineageGlobalVars_camera_needs_client_info_lib := $(TARGET_CAMERA_NEEDS_CLIENT_INFO_LIB)
-SOONG_CONFIG_lineageGlobalVars_disable_postrender_cleanup := $(TARGET_DISABLE_POSTRENDER_CLEANUP)
 SOONG_CONFIG_lineageGlobalVars_needs_camera_boottime := $(TARGET_CAMERA_BOOTTIME_TIMESTAMP)
 SOONG_CONFIG_lineageGlobalVars_uses_egl_display_array := $(TARGET_USES_EGL_DISPLAY_ARRAY)
 SOONG_CONFIG_lineageNvidiaVars_uses_nvidia_enhancements := $(NV_ANDROID_FRAMEWORK_ENHANCEMENTS)
