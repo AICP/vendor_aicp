2f4991f16c3470e9182ca55b7943889c3dc38470
Semavi Ulusoy <doc.divxm@gmail.com>
2022-09-01 17:47:01 +0300

    vendor_aicp: Disable TEMP some packages and cleanup
     -- disabled:
       AicpSetupWizard, OmniStyle, MusicFX, GameSpace, Phonograph
    
     -- removed: BluetoothExt, Email, Exchange2, SpareParts, LockClock,
       Basic, PhaseBeam, Launcher3Dark and invictrix fonts (we don't build them last 3 years)
    
     -- removed missing cli tools: 7z, lib7z, pigz, unrar
    
     -- Alphabetize !
    
    Change-Id: Ib01b14aafdb83f16374f5a890715e50399a640fe

--- config/aicp_packages.mk
+++ config/aicp_packages.mk
@@ -1,16 +1,71 @@
-# Required packages
+# AICP packages
 PRODUCT_PACKAGES += \
-    CellBroadcastReceiver \
-    SpareParts \
-#    LockClock
+    AicpExtras \
+    AicpThemesStub \
+    TilesWallpaper
+
+# AICP Ad-block
+PRODUCT_PACKAGES += \
+    hosts.aicp_adblock
+
+# AICP OTA
+ifneq ($(AICP_BUILDTYPE),UNOFFICIAL)
+PRODUCT_PACKAGES +=  \
+   Updater
+
+PRODUCT_COPY_FILES += \
+    vendor/aicp/prebuilt/common/etc/init/init.aicp-updater.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.aicp-updater.rc
+endif
+
+# A/B OTA Optimization
+ifneq ($(AB_OTA_PARTITIONS),)
+PRODUCT_PACKAGES += \
+    checkpoint_gc \
+    otapreopt_script
+endif
+
+# AicpSetupWizard
+#ifneq ($(WITH_GMS),true)
+#PRODUCT_PACKAGES += \
+#    AicpSetupWizard
+#endif
+
+# Bootanimation include
+PRODUCT_PACKAGES += \
+    bootanimation.zip
+
+# Custom off-mode charger
+PRODUCT_PACKAGES += \
+    charger_res_images
+
+ifeq ($(WITH_AICP_CHARGER),true)
+PRODUCT_PACKAGES += \
+    aicp_charger_res_images \
+    font_log.png \
+    libhealthd.aicp
+endif
+
+# OmniJaws
+PRODUCT_PACKAGES += \
+    OmniJaws
 
 # Optional packages
 PRODUCT_PACKAGES += \
     Aperture \
-    Basic \
-    PhaseBeam \
+    AppPredictionService \
+    Etar \
+    ExactCalculator \
+    Jelly \
     ThemePicker \
     WallpaperPicker2
+
+# Required packages
+PRODUCT_PACKAGES += \
+    CellBroadcastReceiver \
+
+# SystemUI plugins
+PRODUCT_PACKAGES += \
+    QuickAccessWallet
 
 # su
 PRODUCT_PACKAGES += \
@@ -21,55 +76,31 @@ PRODUCT_PACKAGES += \
     su
 endif
 
-# Extra Optional packages
-PRODUCT_PACKAGES += \
-    BluetoothExt \
-    Email \
-    Etar \
-    ExactCalculator \
-    Exchange2
-#    Launcher3Dark
-
-#AicpSetupWizard
-ifneq ($(WITH_GMS),true)
-PRODUCT_PACKAGES += \
-    AicpSetupWizard
-endif
-
 # MusicFX
-PRODUCT_PACKAGES += \
-    MusicFX
+# PRODUCT_PACKAGES += \
+#    MusicFX
 
 # Phonograph
-PRODUCT_PACKAGES += \
-    Phonograph
+# PRODUCT_PACKAGES += \
+#    Phonograph
 
-# Jelly
-PRODUCT_PACKAGES += \
-    Jelly
-
-PRODUCT_PACKAGES += \
-    GameSpace
-
-# OnDeviceAppPrediction
-PRODUCT_PACKAGES += \
-    AppPredictionService
-
-# OmniJaws
-PRODUCT_PACKAGES += \
-    OmniJaws
+# PRODUCT_PACKAGES += \
+#    GameSpace
 
 # OmniStyle
-PRODUCT_PACKAGES += \
-    OmniStyle
-
-# SystemUI plugins
-PRODUCT_PACKAGES += \
-    QuickAccessWallet
+# PRODUCT_PACKAGES += \
+#    OmniStyle
 
 # Config
 PRODUCT_PACKAGES += \
     SimpleDeviceConfig
+
+# Extra tools
+PRODUCT_PACKAGES += \
+    openvpn \
+    e2fsck \
+    mke2fs \
+    tune2fs
 
 # Filesystems tools
 PRODUCT_PACKAGES += \
@@ -84,79 +115,24 @@ PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
     system/%/libfuse-lite.so \
     system/%/libntfs-3g.so
 
-# Fonts packages
-#PRODUCT_PACKAGES += \
-#    invictrix-fonts
-
 # Extra tools
 PRODUCT_PACKAGES += \
-    openvpn \
-    e2fsck \
-    mke2fs \
-    tune2fs
-
-PRODUCT_PACKAGES += \
-    charger_res_images
-
-# Custom off-mode charger
-ifeq ($(WITH_AICP_CHARGER),true)
-PRODUCT_PACKAGES += \
-    aicp_charger_res_images \
-    font_log.png \
-    libhealthd.aicp
-endif
-
-# Extra tools
-PRODUCT_PACKAGES += \
-    7z \
+#    7z \
     bash \
     curl \
     getcap \
     htop \
-    lib7z \
+#    lib7z \
     libsepol \
     nano \
-    pigz \
+#    pigz \
     powertop \
     setcap \
-    unrar \
+#    unrar \
     unzip \
     vim \
     wget \
     zip
 
-#PRODUCT_PACKAGES += \
-#    AndroidDarkThemeOverlay \
-#    SettingsDarkThemeOverlay
-
-# A/B OTA Optimization
-ifneq ($(AB_OTA_PARTITIONS),)
-PRODUCT_PACKAGES += \
-    checkpoint_gc \
-    otapreopt_script
-endif
-
-# Bootanimation include
-PRODUCT_PACKAGES += \
-    bootanimation.zip
-
-# AICP OTA
-ifneq ($(AICP_BUILDTYPE),UNOFFICIAL)
-PRODUCT_PACKAGES +=  \
-   Updater
-
-PRODUCT_COPY_FILES += \
-    vendor/aicp/prebuilt/common/etc/init/init.aicp-updater.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.aicp-updater.rc
-endif
-
-# AICP packages
-PRODUCT_PACKAGES += \
-    AicpExtras \
-    AicpThemesStub \
-    TilesWallpaper
-
-# AICP Ad-block
-PRODUCT_PACKAGES += \
-    hosts.aicp_adblock
-
+# AICP overlays
 -include packages/overlays/AICP/product_packages.mk
