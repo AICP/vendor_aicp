2a62761e4d35f928c1747172051fb62c76c1371c
Semavi Ulusoy <doc.divxm@gmail.com>
2022-09-01 17:47:01 +0300

    vendor_aicp: Disable TEMP some packages and cleanup
     -- disabled:
       OmniStyle, GameSpace, Phonograph
    
     -- removed: SpareParts, LockClock, Basic, PhaseBeam, Launcher3Dark
       MusicFX and invictrix fonts (we don't build them last 3 years)
    
    Change-Id: Ib01b14aafdb83f16374f5a890715e50399a640fe

--- config/aicp_packages.mk
+++ config/aicp_packages.mk
@@ -1,13 +1,9 @@
 # Required packages
 PRODUCT_PACKAGES += \
     CellBroadcastReceiver \
-    SpareParts \
-#    LockClock
 
 # Optional packages
 PRODUCT_PACKAGES += \
-    Basic \
-    PhaseBeam \
     ThemePicker \
     WallpaperPicker2
 
@@ -27,7 +23,6 @@ PRODUCT_PACKAGES += \
     Etar \
     ExactCalculator \
     Exchange2
-#    Launcher3Dark
 
 #AicpSetupWizard
 ifneq ($(WITH_GMS),true)
@@ -35,19 +30,15 @@ PRODUCT_PACKAGES += \
     AicpSetupWizard
 endif
 
-# MusicFX
-PRODUCT_PACKAGES += \
-    MusicFX
-
 # Phonograph
-PRODUCT_PACKAGES += \
+# PRODUCT_PACKAGES += \
     Phonograph
 
 # Jelly
 PRODUCT_PACKAGES += \
     Jelly
 
-PRODUCT_PACKAGES += \
+# PRODUCT_PACKAGES += \
     GameSpace
 
 # OnDeviceAppPrediction
@@ -59,7 +50,7 @@ PRODUCT_PACKAGES += \
     OmniJaws
 
 # OmniStyle
-PRODUCT_PACKAGES += \
+# PRODUCT_PACKAGES += \
     OmniStyle
 
 # SystemUI plugins
@@ -69,10 +60,6 @@ PRODUCT_PACKAGES += \
 # Config
 PRODUCT_PACKAGES += \
     SimpleDeviceConfig
-
-# Fonts packages
-#PRODUCT_PACKAGES += \
-#    invictrix-fonts
 
 # Extra tools
 PRODUCT_PACKAGES += \
@@ -115,10 +102,6 @@ PRODUCT_PACKAGES += \
     vim \
     wget \
     zip
-
-#PRODUCT_PACKAGES += \
-#    AndroidDarkThemeOverlay \
-#    SettingsDarkThemeOverlay
 
 # A/B OTA Optimization
 ifneq ($(AB_OTA_PARTITIONS),)
