690a9f54c91fe9c8cc59b05acaf05177d9b1c0a1
Nolen Johnson <johnsonnolen@gmail.com>
2022-10-04 06:29:47 +0300

    lineage: common: Exclude ntfs-3g from artifact path requirement
    Change-Id: I6253ac0b326f86ba7fd78f6babb9ee2242ba9133

--- config/aicp_packages.mk
+++ config/aicp_packages.mk
@@ -76,6 +76,13 @@ PRODUCT_PACKAGES += \
     mkfs.ntfs \
     mount.ntfs
 
+PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
+    system/bin/fsck.ntfs \
+    system/bin/mkfs.ntfs \
+    system/bin/mount.ntfs \
+    system/%/libfuse-lite.so \
+    system/%/libntfs-3g.so
+
 # Fonts packages
 #PRODUCT_PACKAGES += \
 #    invictrix-fonts
