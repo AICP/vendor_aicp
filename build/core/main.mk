64e3e8f0eba8c382f40ce297332fb54d3c44b238
Michael Bestas <mkbestas@gmail.com>
2024-09-12 03:51:01 +0300

    build: Move version & legal properties to PRODUCT_SYSTEM_PROPERTIES
    ADDITIONAL_SYSTEM_PROPERTIES is being deprecated.
    No functional changes.
    
    Change-Id: I01fd45edcc6c700540d3b7352fc466e65677a44e

--- build/core/main.mk
+++ build/core/main.mk
@@ -3,7 +3,7 @@ include $(TOPDIR)vendor/aicp/build/core/main_version.mk
 
 # Override product info for Google Play Services and SafetyNet
 ifeq ($(PRODUCT_OVERRIDE_INFO),true)
-ADDITIONAL_SYSTEM_PROPERTIES += \
+PRODUCT_SYSTEM_PROPERTIES += \
     ro.build.tags=release-keys \
     ro.build.stock_fingerprint=$(PRODUCT_OVERRIDE_FINGERPRINT) \
 
