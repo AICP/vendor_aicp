872ffb1696eccbd473c89ce4106ab359064b74f7
micky387 <mickaelsaibi@free.fr>
2024-10-12 10:40:41 -0400

    envsetup.sh: Export LLVM_AOSP_PREBUILTS_VERSION with the current clang version from soong
    Needed for KERNEL_CLANG_VERSION if TARGET_KERNEL_CLANG_VERSION hasn't been set by the device config
    
    It's boring to change it on each Google change ...
    
    Change-Id: I1d682611cbf6c60d7ae6227b0b5afde591adf471

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -53,6 +53,9 @@ function mk_timer()
     return $ret
 }
 
+CLANG_VERSION=$(build/soong/scripts/get_clang_version.py)
+export LLVM_AOSP_PREBUILTS_VERSION="${CLANG_VERSION}"
+
 # check to see if the supplied product is one we can build
 function check_product()
 {
