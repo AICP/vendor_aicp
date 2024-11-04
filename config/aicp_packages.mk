da4340d492a484a245ddb4da3164400eda501256
LuK1337 <priv.luk@gmail.com>
2024-11-04 20:24:31 +0100

    config: Add text classifier files to allowed product artifacts
    Change-Id: Ia568e8ad8aec14784f1649ec4e14b961a3c17f91

--- config/aicp_packages.mk
+++ config/aicp_packages.mk
@@ -75,6 +75,12 @@ PRODUCT_PACKAGES += \
     libtextclassifier_actions_suggestions_universal_model \
     libtextclassifier_lang_id_model
 
+PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
+    system/etc/textclassifier/actions_suggestions.universal.model \
+    system/etc/textclassifier/lang_id.model \
+    system/etc/textclassifier/textclassifier.en.model \
+    system/etc/textclassifier/textclassifier.universal.model
+
 # su
 PRODUCT_PACKAGES += \
     adb_root
