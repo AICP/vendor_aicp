8b8263d93f450c0680f1ee3ecdca1a5f11553678
Yumi Yukimura <me.cafebabe@gmail.com>
2025-02-25 13:51:15 +0800

    tablet: Enable desktop windowing mode
    * This replaces the old freeform window management
    
    Change-Id: I8711af4d90662252a37bc717590d35651460ad25

--- config/tablet.mk
+++ config/tablet.mk
@@ -6,7 +6,3 @@ PRODUCT_PRODUCT_PROPERTIES += \
 
 # Tablet-specific overlay
 PRODUCT_PACKAGE_OVERLAYS += vendor/aicp/overlay/tablet
-
-# Freeform window management
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.software.freeform_window_management.xml
