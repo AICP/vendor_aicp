0aa3bf6ffe14fa9e3fd17e0b055f7e7397ad85f9
Yumi Yukimura <me.cafebabe@gmail.com>
2025-02-25 22:20:10 +0800

    config: tablet: Include tablet-specific overlay
    Change-Id: I8235aad467d643b06d0ad1dd2c7c0de24bdf4679

--- config/tablet.mk
+++ config/tablet.mk
@@ -4,6 +4,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/window_extensions.mk)
 PRODUCT_PRODUCT_PROPERTIES += \
     persist.settings.large_screen_opt.enabled=true
 
+# Tablet-specific overlay
+PRODUCT_PACKAGE_OVERLAYS += vendor/aicp/overlay/tablet
+
 # Freeform window management
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.software.freeform_window_management.xml
