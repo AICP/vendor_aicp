f07e7b8254ce111c66f3f112113743667ed4dc9c
Bruno Martins <bgcngm@gmail.com>
2025-04-30 21:48:40 +0100

    config: Deprecate TARGET_POWER_LIBPERFMGR_MODE_EXTENSION_LIB
    All users have migrated to soong_config_set.
    
    Change-Id: I55e7907253c85f6c0615ceb67be4638ad177b6b0

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -65,11 +65,6 @@ ifneq ($(TARGET_POWERSHARE_DISABLED),)
     $(call soong_config_set,lineage_powershare,powershare_disabled,$(TARGET_POWERSHARE_DISABLED))
 endif
 
-# Power HAL
-ifneq ($(TARGET_POWER_LIBPERFMGR_MODE_EXTENSION_LIB),)
-    $(call soong_config_set,power_libperfmgr,mode_extension_lib,$(TARGET_POWER_LIBPERFMGR_MODE_EXTENSION_LIB))
-endif
-
 # Recovery
 ifneq ($(BOOTLOADER_MESSAGE_OFFSET),)
     $(call soong_config_set,lineage_recovery,bootloader_message_offset,$(BOOTLOADER_MESSAGE_OFFSET))
