aa2db3b9b5969a08d8e54a35f84b89883ae3ec23
Bruno Martins <bgcngm@gmail.com>
2025-05-08 23:36:53 +0100

    config: Deprecate TARGET_POWERSHARE_* flags
    Change-Id: I8fc2be89d4bcb2ea7c253376ba656321eccda41b

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -54,17 +54,6 @@ ifneq ($(TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_TOGGLE),)
     $(call soong_config_set,lineage_health,charging_control_supports_toggle,$(TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_TOGGLE))
 endif
 
-# Lineage PowerShare HAL
-ifneq ($(TARGET_POWERSHARE_PATH),)
-    $(call soong_config_set,lineage_powershare,powershare_path,$(TARGET_POWERSHARE_PATH))
-endif
-ifneq ($(TARGET_POWERSHARE_ENABLED),)
-    $(call soong_config_set,lineage_powershare,powershare_enabled,$(TARGET_POWERSHARE_ENABLED))
-endif
-ifneq ($(TARGET_POWERSHARE_DISABLED),)
-    $(call soong_config_set,lineage_powershare,powershare_disabled,$(TARGET_POWERSHARE_DISABLED))
-endif
-
 # Recovery
 ifneq ($(BOOTLOADER_MESSAGE_OFFSET),)
     $(call soong_config_set,lineage_recovery,bootloader_message_offset,$(BOOTLOADER_MESSAGE_OFFSET))
