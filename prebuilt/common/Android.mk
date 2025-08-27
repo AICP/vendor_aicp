fd0b43306bd3d3e0d2f1075061298ac603f68836
Julian Veit <claymore1298@gmail.com>
2025-08-27 10:01:01 +0000

    apns: cleanup Android.mk for apns-conf.xml
    Change-Id: I7e759d38f048d18eee258b6ff77cb2813ea4edc1

--- prebuilt/common/Android.mk
+++ prebuilt/common/Android.mk
@@ -10,36 +10,6 @@ LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
 ################################
-# Copies the APN list file into $(TARGET_COPY_OUT_PRODUCT)/etc for the product as apns-conf.xml.
-# In the case where $(CUSTOM_APNS_FILE) is defined, the content of $(CUSTOM_APNS_FILE)
-# is added or replaced to the $(DEFAULT_APNS_FILE).
-include $(CLEAR_VARS)
-
-LOCAL_MODULE := apns-conf.xml
-LOCAL_MODULE_CLASS := ETC
-
-DEFAULT_APNS_FILE := vendor/aicp/prebuilt/common/etc/apns-conf.xml
-
-ifdef CUSTOM_APNS_FILE
-CUSTOM_APNS_SCRIPT := vendor/aicp/tools/custom_apns.py
-FINAL_APNS_FILE := $(local-generated-sources-dir)/apns-conf.xml
-
-$(FINAL_APNS_FILE): PRIVATE_SCRIPT := $(CUSTOM_APNS_SCRIPT)
-$(FINAL_APNS_FILE): PRIVATE_CUSTOM_APNS_FILE := $(CUSTOM_APNS_FILE)
-$(FINAL_APNS_FILE): $(CUSTOM_APNS_SCRIPT) $(DEFAULT_APNS_FILE)
-	rm -f $@
-	python $(PRIVATE_SCRIPT) $@ $(PRIVATE_CUSTOM_APNS_FILE)
-else
-FINAL_APNS_FILE := $(DEFAULT_APNS_FILE)
-endif
-
-LOCAL_PREBUILT_MODULE_FILE := $(FINAL_APNS_FILE)
-
-LOCAL_PRODUCT_MODULE := true
-
-include $(BUILD_PREBUILT)
-
-################################
 # Copies the font configuration file into product/etc for the product as fonts_customization.xml.
 # In the case where $(ADDITIONAL_FONTS_FILE) is defined, the content(s) of $(ADDITIONAL_FONTS_FILE)
 # are added to the $(AICP_FONTS_FILE).
