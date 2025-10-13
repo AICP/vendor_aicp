40aa90fb08cbfc9ab8cbf5089b9b2373f143de6d
mosimchah <mosimchah@gmail.com>
2025-10-13 16:37:24 -0400

    aicp: add our prebuilt
    chmod 755 for the scripts
    
    Change-Id: Ia430b7756617149bf7c88458359451643d25993e

--- prebuilt/common/bin/50-aicp.sh
+++ prebuilt/common/bin/50-aicp.sh
@@ -2,8 +2,8 @@
 #
 # ADDOND_VERSION=2
 #
-# /system/addon.d/50-lineage.sh
-# During a LineageOS upgrade, this script backs up /system/etc/hosts,
+# /system/addon.d/50-aicp.sh
+# During a AICP upgrade, this script backs up /system/etc/hosts,
 # /system is formatted and reinstalled, then the file is restored.
 #
 
--- prebuilt/common/bin/backuptool_postinstall.sh
+++ prebuilt/common/bin/backuptool_postinstall.sh
@@ -1,6 +1,6 @@
 #!/system/bin/sh
 #
-# LineageOS A/B OTA Postinstall Script
+# AICP A/B OTA Postinstall Script
 #
 
 /postinstall/system/bin/backuptool_ab.sh backup
