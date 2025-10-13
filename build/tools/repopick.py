4c1496021224f9fbc9d255b598773d987e55b853
mosimchah <mosimchah@gmail.com>
2025-10-13 17:44:48 -0400

    aicp: use our roomservice
    Change-Id: Ic848550a06e9bc1d64d4eac26e7a58ed4a4524d2

--- build/tools/repopick.py
+++ build/tools/repopick.py
@@ -34,7 +34,6 @@ from concurrent.futures import ThreadPoolExecutor
 from functools import cmp_to_key, partial
 from xml.etree import ElementTree
 
-
 # cmp() is not available in Python 3, define it manually
 # See https://docs.python.org/3.0/whatsnew/3.0.html#ordering-comparisons
 def cmp(a, b):
@@ -216,7 +215,7 @@ def main():
         description=textwrap.dedent(
             """\
         repopick.py is a utility to simplify the process of cherry picking
-        patches from LineageOS's Gerrit instance (or any gerrit instance of your choosing)
+        patches from AICP's Gerrit instance (or any gerrit instance of your choosing)
 
         Given a list of change numbers, repopick will cd into the project path
         and cherry pick the latest patch available.
@@ -378,7 +377,7 @@ def main():
             if not args.quiet:
                 print("")
 
-    # Get the main manifest from repo
+    # Get the master manifest from repo
     #   - convert project name and revision to a path
     project_name_to_data = {}
     manifest = subprocess.check_output(["repo", "manifest"], text=True)
