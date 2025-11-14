f768becbc0ad4db04a7fb8c8fb655c3f8ec96a27
SpiritCroc <dev@spiritcroc.de>
2025-11-14 08:08:23 +0100

    Fetch kernel source from lineage rather than AICP
    Change-Id: Ic1440e548909b90344e4d3b6887ee5c77c59453a

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -1016,7 +1016,7 @@ function build_kernel() {
         echo "Skipping kernel build"
         return
     fi
-    local lineage_version="lineage-$(_get_build_var_cached PRODUCT_VERSION_MAJOR).$(_get_build_var_cached PRODUCT_VERSION_MINOR)"
+    local lineage_version="lineage-$(_get_build_var_cached LINEAGE_VERSION_MAJOR).$(_get_build_var_cached LINEAGE_VERSION_MINOR)"
 
     local target_kernel_device="$(_get_build_var_cached TARGET_KERNEL_DEVICE)"
     local target_kernel_dir="${ANDROID_BUILD_TOP}/$(_get_build_var_cached TARGET_KERNEL_DIR)"
@@ -1065,7 +1065,7 @@ function build_kernel() {
             repo_init_args+=("--repo-rev" "${REPO_VERSION}")
         fi
 
-        yes | repo init -u https://github.com/AICP/${target_kernel_manifest}.git ${repo_init_args[@]} || [ $? -eq 141 ]
+        yes | repo init -u https://github.com/LineageOS/${target_kernel_manifest}.git ${repo_init_args[@]} || [ $? -eq 141 ]
         if [ $? -ne 0 ]; then
             echo "Kernel source repo init failed"
             popd > /dev/null
