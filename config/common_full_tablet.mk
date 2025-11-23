825dfb67afcc4d5c21534a04b6013214bb5b4101
mosimchah <mosimchah@gmail.com>
2025-11-23 01:28:41 -0500

    diff
    Change-Id: I7d4be96aa601a22baf4e2f42b4edb4aca306aa84

--- config/common_full_tablet.mk
+++ config/common_full_tablet.mk
@@ -1,7 +1,15 @@
-# Inherit mobile full common Lineage stuff
-$(call inherit-product, vendor/aicp/config/common_mobile_full.mk)
+# Inherit full common AICP stuff
+$(call inherit-product, vendor/aicp/config/common_full.mk)
 
-# Inherit tablet common Lineage stuff
+# Inherit tablet common AICP stuff
 $(call inherit-product, vendor/aicp/config/tablet.mk)
+
+# Required packages
+PRODUCT_PACKAGES += \
+    LatinIME
+
+# Include Lineage LatinIME dictionaries
+PRODUCT_PACKAGE_OVERLAYS += vendor/aicp/overlay/dictionaries
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/aicp/overlay/dictionaries
 
 $(call inherit-product, vendor/aicp/config/telephony.mk)
