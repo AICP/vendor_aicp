75f0b8f24be4a63c623a165f63f1362b83445592
mosimchah <mosimchah@gmail.com>
2025-12-01 20:15:47 -0500

    aicp: fixup config
    Change-Id: I260dac3b745dd3f945239d19acb2295eb7bbb2ef

---
+++ config/aicp_packages.mk
@@ -0,0 +1,60 @@
+# AICP packages
+PRODUCT_PACKAGES += \
+    AicpExtras \
+    TilesWallpaper
+
+# AICP Ad-block
+PRODUCT_PACKAGES += \
+    hosts.aicp_adblock
+
+# A/B OTA Optimization
+ifneq ($(AB_OTA_PARTITIONS),)
+PRODUCT_PACKAGES += \
+    checkpoint_gc \
+    otapreopt_script
+endif
+
+# Bootanimation include
+PRODUCT_PACKAGES += \
+    bootanimation.zip
+
+# Custom off-mode charger
+PRODUCT_PACKAGES += \
+    charger_res_images
+
+ifeq ($(WITH_AICP_CHARGER),true)
+PRODUCT_PACKAGES += \
+    aicp_charger_res_images \
+    font_log.png \
+    libhealthd.aicp
+endif
+
+# OmniJaws
+PRODUCT_PACKAGES += \
+    OmniJaws
+
+# Optional packages
+PRODUCT_PACKAGES += \
+    AppPredictionService \
+    WallpaperPicker2
+
+# Extra tools
+PRODUCT_PACKAGES += \
+    e2fsck \
+    libsepol \
+    mke2fs \
+    tune2fs \
+    wget
+
+# Audio
+LOCAL_PATH := frameworks/base/data/sounds
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/Alarm_Classic.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/Alarm_Classic.ogg \
+    $(LOCAL_PATH)/alarms/ogg/Krypton.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/Krypton-old.ogg \
+    $(LOCAL_PATH)/alarms/ogg/Neon.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/Neon-old.ogg \
+    $(LOCAL_PATH)/alarms/ogg/Osmium.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/Osmium-old.ogg \
+    $(LOCAL_PATH)/alarms/ogg/Oxygen.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/Oxygen-old.ogg \
+    $(LOCAL_PATH)/alarms/ogg/Platinum.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/Platinum-old.ogg \
+
+# AICP overlays
+-include packages/overlays/AICP/product_packages.mk
