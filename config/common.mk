68ff87e75c62c900659cf41d198b72a9361e4681
Julian Veit <claymore1298@gmail.com>
2025-12-05 11:22:01 +0000

    aicp: fix version logic
    Change-Id: Ie0c46281a62c1f68d039fb2ff4c3af9194b510b4

--- config/common.mk
+++ config/common.mk
@@ -285,7 +285,7 @@ PRODUCT_PACKAGE_OVERLAYS += vendor/crowdin/overlay
 PRODUCT_EXTRA_RECOVERY_KEYS += \
     vendor/aicp/build/target/product/security/lineage
 
-include vendor/aicp/config/version.mk
+#include vendor/aicp/config/version.mk
 
 # Include AICP packages
 include vendor/aicp/config/aicp_packages.mk
