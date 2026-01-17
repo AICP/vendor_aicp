3ca8e070cccc3351f5ffef2ad2405b280171226e
mosimchah <mosimchah@gmail.com>
2026-01-17 03:55:58 -0500

    aicp: remove wget
    Change-Id: Ia4a6a54ac13a1fa10d4d8eafeaf828155bf80555

--- prebuilt/common/Android.mk
+++ prebuilt/common/Android.mk
@@ -1,14 +1,5 @@
 LOCAL_PATH := $(call my-dir)
 
-# a wrapper for curl which provides wget syntax, for compatibility
-include $(CLEAR_VARS)
-LOCAL_MODULE := wget
-LOCAL_SRC_FILES := bin/wget
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := EXECUTABLES
-LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
-include $(BUILD_PREBUILT)
-
 ################################
 # Copies the font configuration file into product/etc for the product as fonts_customization.xml.
 # In the case where $(ADDITIONAL_FONTS_FILE) is defined, the content(s) of $(ADDITIONAL_FONTS_FILE)
