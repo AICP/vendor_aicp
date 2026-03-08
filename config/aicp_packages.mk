7dd3b6f9d263606e1cb77709e8475406a5c4db1d
Moshe Barash <mosimchah@gmail.com>
2026-03-08 02:40:40 +0100

    Revert "aicp: move privapp-permissions-google to system_ext"
    This reverts commit 4c42632a406021e0c52123475d9897d681e89728.
    
    Change-Id: Ie66e6af1327302def92e4b98cbea51e887ddde3f

--- config/aicp_packages.mk
+++ config/aicp_packages.mk
@@ -126,7 +126,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/aicp/config/permissions/com.google.android.dialer.support.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.google.android.dialer.support.xml \
     vendor/aicp/config/permissions/com.google.android.feature.ANDROID_ONE_EXPERIENCE.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.google.android.feature.ANDROID_ONE_EXPERIENCE.xml \
-    vendor/aicp/config/permissions/privapp-permissions-google.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-google.xml \
+    vendor/aicp/config/permissions/privapp-permissions-google.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-google.xml \
     vendor/aicp/config/permissions/privapp-permissions-google-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-google-product.xml \
     vendor/aicp/config/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml \
     vendor/aicp/config/permissions/google_build.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/google_build.xml \
