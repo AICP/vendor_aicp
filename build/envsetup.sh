cb77aaf1a6d73b8af31385c83544987881fd43cf
Nolen Johnson <johnsonnolen@gmail.com>
2026-04-16 22:12:41 -0400

    aicp: Move repopick to our lineage/scripts
    * So it tracks `main` instead of versioned branches.
    
    Change-Id: If6c3c336a3684d145a58adbbd7f22c8bc7cdebd2

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -984,7 +984,7 @@ alias cmkap='dopush cmka'
 
 function repopick() {
     T=$(gettop)
-    $T/vendor/aicp/build/tools/repopick.py $@
+    $T/lineage/scripts/repopick/repopick.py $@
 }
 
 function sort-blobs-list() {
