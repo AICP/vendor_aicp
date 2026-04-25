b9c5327e37bbf30d34611e3cae9f3ea7c0a3e984
mosimchah <mosimchah@gmail.com>
2026-04-25 16:22:19 -0400

    aicp: add gms perms from mind the gapps
    Change-Id: Iecb7ea44c4c68f5179beb86a30d152adcbf107f8

---
+++ config/aicp_gms.mk
@@ -0,0 +1,17 @@
+# Google extra permissions and features
+PRODUCT_COPY_FILES += \
+vendor/aicp/config/permissions/gms/product/etc/default-permissions/default-permissions-google.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/default-permissions/default-permissions-google.xml \
+vendor/aicp/config/permissions/gms/product/etc/default-permissions/default-permissions-mtg.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/default-permissions/default-permissions-mtg.xml \
+vendor/aicp/config/permissions/gms/product/etc/permissions/com.google.android.dialer.support.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.google.android.dialer.support.xml \
+vendor/aicp/config/permissions/gms/product/etc/permissions/privapp-permissions-google-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-google-product.xml \
+vendor/aicp/config/permissions/gms/product/etc/permissions/privapp-permissions-mtg.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-mtg.xml \
+vendor/aicp/config/permissions/gms/product/etc/sysconfig/d2d_cable_migration_feature.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/d2d_cable_migration_feature.xml \
+vendor/aicp/config/permissions/gms/product/etc/sysconfig/google-hiddenapi-package-allowlist.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/google-hiddenapi-package-allowlist.xml \
+vendor/aicp/config/permissions/gms/product/etc/sysconfig/google.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/google.xml \
+vendor/aicp/config/permissions/gms/product/etc/sysconfig/google_build.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/google_build.xml \
+vendor/aicp/config/permissions/gms/product/etc/sysconfig/sysconfig_contextual_search.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/sysconfig_contextual_search.xml \
+vendor/aicp/config/permissions/gms/product/etc/sysconfig/wellbeing.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/wellbeing.xml \
+vendor/aicp/config/permissions/gms/system_ext/etc/permissions/privapp-permissions-google-system-ext.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-google-system-ext.xml.xml
+
+
+
