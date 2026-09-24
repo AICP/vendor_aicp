4ef9fd1e149c26ddf83809f5745dafaaabbadcd7
mosimchah <mosimchah@gmail.com>
2026-09-24 10:33:55 -0400

    aicp: charger: aicp-ify
    - Generate new svg's
    
    - Use our images from before the parent revert
    
    Change-Id: I7399b2edfc8d647cc59aadd13dec775d22e06090

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -33,18 +33,18 @@ $(foreach v,$(EXPORT_TO_SOONG),$(eval $(call add_soong_config_var,lineageVarsPlu
 #endif
 
 # Charger
-#lineage_charger_density := mdpi
-#ifneq (,$(TARGET_SCREEN_DENSITY))
-#lineage_charger_density := $(strip \
-#  $(or $(if $(filter $(shell echo $$(($(TARGET_SCREEN_DENSITY) >= 560))),1),xxxhdpi),\
-#       $(if $(filter $(shell echo $$(($(TARGET_SCREEN_DENSITY) >= 400))),1),xxhdpi),\
-#       $(if $(filter $(shell echo $$(($(TARGET_SCREEN_DENSITY) >= 280))),1),xhdpi),\
-#       $(if $(filter $(shell echo $$(($(TARGET_SCREEN_DENSITY) >= 200))),1),hdpi,mdpi)))
-#else ifneq (,$(filter mdpi hdpi xhdpi xxhdpi xxxhdpi,$(PRODUCT_AAPT_PREF_CONFIG)))
+lineage_charger_density := mdpi
+ifneq (,$(TARGET_SCREEN_DENSITY))
+lineage_charger_density := $(strip \
+  $(or $(if $(filter $(shell echo $$(($(TARGET_SCREEN_DENSITY) >= 560))),1),xxxhdpi),\
+       $(if $(filter $(shell echo $$(($(TARGET_SCREEN_DENSITY) >= 400))),1),xxhdpi),\
+       $(if $(filter $(shell echo $$(($(TARGET_SCREEN_DENSITY) >= 280))),1),xhdpi),\
+       $(if $(filter $(shell echo $$(($(TARGET_SCREEN_DENSITY) >= 200))),1),hdpi,mdpi)))
+else ifneq (,$(filter mdpi hdpi xhdpi xxhdpi xxxhdpi,$(PRODUCT_AAPT_PREF_CONFIG)))
 # If PRODUCT_AAPT_PREF_CONFIG includes a dpi bucket, then use that value.
-#lineage_charger_density := $(PRODUCT_AAPT_PREF_CONFIG)
-#endif
-#$(call soong_config_set,lineage_charger,density,$(lineage_charger_density))
+lineage_charger_density := $(PRODUCT_AAPT_PREF_CONFIG)
+endif
+$(call soong_config_set,lineage_charger,density,$(lineage_charger_density))
 
 # Libui
 ifneq ($(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS),)
--- config/common_mobile.mk
+++ config/common_mobile.mk
@@ -38,14 +38,14 @@ PRODUCT_PACKAGES += \
     Launcher3Overlay
 
 # Charger
-#PRODUCT_PACKAGES += \
-#    charger_res_images
-#
-#ifneq ($(WITH_LINEAGE_CHARGER),false)
-#PRODUCT_PACKAGES += \
-#    lineage_charger_animation \
-#    lineage_charger_animation_vendor
-#endif
+PRODUCT_PACKAGES += \
+    charger_res_images
+
+ifneq ($(WITH_LINEAGE_CHARGER),false)
+PRODUCT_PACKAGES += \
+    lineage_charger_animation \
+    lineage_charger_animation_vendor
+endif
 
 # Credential storage
 PRODUCT_PACKAGES += \
